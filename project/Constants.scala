9071e007d1e451186a09811cba9e5eedd7f57467
Aidan Cunniffe <acunniffe@gmail.com>
2018-03-25 18:20:13 -0400

    Bumped Optic Markdown version to remove validation bug

--- project/Constants.scala
+++ project/Constants.scala
@@ -4,9 +4,9 @@ import scala.io.Source
 import scala.util.Try
 
 object Constants {
-  val opticMDVersion = "0.1.3"
+  val opticMDVersion = "0.1.4"
   val opticMDTar = s"https://registry.npmjs.org/optic-markdown/-/optic-markdown-${opticMDVersion}.tgz"
-  val opticMDTarSum = "e3eabdf34733e38923d2b308015e36d6378a5859"
+  val opticMDTarSum = "6c039401e306eefa5fcaf6d4b03051a33a6ac54a"
 
   val mixpanelToken: String = Try { Source.fromFile(new File("project/mixpanel-token")).getLines().toVector.head }.getOrElse(null)
 
