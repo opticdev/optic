346ac8575dc06c71450408c331ca49b513af2b3e
Dev Doshi <dev@useoptic.com>
2019-06-23 13:16:31 -0400

    change response status code selector

--- webapp/src/components/http/HttpResponseStatusCodeInput.js
+++ webapp/src/components/http/HttpResponseStatusCodeInput.js
@@ -1,25 +1,52 @@
+import IconButton from '@material-ui/core/IconButton';
 import React from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
+import EditIcon from '@material-ui/icons/Edit'
+import Menu from '@material-ui/core/Menu';
 import {STATUS_CODES} from 'http';
 
 const statusCodes = [...new Set(Object.keys(STATUS_CODES).filter(x => x >= 200).map(x => x.toString()))].sort()
 
-function HttpResponseStatusCodeInput({open, selectedStatusCode, setSelectedStatusCode}) {
-    const handleChange = (e) => {
+function HttpResponseStatusCodeInput({selectedStatusCode, setSelectedStatusCode}) {
+
+    function handleStatusCodeSelected(e) {
         setSelectedStatusCode(parseInt(e.target.value, 10))
+        handleClose()
     }
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    function handleClick(event) {
+        setAnchorEl(event.currentTarget);
+    }
+
+    function handleClose() {
+        setAnchorEl(null);
+    }
+
     return (
-        <Select value={selectedStatusCode} onChange={handleChange}>
-            {statusCodes
-                .map((statusCode) => {
-                    const description = `${statusCode}: ${STATUS_CODES[statusCode]}`
-                    return (
-                        <MenuItem button value={statusCode} key={statusCode}>{description}</MenuItem>
-                    )
-                })
-            }
-        </Select>
+        <>
+            <IconButton onClick={handleClick}><EditIcon/></IconButton>
+            <Menu
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                anchorEl={anchorEl}
+            >
+                {statusCodes
+                    .map((statusCode) => {
+                        const description = `${statusCode}: ${STATUS_CODES[statusCode]}`
+                        return (
+                            <MenuItem
+                                button
+                                value={statusCode} key={statusCode}
+                                onClick={handleStatusCodeSelected}
+                            >{description}</MenuItem>
+                        )
+                    })
+                }
+            </Menu>
+        </>
     )
 }
 
