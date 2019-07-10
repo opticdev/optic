ed0554608ad51f89672db4517e3ebc074434ce9f
Aidan Cunniffe <acunniffe@gmail.com>
2019-07-10 08:56:59 -0700

    fixed analytics code

--- webapp/src/Analytics.js
+++ webapp/src/Analytics.js
@@ -5,7 +5,7 @@ export function track(event, props) {
 	mixpanel.track(event, props)
 }
 
-if (window.localStorage.getItem('_known_user')) {
+if (!window.localStorage.getItem('_known_user')) {
 	window.localStorage.setItem('_known_user', 'true')
 	fetch('https://hooks.slack.com/services/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', {
 		method: 'POST',
