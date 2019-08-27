2121e31c0eb0d9e5875a138c8200af6e1faef178
Aidan Cunniffe <acunniffe@gmail.com>
2019-08-27 14:07:50 -0400

    Analytics

--- webapp/src/Analytics.js
+++ webapp/src/Analytics.js
@@ -3,7 +3,8 @@ import mixpanel from 'mixpanel-browser'
 if (process.env.REACT_APP_ENABLE_ANALYTICS === 'no') {
     console.warn('Analytics is disabled')
 } else {
-    mixpanel.init('78a42ccba0e9a55de00c30b454c5da8e', { api_host: 'data.useoptic.com' });
+    mixpanel.init('78a42ccba0e9a55de00c30b454c5da8e', { api_host: 'https://data.useoptic.com' });
+    window.mixpanel = mixpanel
 }
 
 export function track(event, props) {
