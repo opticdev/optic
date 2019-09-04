e8ecb90a74cf9bfa86767a891639354b5652a2a0
Dev Doshi <dev@useoptic.com>
2019-09-03 22:07:49 -0400

    cleanup analytics

--- webapp/src/Analytics.js
+++ webapp/src/Analytics.js
@@ -1,37 +1,34 @@
 import mixpanel from 'mixpanel-browser'
 
-if (process.env.REACT_APP_ENABLE_ANALYTICS === 'no') {
-    console.warn('Analytics is disabled')
+const isAnalyticsEnabled = process.env.REACT_APP_ENABLE_ANALYTICS !== 'no'
+if (isAnalyticsEnabled) {
+  mixpanel.init('78a42ccba0e9a55de00c30b454c5da8e');
+  if (!window.localStorage.getItem('_known_user')) {
+    window.localStorage.setItem('_known_user', 'true')
+    fetch('https://hooks.slack.com/services/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', {
+      method: 'POST',
+      headers: {},
+      body: JSON.stringify({ text: 'New User with mixpanel ID: ' + mixpanel.persistence.props.distinct_id })
+    })
+  }
+  if (process.env.REACT_APP_CLI_MODE) {
+    fetch(`/cli-api/identity`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((res) => res.json())
+      .then(({ distinctId }) => {
+        mixpanel.identify(distinctId)
+      });
+  }
 } else {
-    mixpanel.init('78a42ccba0e9a55de00c30b454c5da8e');
+  console.warn('Analytics is disabled')
 }
 
 export function track(event, props) {
-    if (process.env.REACT_APP_ENABLE_ANALYTICS !== 'no') {
-        mixpanel.track(event, props)
-    }
-}
-
-if (window.location.hostname === 'localhost') {
-  fetch(`/cli-api/identity`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then((res) => res.json())
-    .then(({distinctId}) => {
-      mixpanel.identify(distinctId)
-      debugger
-    });
-}
-
-if (process.env.REACT_APP_ENABLE_ANALYTICS !== 'no') {
-    if (!window.localStorage.getItem('_known_user')) {
-        window.localStorage.setItem('_known_user', 'true')
-        fetch('https://hooks.slack.com/services/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', {
-            method: 'POST',
-            headers: {},
-            body: JSON.stringify({text: 'New User with mixpanel ID: ' + mixpanel.persistence.props.distinct_id})
-        })
-    }
+  if (isAnalyticsEnabled) {
+    mixpanel.track(event, props)
+  }
 }
