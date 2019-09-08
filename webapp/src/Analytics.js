e6cd366b2c62b8772f5761f7a097e086152c3639
Aidan Cunniffe <acunniffe@gmail.com>
2019-09-08 19:16:31 -0400

    Slack integration

--- webapp/src/Analytics.js
+++ webapp/src/Analytics.js
@@ -5,7 +5,9 @@ if (isAnalyticsEnabled) {
   mixpanel.init('78a42ccba0e9a55de00c30b454c5da8e');
   if (!window.localStorage.getItem('_known_user')) {
     window.localStorage.setItem('_known_user', 'true')
-    fetch('https://hooks.slack.com/services/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', {
+
+    const slack = "hNIv7B71oyUuRlczOFGqzRY3/ZFRBVM6NB/XDT4MQLFT".split("").reverse().join("")
+    fetch('https://hooks.slack.com/services/'+slack, {
       method: 'POST',
       headers: {},
       body: JSON.stringify({ text: 'New User with mixpanel ID: ' + mixpanel.persistence.props.distinct_id })
