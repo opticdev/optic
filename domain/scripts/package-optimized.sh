35fc967f3a5e3533c6be6dfb004bc5be148fafa5
Aidan Cunniffe <acunniffe@gmail.com>
2019-09-08 16:17:08 -0400

    Fixed API CLI

--- domain/scripts/package-optimized.sh
+++ domain/scripts/package-optimized.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 sbt fullOptJS
 OUTPUT_FILE=./build/domain.js
+API_CLI_FILE=../api-cli/provided/domain.js
 rm -rf $OUTPUT_FILE
+rm -rf API_CLI_FILE
 
 touch $OUTPUT_FILE
+touch $API_CLI_FILE
 
 echo "/* eslint-disable */\n" >> $OUTPUT_FILE
+echo "/* eslint-disable */\n" >> $API_CLI_FILE
 cat target/scala-2.12/seamless-ddd-opt.js >> $OUTPUT_FILE
+cat target/scala-2.12/seamless-ddd-opt.js >> $API_CLI_FILE
 
 echo "domain logic written to $OUTPUT_FILE"
+echo "domain logic written to $API_CLI_FILE"
