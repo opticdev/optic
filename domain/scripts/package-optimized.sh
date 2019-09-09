f863dfd9a251ce8ff5dba5d2a40d7051bc977f45
Dev Doshi <dev@useoptic.com>
2019-09-09 13:37:56 -0400

    build script should ensure output directory exists

--- domain/scripts/package-optimized.sh
+++ domain/scripts/package-optimized.sh
@@ -2,14 +2,12 @@
 sbt fullOptJS
 OUTPUT_FILE=./build/domain.js
 API_CLI_FILE=../api-cli/provided/domain.js
-rm -rf $OUTPUT_FILE
-rm -rf API_CLI_FILE
 
-touch $OUTPUT_FILE
-touch $API_CLI_FILE
+mkdir -p `dirname $OUTPUT_FILE`
+mkdir -p `dirname $API_CLI_FILE`
 
-echo "/* eslint-disable */\n" >> $OUTPUT_FILE
-echo "/* eslint-disable */\n" >> $API_CLI_FILE
+echo "/* eslint-disable */\n" > $OUTPUT_FILE
+echo "/* eslint-disable */\n" > $API_CLI_FILE
 cat target/scala-2.12/seamless-ddd-opt.js >> $OUTPUT_FILE
 cat target/scala-2.12/seamless-ddd-opt.js >> $API_CLI_FILE
 
