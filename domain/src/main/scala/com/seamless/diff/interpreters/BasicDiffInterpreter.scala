95bc03ae2551fff966eb8bc762fd22fc042dacc4
Dev Doshi <dev@useoptic.com>
2019-10-07 16:10:21 -0400

    cleanup

--- domain/src/main/scala/com/seamless/diff/interpreters/BasicDiffInterpreter.scala
+++ domain/src/main/scala/com/seamless/diff/interpreters/BasicDiffInterpreter.scala
@@ -1,9 +1,7 @@
 package com.seamless.diff.interpreters
 
-import com.seamless.contexts.shapes.ShapesHelper._
 import com.seamless.contexts.shapes._
 import com.seamless.diff.RequestDiffer._
-import com.seamless.diff.initial._
 import com.seamless.diff.{DiffInterpretation, Interpretations, ShapeDiffer}
 
 class BasicDiffInterpreter(_shapesState: ShapesState) extends Interpreter[RequestDiffResult] {
@@ -16,30 +14,29 @@ class BasicDiffInterpreter(_shapesState: ShapesState) extends Interpreter[Reques
       case d: UnmatchedRequestContentType => Seq(Interpretations.ChangeRequestContentType(d.requestId, d.contentType, d.previousContentType))
       case d: UnmatchedRequestBodyShape => {
         d.shapeDiff match {
-          case sd: ShapeDiffer.UnsetShape => Seq(Interpretations.AddInitialRequestBodyShape(sd.actual, d.requestId, d.contentType))
-          case sd: ShapeDiffer.UnexpectedObjectKey => Seq(Interpretations.AddFieldToRequestShape(sd.key, sd.actual, sd.parentObjectShapeId, d.requestId))
-          case sd: ShapeDiffer.KeyShapeMismatch => {
+          case sd: ShapeDiffer.UnsetShape =>
+            Seq(Interpretations.AddInitialRequestBodyShape(sd.actual, d.requestId, d.contentType))
+          case sd: ShapeDiffer.UnexpectedObjectKey =>
+            Seq(Interpretations.AddFieldToRequestShape(sd.key, sd.actual, sd.parentObjectShapeId, d.requestId))
+          case sd: ShapeDiffer.KeyShapeMismatch =>
             Seq(Interpretations.ChangeFieldInRequestShape(sd.key, sd.fieldId, sd.actual, d.requestId))
-          }
           case _ => Seq.empty
         }
       }
-      case d: UnmatchedResponseContentType => Seq(Interpretations.ChangeResponseContentType(d.statusCode, d.responseId, d.contentType, d.previousContentType))
+
+      case d: UnmatchedResponseContentType =>
+        Seq(Interpretations.ChangeResponseContentType(d.statusCode, d.responseId, d.contentType, d.previousContentType))
 
       case d: UnmatchedResponseBodyShape => {
         d.shapeDiff match {
           case sd: ShapeDiffer.UnsetShape =>
             Seq(Interpretations.AddInitialResponseBodyShape(sd.actual, d.responseStatusCode, d.responseId, d.contentType))
-
           case sd: ShapeDiffer.UnexpectedObjectKey =>
             Seq(Interpretations.AddFieldToResponseShape(sd.key, sd.actual, sd.parentObjectShapeId, d.responseStatusCode, d.responseId))
-          case sd: ShapeDiffer.KeyShapeMismatch => {
-            val newShapeId = ShapeResolver.resolveJsonToShapeId(sd.actual).getOrElse(AnyKind.baseShapeId)
+          case sd: ShapeDiffer.KeyShapeMismatch =>
             Seq(Interpretations.ChangeFieldInResponseShape(sd.key, sd.fieldId, sd.actual, d.responseStatusCode))
-          }
           case _ => Seq.empty
         }
-
       }
 
       case _ => Seq.empty
