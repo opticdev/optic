cd146070551d4b4f62f08de5c70f2605c5e964c6
Dev Doshi <dev@useoptic.com>
2020-01-17 12:18:54 -0500

    scala domain build scripts should output to workspace

--- core/scripts/package.sh
+++ core/scripts/package.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 sbt fastOptJS
-OUTPUT_FILE=./build/domain.js
-API_CLI_FILE=../api-cli/provided/domain.js
+OUTPUT_FILE=../workspaces/domain/src/domain.js
 
-mkdir -p `dirname $OUTPUT_FILE`
-mkdir -p `dirname $API_CLI_FILE`
+mkdir -p $(dirname $OUTPUT_FILE)
 
-echo "/* eslint-disable */\n" > $OUTPUT_FILE
-echo "/* eslint-disable */\n" > $API_CLI_FILE
 cat target/scala-2.12/optic-core-fastopt.js >> $OUTPUT_FILE
-cat target/scala-2.12/optic-core-fastopt.js >> $API_CLI_FILE
 
 echo "domain logic written to $OUTPUT_FILE"
-echo "domain logic written to $API_CLI_FILE"
