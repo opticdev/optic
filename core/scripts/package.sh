83d307cf238ef144f82afa745ce4ecd27e810539
Dev Doshi <dev@useoptic.com>
2020-01-18 20:30:53 -0500

    build scripts should overwrite output file

--- core/scripts/package.sh
+++ core/scripts/package.sh
@@ -4,6 +4,6 @@ OUTPUT_FILE=../workspaces/domain/src/domain.js
 
 mkdir -p $(dirname $OUTPUT_FILE)
 
-cat target/scala-2.12/optic-core-fastopt.js >> $OUTPUT_FILE
+cat target/scala-2.12/optic-core-fastopt.js > $OUTPUT_FILE
 
 echo "domain logic written to $OUTPUT_FILE"
