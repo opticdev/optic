5dc92b33454159da67af9e01411fcaf39d289397
Dev Doshi <dev@useoptic.com>
2020-01-20 16:41:21 -0500

    add command to watch and rebuild workspace packages

--- export_development_aliases.sh
+++ export_development_aliases.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 alias apidev="$DIR/workspaces/local-cli/bin/run"
+alias watch-optic="yarn wsrun -p '@useoptic/*' -c watch 'yarn run ws:build' './src'"
