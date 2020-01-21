6344ced6c96007ad274cf05f29499766972a6881
Dev Doshi <dev@useoptic.com>
2020-01-21 15:13:51 -0500

    development scripts should run from the source directory

--- export_development_aliases.sh
+++ export_development_aliases.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-alias apidev="$DIR/workspaces/local-cli/bin/run"
-alias watch-optic="yarn wsrun -p '@useoptic/*' -c watch 'yarn run ws:build' './src'"
+# usage: $ source ./export_development_aliases.sh
+export OPTIC_SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "Optic development scripts will run from $OPTIC_SRC_DIR"
+alias apidev="OPTIC_DAEMON_ENABLE_DEBUGGING=yes $OPTIC_SRC_DIR/workspaces/local-cli/bin/run"
+alias watch-optic="cd $OPTIC_SRC_DIR && yarn wsrun -p '@useoptic/*' -c watch 'yarn run ws:build' './src'"
 alias rescue-optic="rm -rf ~/.optic/daemon-lock.json.lock/ ~/.optic/daemon-lock.json"
