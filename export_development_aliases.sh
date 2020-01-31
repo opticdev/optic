b4aa9ec868ebd9dab589eaf14e30440ceb7f1f8c
Dev Doshi <dev@useoptic.com>
2020-01-31 14:02:42 -0500

    add convenience script for installing from local registry

--- export_development_aliases.sh
+++ export_development_aliases.sh
@@ -6,3 +6,4 @@ alias apidev="OPTIC_DAEMON_ENABLE_DEBUGGING=yes OPTIC_UI_HOST=http://localhost:3
 alias watch-optic="cd $OPTIC_SRC_DIR && yarn wsrun -p '@useoptic/*' -c watch 'yarn run ws:build' './src'"
 alias rescue-optic="rm -rf ~/.optic/daemon-lock.json.lock/ ~/.optic/daemon-lock.json"
 alias publish-optic-locally="cd $OPTIC_SRC_DIR && yarn run registry:clean-optic && yarn run registry:start-background && yarn run publish-local"
+alias install-optic-from-local-registry="YARN_REGISTRY=http://localhost:4873 yarn global add @useoptic/cli --registry=http://localhost:4873"
