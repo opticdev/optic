ab477c66bad527d40dfb2b5f0dce0237091e174d
Dev Doshi <dev@useoptic.com>
2020-02-26 12:19:13 -0500

    responses should be resolved via (pathId, method, statusCode)

--- core/optic/shared/src/main/scala/com/useoptic/diff/interactions/Resolvers.scala
+++ core/optic/shared/src/main/scala/com/useoptic/diff/interactions/Resolvers.scala
@@ -7,12 +7,23 @@ import com.useoptic.types.capture.HttpInteraction
 object Resolvers {
   def resolveOperations(interaction: HttpInteraction, pathId: Commands.PathComponentId, requestsState: RequestsState) = {
     requestsState.requests.values
-      .filter(r => r.requestDescriptor.pathComponentId == pathId && r.requestDescriptor.httpMethod == interaction.request.method)
+      .filter(r => (
+        r.requestDescriptor.pathComponentId == pathId
+          && r.requestDescriptor.httpMethod == interaction.request.method
+        )
+      )
   }
 
   def resolveResponses(interaction: HttpInteraction, requestId: Commands.RequestId, requestsState: RequestsState) = {
+    val request = requestsState.requests(requestId)
     println(requestsState.responses.values)
     requestsState.responses.values
-      .filter(r => r.responseDescriptor.httpStatusCode == interaction.response.statusCode)
+      .filter(r => {
+        (
+          r.responseDescriptor.pathId == request.requestDescriptor.pathComponentId
+            && r.responseDescriptor.httpMethod == request.requestDescriptor.httpMethod
+            && r.responseDescriptor.httpStatusCode == interaction.response.statusCode
+          )
+      })
   }
 }
