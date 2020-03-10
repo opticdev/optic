fdeab304ce2453afbddbde91c34313c994027869
Dev Doshi <dev@useoptic.com>
2020-03-09 23:15:28 -0400

    support start and end commands to be run before and after saving a batch of commands

--- core/optic/shared/src/main/scala/com/useoptic/serialization/EventSerialization.scala
+++ core/optic/shared/src/main/scala/com/useoptic/serialization/EventSerialization.scala
@@ -1,7 +1,7 @@
 package com.useoptic.serialization
 
 import com.useoptic.contexts.requests.Events.RequestsEvent
-import com.useoptic.contexts.rfc.Events.{ContributionEvent, RfcEvent, VersionControlEvent}
+import com.useoptic.contexts.rfc.Events.{ContributionEvent, RfcEvent, SpecEvolutionEvent, VersionControlEvent}
 import com.useoptic.contexts.shapes.Events.ShapesEvent
 import io.circe.Decoder.Result
 import io.circe._
@@ -17,6 +17,7 @@ object EventSerialization {
       case requestEvent: RequestsEvent => requestEvent.asJson
       case contributionEvent: ContributionEvent => contributionEvent.asJson
       case versionControlEvent: VersionControlEvent => versionControlEvent.asJson
+      case specEvolutionEvent: SpecEvolutionEvent => specEvolutionEvent.asJson
       case _ => throw new java.lang.Error("Unhandled Event Type")
     }.asJson
   }
@@ -29,13 +30,16 @@ object EventSerialization {
 
   private def decodeVersionControlEvent(item: Json): Result[VersionControlEvent] = item.as[VersionControlEvent]
 
+  private def decodeSpecEvolutionEvent(item: Json): Result[SpecEvolutionEvent] = item.as[SpecEvolutionEvent]
+
   def fromJson(json: Json): Try[Vector[RfcEvent]] = Try {
     val parseResults = json.asArray.get.map {
       case i => TryChainUtil.firstSuccessIn(i,
         (j: Json) => Try(decodeShapesEvent(j).right.get),
         (j: Json) => Try(decodeRequestEvent(j).right.get),
         (j: Json) => Try(decodeContributionEvent(j).right.get),
-        (j: Json) => Try(decodeVersionControlEvent(j).right.get)
+        (j: Json) => Try(decodeVersionControlEvent(j).right.get),
+        (j: Json) => Try(decodeSpecEvolutionEvent(j).right.get)
       )
     }
     require(parseResults.forall(_.isDefined), "Some events could not be decoded")
