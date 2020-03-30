e2e6f62d868a3344b4b70e9147b382b262438187
Jaap Rood <jaap@jaaprood.nl>
2020-03-30 12:43:38 +0200

    Make component definitions the most prominent in a component module

--- workspaces/ui/src/components/dashboards/TestingDashboard.js
+++ workspaces/ui/src/components/dashboards/TestingDashboard.js
@@ -18,6 +18,26 @@ import {
 import { stuffFromQueries } from '../../contexts/RfcContext';
 import * as uniqBy from 'lodash.uniqby';
 
+export default function TestingDashboardContainer(props) {
+  const { match, service } = props;
+
+  if (!service) {
+    return <Loading />;
+  }
+
+  return (
+    <TestingServiceContextProvider value={service}>
+      <Switch>
+        <Route
+          path={`${match.url}/captures/:captureId`}
+          component={TestingDashboard}
+        />
+        <Route component={DefaultReportRedirect} />
+      </Switch>
+    </TestingServiceContextProvider>
+  );
+}
+
 function DefaultReportRedirect(props) {
   const { match } = props;
   const baseUrl = match.url;
@@ -42,6 +62,63 @@ function DefaultReportRedirect(props) {
     // TODO: revisit this UI state
     return <div>You don't have any captures yet</div>;
   }
+}
+export function TestingDashboard(props) {
+  const { captureId } = props.match.params;
+  const { loading: loadingReport, result: report } = useTestingService(
+    (service) => service.loadReport(captureId),
+    [captureId]
+  );
+
+  const { loading: loadingSpec, result: spec } = useSpec(captureId);
+
+  return (
+    <div>
+      <h2>Live Contract Testing Dashboard for capture {captureId}</h2>
+
+      {(loadingReport || loadingSpec) && <Loading />}
+
+      {report && spec && <TestingReport report={report} spec={spec} />}
+    </div>
+  );
+}
+
+export function TestingReport(props) {
+  const { report, spec } = props;
+  const { counts } = report;
+  const { endpoints } = spec;
+
+  return (
+    <div>
+      <h3>Testing report</h3>
+
+      <h4>Summary for {spec.apiName}</h4>
+      <ul>
+        <li>Created at: {report.createdAt}</li>
+        <li>Last updated: {report.updatedAt}</li>
+        <li>Total interactions: {counts.totalInteractions}</li>
+        <li>Compliant interactions: {counts.totalCompliantInteractions}</li>
+        <li>Unmatched paths: {counts.totalUnmatchedPaths}</li>
+        <li>Total diffs: {counts.totalDiffs}</li>
+      </ul>
+
+      <h4>Endpoints</h4>
+
+      {endpoints.length > 0 ? (
+        <ul>
+          {endpoints.map((endpoint) => (
+            <li key={endpoint.request.requestId}>
+              <strong>{endpoint.request.httpMethod}</strong>{' '}
+              {endpoint.path.name}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        // @TODO: revisit this empty state
+        <p>No endpoints have been documented yet</p>
+      )}
+    </div>
+  );
 }
 
 function useSpec(captureId) {
@@ -104,82 +181,4 @@ export function specFromEvents(events) {
     apiName,
     endpoints
   };
-}
-
-export default function TestingDashboardContainer(props) {
-  const { match, service } = props;
-
-  if (!service) {
-    return <Loading />;
-  }
-
-  return (
-    <TestingServiceContextProvider value={service}>
-      <Switch>
-        <Route
-          path={`${match.url}/captures/:captureId`}
-          component={TestingDashboard}
-        />
-        <Route component={DefaultReportRedirect} />
-      </Switch>
-    </TestingServiceContextProvider>
-  );
-}
-
-export function TestingDashboard(props) {
-  const { captureId } = props.match.params;
-  const { loading: loadingReport, result: report } = useTestingService(
-    (service) => service.loadReport(captureId),
-    [captureId]
-  );
-
-  const { loading: loadingSpec, result: spec } = useSpec(captureId);
-
-  return (
-    <div>
-      <h2>Live Contract Testing Dashboard for capture {captureId}</h2>
-
-      {(loadingReport || loadingSpec) && <Loading />}
-
-      {report && spec && <TestingReport report={report} spec={spec} />}
-    </div>
-  );
-}
-
-export function TestingReport(props) {
-  const { report, spec } = props;
-  const { counts } = report;
-  const { endpoints } = spec;
-
-  return (
-    <div>
-      <h3>Testing report</h3>
-
-      <h4>Summary for {spec.apiName}</h4>
-      <ul>
-        <li>Created at: {report.createdAt}</li>
-        <li>Last updated: {report.updatedAt}</li>
-        <li>Total interactions: {counts.totalInteractions}</li>
-        <li>Compliant interactions: {counts.totalCompliantInteractions}</li>
-        <li>Unmatched paths: {counts.totalUnmatchedPaths}</li>
-        <li>Total diffs: {counts.totalDiffs}</li>
-      </ul>
-
-      <h4>Endpoints</h4>
-
-      {endpoints.length > 0 ? (
-        <ul>
-          {endpoints.map((endpoint) => (
-            <li key={endpoint.request.requestId}>
-              <strong>{endpoint.request.httpMethod}</strong>{' '}
-              {endpoint.path.name}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        // @TODO: revisit this empty state
-        <p>No endpoints have been documented yet</p>
-      )}
-    </div>
-  );
 }
