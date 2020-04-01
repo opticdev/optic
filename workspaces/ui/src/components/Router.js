c26e18543cfa52b7b30512d0a1820f2059dd7622
Jaap Rood <jaap@jaaprood.nl>
2020-04-01 15:36:43 +0200

    Attempt to wrap the React Router components to automatically prefix them with debug paths

---
+++ workspaces/ui/src/components/Router.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import {
+  Link as RouterLink,
+  NavLink as RouterNavLink,
+  Redirect as RouterRedirect,
+  Route as RouterRoute,
+  Switch
+} from 'react-router-dom';
+import { useDebugPath } from '../contexts/DebugSessionContext';
+
+// re-export Switch for convenience
+export { Switch };
+
+export function Link(props) {
+  const debugPrefix = useDebugPath();
+  const prefix = typeof props.prefix === 'undefined' ? false : props.prefix;
+  const to = prefix ? prefixPath(props.to, debugPrefix) : props.to;
+
+  return <RouterLink {...props} to={to} />;
+}
+Link.displayName = 'OpticLink';
+
+export function NavLink(props) {
+  const debugPrefix = useDebugPath();
+  const prefix = typeof props.prefix === 'undefined' ? false : props.prefix;
+  const to = prefix ? prefixPath(props.to, debugPrefix) : props.to;
+
+  return <RouterNavLink {...props} to={to} />;
+}
+NavLink.dislayName = 'OpticNavLink';
+
+export function Redirect(props) {
+  const debugPrefix = useDebugPath();
+  const prefix = typeof props.prefix === 'undefined' ? false : props.prefix;
+  const from = prefix ? prefixPath(props.from, debugPrefix) : props.from;
+  const to = prefix ? prefixPath(props.to, debugPrefix) : props.to;
+
+  return <RouterRedirect from={from} to={to} />;
+}
+Redirect.displayName = 'OpticRedirect';
+
+export function Route(props) {
+  const debugPrefix = useDebugPath();
+  const path = prefixPath(props.path, debugPrefix);
+
+  return <RouterRoute {...props} path={path} />;
+}
+Route.displayName = 'OpticRoute';
+
+function prefixPath(path, prefix) {
+  if (!path) {
+    // skip undefined paths
+    return path;
+  } else if (typeof path === 'function') {
+    // functions for Link's `to`
+    return (location) => {
+      return prefix(location, prefix);
+    };
+  } else if (Array.isArray(path)) {
+    // array of pathnames, for Route
+    return path.map((p) => prefixPath(p, prefix));
+  } else if (typeof path === 'object' && path.pathname) {
+    // location object
+    return {
+      ...path,
+      pathName: prefix(path.pathname, prefix)
+    };
+  } else if (typeof path === 'string') {
+    return `${prefix}${path}`;
+  } else {
+    debugger;
+    throw Error('Invalid type of path to prefix with debug path');
+  }
+}
