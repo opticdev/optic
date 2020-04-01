16f143c73ac4e2d1335d286f54eb921672895e62
Jaap Rood <jaap@jaaprood.nl>
2020-04-01 11:29:02 +0200

    Refactor app routes making every route accept a '/debug' prefix and remove distinction between local and web routes

--- workspaces/ui/src/routes.js
+++ workspaces/ui/src/routes.js
@@ -3,63 +3,88 @@ import { Redirect, Switch, Route } from 'react-router-dom';
 import Welcome from './components/onboarding/Welcome';
 import ExampleSessionsLoader from './components/loaders/ExampleSessionsLoader.js';
 import LocalLoader from './components/routes/local';
-import { routerPaths } from './RouterPaths'
+import { routerPaths } from './RouterPaths';
 import SharedLoader from './components/loaders/SharedLoader';
 import { ExampleTestingServiceLoaderComponent } from './components/loaders/TestingDashboardLoader';
 
+function AppRoutes(props) {
+  const baseUrl = props.baseUrl || '';
 
-class AppRoutes extends React.Component {
-  render() {
-    // in local mode
-    if (process.env.REACT_APP_CLI_MODE) {
-      return (
-        <div>
-          <Switch>
-            <Route
-              strict
-              path={routerPaths.exampleTestingDashboard()}
-              component={ExampleTestingServiceLoaderComponent}
-            />
-            <Route
-              strict
-              path={routerPaths.exampleTestingDashboard()}
-              to={routerPaths.exampleTestingDashboard()}
-            />
-            <Route
-              strict
-              path={routerPaths.exampleSessionsRoot()}
-              component={ExampleSessionsLoader}
-            />
-            <Redirect
-              from={routerPaths.exampleSessionsRoot()}
-              to={routerPaths.exampleSessionsRoot()}
-            />
-            <Route
-              strict
-              path={routerPaths.localRoot()}
-              component={LocalLoader}
-            />
-            <Redirect to={routerPaths.localRoot()} />
-          </Switch>
-        </div>
-      );
-    }
-
-    // running on website
-    return (
-      <div>
-        <Switch>
-          <Route strict path={routerPaths.sharedRoot()} component={SharedLoader} />
-          <Redirect from={routerPaths.sharedRoot()} to={routerPaths.sharedRoot()} />
-          <Route strict path={routerPaths.exampleSessionsRoot()} component={ExampleSessionsLoader} />
-          <Redirect from={routerPaths.exampleSessionsRoot()} to={routerPaths.exampleSessionsRoot()} />
-          <Route exact path={'/'} component={Welcome} />
-          <Redirect to={'/'} />
-        </Switch>
-      </div>
-    );
-
-  }
+  // TODO: refactor router paths to no longer have special example routes and accept
+  // baseurl natively
+  return (
+    <Switch>
+      <Route
+        strict
+        path={baseUrl + routerPaths.exampleTestingDashboard()}
+        component={ExampleTestingServiceLoaderComponent}
+      />
+      <Route
+        strict
+        path={baseUrl + routerPaths.exampleTestingDashboard()}
+        to={routerPaths.exampleTestingDashboard()}
+      />
+      <Route
+        strict
+        path={baseUrl + routerPaths.exampleSessionsRoot()}
+        component={ExampleSessionsLoader}
+      />
+      <Redirect
+        from={baseUrl + routerPaths.exampleSessionsRoot()}
+        to={baseUrl + routerPaths.exampleSessionsRoot()}
+      />
+      <Route
+        strict
+        path={baseUrl + routerPaths.localRoot()}
+        component={LocalLoader}
+      />
+      <Redirect to={baseUrl + routerPaths.localRoot()} />
+    </Switch>
+  );
 }
 
-export default AppRoutes;
+function ExampleRoutes(props) {
+  // TODO: wrap with context that provides the example data
+
+  return <AppRoutes baseUrl={props.match.url} />;
+}
+
+export default function Routes() {
+  return (
+    <div>
+      <Switch>
+        <Route strict path="/debug" component={ExampleRoutes} />
+        <Route component={AppRoutes} />
+      </Switch>
+    </div>
+  );
+}
+
+// class AppRoutes extends React.Component {
+//   render() {
+//     // in local mode
+//     if (process.env.REACT_APP_CLI_MODE) {
+//       return (
+//         <div>
+//         </div>
+//       );
+//     }
+
+//     // running on website
+//     return (
+//       <div>
+//         <Switch>
+//           <Route strict path={routerPaths.sharedRoot()} component={SharedLoader} />
+//           <Redirect from={routerPaths.sharedRoot()} to={routerPaths.sharedRoot()} />
+//           <Route strict path={routerPaths.exampleSessionsRoot()} component={ExampleSessionsLoader} />
+//           <Redirect from={routerPaths.exampleSessionsRoot()} to={routerPaths.exampleSessionsRoot()} />
+//           <Route exact path={'/'} component={Welcome} />
+//           <Redirect to={'/'} />
+//         </Switch>
+//       </div>
+//     );
+
+//   }
+// }
+
+// export default AppRoutes;
