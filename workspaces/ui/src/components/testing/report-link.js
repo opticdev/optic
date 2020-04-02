6860aef36714ec2059e1da06714ba598341d966e
Aidan Cunniffe <acunniffe@gmail.com>
2020-04-02 08:38:18 -0400

    Revert "Restructure routing and loading of example state to debug with (#79)" (#82)
    This reverts commit fb49083d309d93fdc775f50e32bb68181337f1f3.

--- workspaces/ui/src/components/testing/report-link.js
+++ workspaces/ui/src/components/testing/report-link.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { NavLink } from '../Router';
+import { NavLink } from 'react-router-dom';
+import { useReportPath } from '../../contexts/TestingDashboardContext';
 
 export default function ReportLink(props) {
   const { captureId, ...otherProps } = props;
+  const path = useReportPath(captureId);
 
-  return <NavLink {...otherProps} to={`/testing/captures/${captureId}`} />;
+  return <NavLink {...otherProps} to={path} />;
 }
