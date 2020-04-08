a7266de1f2a9973ae02a576ace09a39ba07eb50b
Jaap Rood <jaap@jaaprood.nl>
2020-04-08 14:52:14 +0200

    Render ReportsNav in groups of active and completed captures

--- workspaces/ui/src/components/testing/reports-nav.js
+++ workspaces/ui/src/components/testing/reports-nav.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useTestingService } from '../../contexts/TestingDashboardContext';
 import Loading from '../navigation/Loading';
 import ReportLink from './report-link';
+import dateParseISO from 'date-fns/parseISO';
+import groupBy from 'lodash.groupby';
 
 export default function ReportsNavigation() {
   const { loading, result: captures } = useTestingService(
@@ -9,19 +11,43 @@ export default function ReportsNavigation() {
     []
   );
 
+  const capturesLists = useMemo(() => createCapturesLists(captures || []), [
+    captures
+  ]);
+
   if (loading) {
     return <Loading />;
   }
 
   return (
     <div>
-      <ul>
-        {captures.map((capture) => (
-          <li key={capture.captureId}>
-            <CaptureNavItem capture={capture} />
-          </li>
-        ))}
-      </ul>
+      {capturesLists.active.length > 0 && (
+        <div>
+          <h6>Active</h6>
+
+          <ul>
+            {capturesLists.active.map((activeCapture) => (
+              <li key={activeCapture.captureId}>
+                <CaptureNavItem capture={activeCapture} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {capturesLists.completed.length > 0 && (
+        <div>
+          <h6>Completed</h6>
+
+          <ul>
+            {capturesLists.completed.map((completeCapture) => (
+              <li key={completeCapture.captureId}>
+                <CaptureNavItem capture={completeCapture} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
@@ -39,4 +65,28 @@ function CaptureNavItem(props) {
       {envTag && `in ${envTag.value}`}
     </ReportLink>
   );
+}
+
+function createCapturesLists(rawCaptures) {
+  const captures = rawCaptures.map((rawCap) => {
+    const createdAt = dateParseISO(rawCap.createdAt);
+    const updatedAt = rawCap.updatedAt && dateParseISO(rawCap.updatedAt);
+    const completedAt = rawCap.completedAt && dateParseISO(rawCap.completedAt);
+    const isActive = !completedAt;
+
+    return {
+      ...rawCap,
+      isActive,
+      createdAt,
+      updatedAt,
+      completedAt
+    };
+
+  const groupedByActive = groupBy(captures, (capture) =>
+    capture.isActive ? 'active' : 'completed'
+  );
+  const active = groupedByActive['active'] || [];
+  const completed = groupedByActive['completed'] || [];
+
+  return { active, completed };
 }
