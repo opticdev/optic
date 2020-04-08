83d9e0805f90ab36b042906c40f34b4e311840b3
Jaap Rood <jaap@jaaprood.nl>
2020-04-08 17:41:43 +0200

    Define re-used as part of the Theme, so makeStyles can just rely on the theming rather than wrapped Context

--- workspaces/ui/src/theme.js
+++ workspaces/ui/src/theme.js
@@ -1,20 +1,21 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-export const primary = '#31366f'
-export const secondary = '#ea4a61'
-
-export const appTheme = createMuiTheme({
-	palette: {
-		primary:  {  main: primary },
-		secondary: { main: secondary }
-	},
-	typography: {
-		// Use the system font instead of the default Roboto font.
-		fontFamily: ['Ubuntu', 'Inter']
-	}
-});
-
-
+export const primary = '#31366f';
+export const secondary = '#ea4a61';
+export const UpdatedBlue = '#2b7bd1';
+export const UpdatedBlueBackground = 'rgba(43,123,209,0.11)';
 export const AddedGreenBackground = 'rgba(0,196,70,0.3)';
 export const ChangedYellowBackground = 'rgba(252,171,16,0.3)';
-export const RemovedRedBackground = 'rgba(248,51,60,0.3)'
+export const RemovedRedBackground = 'rgba(248,51,60,0.3)';
+
+export const appTheme = createMuiTheme({
+  palette: {
+    primary: { main: primary },
+    secondary: { main: secondary },
+    updated: { main: UpdatedBlue, background: UpdatedBlueBackground }
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: ['Ubuntu', 'Inter']
+  }
+});
