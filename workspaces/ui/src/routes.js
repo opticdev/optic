3188d8b239668dcbb3f4c43568e4b556023b4a17
Jaap Rood <jaap@jaaprood.nl>
2020-04-16 11:46:58 +0200

    Use create-react-app's env file functionality to hide the TestingDashboard feature behind a feature flag

--- workspaces/ui/src/routes.js
+++ workspaces/ui/src/routes.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import {Redirect, Switch, Route} from 'react-router-dom';
-import {useRouterPaths} from './RouterPaths';
+import { Redirect, Switch, Route } from 'react-router-dom';
+import { useRouterPaths } from './RouterPaths';
 import TestingDashboardLoader from './components/loaders/TestingDashboardLoader';
-import {CaptureManagerPage} from './components/diff/v2/CaptureManagerPage';
-import {DocsPage} from './components/docs/DocsPage';
+import { CaptureManagerPage } from './components/diff/v2/CaptureManagerPage';
+import { DocsPage } from './components/docs/DocsPage';
 
 export function ApiRoutes(props) {
   const routerPaths = useRouterPaths();
-
   return (
     <Switch>
-      <Route
-        strict
-        path={routerPaths.docsRoot}
-        component={DocsPage}
-      />
+      <Route strict path={routerPaths.docsRoot} component={DocsPage} />
       <Route
         strict
         path={routerPaths.diffsRoot}
         component={CaptureManagerPage}
       />
-      <Route
-        strict
-        path={routerPaths.testingDashboard}
-        component={TestingDashboardLoader}
-      />
-      <Redirect to={routerPaths.docsRoot}/>
+      {process.env.REACT_APP_TESTING_DASHBOARD === 'true' && (
+        <Route
+          strict
+          path={routerPaths.testingDashboard}
+          component={TestingDashboardLoader}
+        />
+      )}
+      <Redirect to={routerPaths.docsRoot} />
     </Switch>
   );
 }
