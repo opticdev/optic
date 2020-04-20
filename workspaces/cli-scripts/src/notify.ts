f52ee3c244c472a8b6ef053557739422f4d175ae
Dev Doshi <dev@useoptic.com>
2020-04-20 00:08:34 -0400

    cli scripts should be in their own workspace so they can be run in local-cli in development and production

---
+++ workspaces/cli-scripts/src/notify.ts
@@ -0,0 +1,17 @@
+import * as notifier from 'node-notifier';
+
+function run(url: string, icon: string) {
+  notifier.notify(
+    {
+      title: 'Observed Unexpected API Behavior',
+      message: 'Click here to review the diff in Optic',
+      icon,
+      timeout: 7,
+      open: url,
+      wait: true
+    }
+  );
+}
+
+const [, , url, icon] = process.argv;
+run(url, icon);
