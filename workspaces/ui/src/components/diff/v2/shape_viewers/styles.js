61e2486292ee3d262f038375d75493fef777a177
Jaap Rood <jaap@jaaprood.nl>
2020-04-20 14:31:48 +0200

    Tweak values breaking behaviour and alignment of ShapeViewer

--- workspaces/ui/src/components/diff/v2/shape_viewers/styles.js
+++ workspaces/ui/src/components/diff/v2/shape_viewers/styles.js
@@ -72,7 +72,8 @@ export const useShapeViewerStyles = makeStyles(theme => ({
     flexDirection: 'row'
   },
   left: {
-    flex: '50%',
+    flexGrow: 1,
+    flexShrink: 1,
     overflow: 'hidden',
     display: 'flex',
     paddingTop: 3,
@@ -81,14 +82,18 @@ export const useShapeViewerStyles = makeStyles(theme => ({
   },
   spacerBorder: {
     maxWidth: 1,
-    backgroundColor: '#4B5A8C'
+    backgroundColor: '#4B5A8C',
+    flexGrow: 0,
+    flexShrink: 0,
   },
   right: {
     display: 'flex',
     paddingLeft: 5,
     paddingTop: 3,
     paddingBottom: 3,
-    flex: '50%',
+    width: '35%',
+    flexGrow: 0,
+    flexShrink: 0,
     overflow: 'hidden'
   },
   typeName: {
