b8f25edba0b389831d8c03e93512d4edc510f75d
Jaap Rood <jaap@jaaprood.nl>
2020-04-28 12:58:54 +0200

    Fix the Storybook configuration by integrating it's Webpack setup with the project's webpack configuration

--- workspaces/ui/src/stories/index.js
+++ workspaces/ui/src/stories/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import {storiesOf} from '@storybook/react';
-import {mui} from './MUI';
-import {DiffHelperCard} from '../components/diff/v2/DiffHelperCard';
+import { storiesOf } from '@storybook/react';
+import { mui } from './MUI';
+// import {DiffHelperCard} from '../components/diff/v2/DiffHelperCard';
 
-storiesOf('stories')
-  .add('diff helper card', mui((() => {
-    const reportStub = {};
-    return (
-      <DiffHelperCard />
-    );
-})()))
+storiesOf('stories').add(
+  'diff helper card',
+  mui(
+    (() => {
+      return <div>Running storybook again</div>;
+      // const reportStub = {};
+      // return (
+      //   <DiffHelperCard />
+      // );
+    })()
+  )
+);
