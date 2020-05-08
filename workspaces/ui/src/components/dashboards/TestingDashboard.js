55fa59146c29627c81a6544a4c546025bb650287
Jaap Rood <jaap@jaaprood.nl>
2020-05-08 13:01:17 +0200

    Add setup and promo message for live contract testing

--- workspaces/ui/src/components/dashboards/TestingDashboard.js
+++ workspaces/ui/src/components/dashboards/TestingDashboard.js
@@ -10,7 +10,11 @@ import { stuffFromQueries } from '../../contexts/RfcContext';
 // --------------------
 import { Switch, Route, Redirect, matchPath } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
 import ReportSummary from '../testing/ReportSummary';
+import SetupLink from '../testing/SetupLink';
+import LiveTestingDiagram from '../../assets/optic-contract-testing-diagram.svg';
 
 import {
   createContext,
@@ -21,43 +25,6 @@ import {
 import ReportsNavigation from '../testing/ReportsNav';
 import Page from '../Page';
 import { useRouterPaths } from '../../RouterPaths';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexGrow: 1,
-    [theme.breakpoints.down('sm')]: {
-      flexDirection: 'column', // stack vertically on smaller screens for now
-    },
-    [theme.breakpoints.up('sm')]: {
-      flexDirection: 'row', // horizontally on larger screens
-    },
-  },
-
-  isEmpty: {},
-
-  navigationContainer: {
-    // keep then navigation fixed
-    width: '100%',
-    flexGrow: 0,
-    flexShrink: 0,
-    display: 'flex',
-
-    [theme.breakpoints.up('sm')]: {
-      width: (theme.breakpoints.values.sm / 3) * 2,
-    },
-
-    '$isEmpty &': {
-      display: 'none',
-    },
-  },
-  reportContainer: {
-    display: 'flex',
-    flexGrow: 1,
-    flexShrink: 1,
-    justifyContent: 'center',
-  },
-}));
 
 export default function TestingDashboardPage(props) {
   const { match, service } = props;
@@ -124,10 +91,31 @@ export default function TestingDashboardPage(props) {
                 </Switch>
               </div>
             ) : (
-              <div className={classes.setupInstructions}>
-                <h4>You don't have any captures yet</h4>
+              <div className={classes.setup}>
+                <div className={classes.setupInstructions}>
+                  <h3>No live captures have been found yet.</h3>
 
-                <p>You might need to set them up.</p>
+                  <p>
+                    For help on how to get started with Live Contract Testing,
+                    see <SetupLink>the setup instructions</SetupLink>.
+                  </p>
+                </div>
+
+                <Card className={classes.setupPitch}>
+                  {/* <img src={LiveTestingDiagram} /> */}
+                  <CardMedia
+                    className={classes.setupPitchDiagram}
+                    image={LiveTestingDiagram}
+                  />
+
+                  <h2>Confidence your API is working as designed</h2>
+
+                  <p>
+                    Optic's Live Contract Testing validates your API is working
+                    as designed in all your environments. Achieve 100% test
+                    coverage of the API's contract from your live traffic.
+                  </p>
+                </Card>
               </div>
             )}
           </div>
@@ -232,6 +220,101 @@ function useSpec(captureId) {
   // rather than RfcState.
   return { ...hookRest, result: spec };
 }
+
+// Styles
+// ------
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexGrow: 1,
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column', // stack vertically on smaller screens for now
+    },
+    [theme.breakpoints.up('sm')]: {
+      flexDirection: 'row', // horizontally on larger screens
+    },
+  },
+
+  isEmpty: {},
+
+  navigationContainer: {
+    // keep then navigation fixed
+    width: '100%',
+    flexGrow: 0,
+    flexShrink: 0,
+    display: 'flex',
+
+    [theme.breakpoints.up('sm')]: {
+      width: (theme.breakpoints.values.sm / 3) * 2,
+    },
+
+    '$isEmpty &': {
+      display: 'none',
+    },
+  },
+  reportContainer: {
+    display: 'flex',
+    flexGrow: 1,
+    flexShrink: 1,
+    justifyContent: 'center',
+  },
+
+  setup: {
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  setupPitch: {
+    width: '100%',
+    padding: theme.spacing(2, 3),
+    [theme.breakpoints.up('md')]: {
+      width: (theme.breakpoints.values.md / 4) * 3,
+    },
+
+    '& h2': {
+      ...theme.typography.h2,
+      fontSize: theme.typography.h4.fontSize,
+      color: theme.palette.primary.main,
+      margin: theme.spacing(4, 0, 2),
+    },
+
+    '& p': {
+      ...theme.typography.subtitle1,
+      fontWeight: theme.typography.fontWeightLight,
+    },
+  },
+
+  setupPitchDiagram: {
+    height: 250,
+    margin: theme.spacing(-2, -3, 3),
+    backgroundSize: 'cover',
+    backgroundPosition: 'top center',
+  },
+
+  setupInstructions: {
+    width: '100%',
+    padding: theme.spacing(2, 3),
+    marginBottom: theme.spacing(3),
+
+    [theme.breakpoints.up('md')]: {
+      width: (theme.breakpoints.values.md / 4) * 3,
+    },
+
+    '& h3': {
+      ...theme.typography.h4,
+      color: theme.palette.primary.main,
+    },
+
+    '& p': {
+      ...theme.typography.body1,
+      // fontSize:
+      fontWeight: theme.typography.fontWeightLight,
+    },
+  },
+}));
 
 // View models
 // -----------
