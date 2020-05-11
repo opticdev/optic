2b991a97e776e73f5630cacfee0e46a4866d2406
Jaap Rood <jaap@jaaprood.nl>
2020-05-11 15:28:19 +0200

    Extract TestingPromo marketing component from TestingDashboard

--- workspaces/ui/src/components/dashboards/TestingDashboard.js
+++ workspaces/ui/src/components/dashboards/TestingDashboard.js
@@ -10,11 +10,8 @@ import { stuffFromQueries } from '../../contexts/RfcContext';
 // --------------------
 import { Switch, Route, Redirect, matchPath } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
 import ReportSummary from '../testing/ReportSummary';
 import SetupLink from '../testing/SetupLink';
-import LiveTestingDiagram from '../../assets/optic-contract-testing-diagram.svg';
 
 import {
   createContext,
@@ -25,6 +22,7 @@ import {
 import ReportsNavigation from '../testing/ReportsNav';
 import Page from '../Page';
 import { useRouterPaths } from '../../RouterPaths';
+import TestingPromo from '../marketing/TestingPromo';
 
 export default function TestingDashboardPage(props) {
   const { match, service } = props;
@@ -101,21 +99,7 @@ export default function TestingDashboardPage(props) {
                   </p>
                 </div>
 
-                <Card className={classes.setupPitch}>
-                  {/* <img src={LiveTestingDiagram} /> */}
-                  <CardMedia
-                    className={classes.setupPitchDiagram}
-                    image={LiveTestingDiagram}
-                  />
-
-                  <h2>Confidence your API is working as designed</h2>
-
-                  <p>
-                    Optic's Live Contract Testing validates your API is working
-                    as designed in all your environments. Achieve 100% test
-                    coverage of the API's contract from your live traffic.
-                  </p>
-                </Card>
+                <TestingPromo />
               </div>
             )}
           </div>
@@ -270,33 +254,6 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.down('md')]: {
       padding: theme.spacing(0, 3),
     },
-  },
-  setupPitch: {
-    width: '100%',
-    padding: theme.spacing(2, 3),
-
-    [theme.breakpoints.up('md')]: {
-      width: (theme.breakpoints.values.md / 4) * 3,
-    },
-
-    '& h2': {
-      ...theme.typography.h2,
-      fontSize: theme.typography.h4.fontSize,
-      color: theme.palette.primary.main,
-      margin: theme.spacing(4, 0, 2),
-    },
-
-    '& p': {
-      ...theme.typography.subtitle1,
-      fontWeight: theme.typography.fontWeightLight,
-    },
-  },
-
-  setupPitchDiagram: {
-    height: 250,
-    margin: theme.spacing(-2, -3, 3),
-    backgroundSize: 'cover',
-    backgroundPosition: 'top center',
   },
 
   setupInstructions: {
