3a1063228ba0c514b366e3e1811b68210efa189a
Aidan Cunniffe <aidan@useoptic.com>
2020-05-12 13:34:48 -0400

    Revert "Revert "Merge branch 'new-publish-local-script' of https://github.com/opticdev/optic into new-publish-local-script""
    This reverts commit ed7a6547d28f3f1238c5a2575769e313ff3bd56a.

---
+++ workspaces/domain-utilities/src/coverage/index.ts
@@ -0,0 +1,11 @@
+import { StableHashableWrapper } from '@useoptic/domain';
+import * as crypto from 'crypto';
+
+function hasher(input: string) {
+  const sha1 = crypto.createHash('sha1');
+  sha1.update(input);
+  const hex = sha1.digest('hex');
+  return hex;
+}
+
+export const StableHasher = StableHashableWrapper(hasher);
