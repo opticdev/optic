e6d330264cda8e6e618d9428d2e90c9d809833b8
Dev Doshi <dev@useoptic.com>
2020-05-14 14:25:18 -0400

    domain-types should be separate from domain

--- core/build.sh
+++ core/build.sh
@@ -4,5 +4,5 @@ sbt "opticJVM/generateTypescript"
 sbt "opticJVM/test:runMain com.useoptic.types.AvroMappings"
 sbt publishLocal
 
-OUTPUT_DIRECTORY=../workspaces/domain/src/domain-types
+OUTPUT_DIRECTORY=../workspaces/domain-types/src
 cp -r ./build/ $OUTPUT_DIRECTORY
