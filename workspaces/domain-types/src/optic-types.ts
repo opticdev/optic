f21eb7797d9ce8bfafdc67aafad1af83766ebd58
Aidan Cunniffe <aidan@useoptic.com>
2020-05-20 13:52:18 -0400

    Try this

--- workspaces/domain-types/src/optic-types.ts
+++ workspaces/domain-types/src/optic-types.ts
@@ -1,49 +1,58 @@
+
 export interface IResponse {
-  statusCode: number;
-  headers: IArbitraryData;
-  body: IBody;
+  statusCode: number
+  headers: IArbitraryData
+  body: IBody
 }
+       
 
 export interface IBody {
-  contentType: string | null;
-  value: IArbitraryData;
+  contentType: (string | null)
+  value: IArbitraryData
 }
+       
 
 export interface IArbitraryData {
-  shapeHashV1Base64: string | null;
-  asJsonString: string | null;
-  asText: string | null;
+  shapeHashV1Base64: (string | null)
+  asJsonString: (string | null)
+  asText: (string | null)
 }
+       
 
 export interface IHttpInteraction {
-  uuid: string;
-  request: IRequest;
-  response: IResponse;
-  tags: IHttpInteractionTag[];
+  uuid: string
+  request: IRequest
+  response: IResponse
+  tags: IHttpInteractionTag[]
 }
+       
 
 export interface IGroupingIdentifiers {
-  agentGroupId: string;
-  captureId: string;
-  agentId: string;
-  batchId: string;
+  agentGroupId: string
+  captureId: string
+  agentId: string
+  batchId: string
 }
+       
 
 export interface IHttpInteractionTag {
-  name: string;
-  value: string;
+  name: string
+  value: string
 }
+       
 
 export interface IRequest {
-  host: string;
-  method: string;
-  path: string;
-  query: IArbitraryData;
-  headers: IArbitraryData;
-  body: IBody;
+  host: string
+  method: string
+  path: string
+  query: IArbitraryData
+  headers: IArbitraryData
+  body: IBody
 }
+       
 
 export interface ICapture {
-  groupingIdentifiers: IGroupingIdentifiers;
-  batchItems: IHttpInteraction[];
+  groupingIdentifiers: IGroupingIdentifiers
+  batchItems: IHttpInteraction[]
 }
+       
