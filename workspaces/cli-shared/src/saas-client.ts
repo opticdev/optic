d4746767509c1025fc522665a8ec9b53dcbc3a98
Jaap Rood <jaap@jaaprood.nl>
2020-05-25 16:14:13 +0200

    Let Prettier do it's formatting thing

--- workspaces/cli-shared/src/saas-client.ts
+++ workspaces/cli-shared/src/saas-client.ts
@@ -1,23 +1,31 @@
-import {JsonHttpClient} from '@useoptic/client-utilities';
+import { JsonHttpClient } from '@useoptic/client-utilities';
 
 class Client {
   private readonly defaultAdditionalHeaders: Record<string, string>;
 
   constructor(private baseUrl: string, private launchToken: string) {
     //@TODO make this actually a jwt
-    console.log({launchToken});
-    this.defaultAdditionalHeaders = {Authorization: `Bearer ${this.launchToken}`};
+    console.log({ launchToken });
+    this.defaultAdditionalHeaders = {
+      Authorization: `Bearer ${this.launchToken}`,
+    };
   }
 
   async getSpecUploadUrl() {
     const url = `${this.baseUrl}/spec-uploads`;
-    console.log({url});
-    return JsonHttpClient.postJsonWithoutBody(url, this.defaultAdditionalHeaders);
+    console.log({ url });
+    return JsonHttpClient.postJsonWithoutBody(
+      url,
+      this.defaultAdditionalHeaders
+    );
   }
 
   async getCaptureUploadUrl(agentId: string, batchId: string) {
     const url = `${this.baseUrl}/capture-uploads/agents/${agentId}/batches/${batchId}`;
-    return JsonHttpClient.postJsonWithoutBody(url, this.defaultAdditionalHeaders);
+    return JsonHttpClient.postJsonWithoutBody(
+      url,
+      this.defaultAdditionalHeaders
+    );
   }
 
   uploadCapture(uploadUrl: string, bytes: Buffer) {
@@ -29,11 +37,9 @@ class Client {
   uploadSpec(uploadUrl: string, spec: any[]) {
     const bytes = Buffer.from(JSON.stringify(spec));
     return JsonHttpClient.putBytes(uploadUrl, bytes, {
-      'content-type': 'application/json'
+      'content-type': 'application/json',
     });
   }
 }
 
-export {
-  Client
-};
+export { Client };
