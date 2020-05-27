7a1e59dbaf09cb4ed18a8222f25095770bac9c1f
Jaap Rood <jaap@jaaprood.nl>
2020-05-27 18:36:54 +0200

    Draft TestingService client with mechanism to deal with dynamic auth token

--- workspaces/ui/src/services/testing/TestingService.ts
+++ workspaces/ui/src/services/testing/TestingService.ts
@@ -1,3 +1,62 @@
 import { ITestingService } from '.';
 
-// export class TestingService implements ITestingService {}
+// TODO: implement ITestingService
+export class TestingService {
+  private authToken: string;
+  private refreshing?: Promise<unknown>;
+
+  constructor(
+    private fetchAuthToken: () => PromiseLike<Response>,
+    baseUrl: string
+  ) {}
+
+  private async callApi(
+    path: string,
+    options: RequestInit = {}
+  ): Promise<Response> {
+    const headers = new Headers(options.headers || {});
+    const { authToken } = this;
+
+    if (!authToken) {
+      await this.refreshAuth();
+    }
+
+    headers.set('Authorization', `Bearer ${this.authToken}`);
+
+    const response = await fetch(path, {
+      headers,
+    });
+
+    if (!response.ok && response.status === 401) {
+      this.refreshAuth(); // trigger refreshing of auth after an Unauthorized, but don't wait on it or retry for now
+    }
+
+    return response;
+  }
+
+  private async refreshAuth() {
+    if (!this.refreshing) {
+      // only refresh auth once, no matter how many endpoints are waiting
+      this.refreshing = refresh.call(this);
+    }
+
+    return this.refreshing;
+
+    async function refresh() {
+      const response = this.fetchAuthToken();
+
+      if (!response.ok)
+        throw new Error(
+          'Could not fetch testing credentials required to use TestingService'
+        );
+
+      const payload = await response.json();
+      if (!payload.authToken)
+        throw new Error(
+          'Could not fetch usable testing credentials to use Testing Service'
+        );
+
+      this.authToken = payload.authToken;
+    }
+  }
+}
