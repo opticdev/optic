9bed7e43f68527e9b0ec8e9f1336834ae417b62e
Aidan Cunniffe <aidan@useoptic.com>
2020-06-02 09:39:50 -0400

    Agent CLI should be less brittle since it's going into production

--- workspaces/agent-cli/src/commands/run.ts
+++ workspaces/agent-cli/src/commands/run.ts
@@ -1,25 +1,30 @@
 import { Command, flags } from '@oclif/command';
-import { SaasCaptureSaver } from '@useoptic/cli-shared';
+import {
+  fromOptic,
+  ICaptureSaver,
+  SaasCaptureSaver,
+} from '@useoptic/cli-shared';
 import { Config } from '../config';
 //@ts-ignore
 import jwtDecode from 'jwt-decode';
+//@ts-ignore
+import niceTry from 'nice-try';
 import * as uuid from 'uuid';
 import { CliTaskSession } from '@useoptic/cli-shared/build/tasks';
 import { IApiCliConfig } from '@useoptic/cli-config';
 import { AgentCliTaskRunner } from '../task-runner';
 import { ICreateCaptureResponse } from '@useoptic/saas-types';
+import { IHttpInteraction } from '@useoptic/domain-types';
 
 export default class Run extends Command {
-  static description = 'describe the command here';
-
-  static examples = [`$ optic-agent run ????`];
+  static description = 'start your API process with Optic monitoring';
 
   static flags = {
     command: flags.string({
       required: true,
-      description: 'the command to run',
+      description: 'the command to start your API',
     }),
-    masquerade: flags.string({
+    listen: flags.string({
       required: true,
       description: 'host:port Optic should start on',
     }),
@@ -34,34 +39,87 @@ export default class Run extends Command {
     const opticConfig = {
       ignoreRequests: undefined,
     };
-    const { agentToken }: ICreateCaptureResponse = JSON.parse(flags.config);
-    const decodedToken = jwtDecode(agentToken);
-    const { opticContext } = decodedToken;
-    const { orgId, agentGroupId, captureId } = opticContext;
 
-    const agentId = uuid.v4();
+    const identifiers:
+      | {
+          orgId: string;
+          agentGroupId: string;
+          captureId: string;
+          agentToken: any;
+        }
+      | undefined = niceTry(() => {
+      const { agentToken }: ICreateCaptureResponse = JSON.parse(flags.config);
+      const decodedToken = jwtDecode(agentToken);
+      const { opticContext } = decodedToken;
 
-    const persistenceManager = new SaasCaptureSaver({
-      orgId,
-      agentGroupId,
-      agentId,
-      baseUrl: Config.apiBaseUrl,
-      launchTokenString: agentToken,
-      captureId,
+      return {
+        orgId: opticContext.orgId,
+        agentGroupId: opticContext.agentGroupId,
+        captureId: opticContext.captureId,
+        agentToken,
+      };
     });
 
-    const runner = new AgentCliTaskRunner(persistenceManager);
-    const cliTaskSession = new CliTaskSession(runner);
-    const config: IApiCliConfig = {
-      name: '',
-      ignoreRequests: opticConfig.ignoreRequests,
-      tasks: {
-        start: {
-          command: flags.command,
-          baseUrl: flags.masquerade,
+    //Optic has valid config
+    if (identifiers) {
+      this.log(fromOptic('Starting your API with Optic monitoring...'));
+      const agentId = uuid.v4();
+      const persistenceManager = new SaasCaptureSaver({
+        orgId: identifiers!.orgId,
+        agentGroupId: identifiers!.agentGroupId,
+        agentId,
+        baseUrl: Config.apiBaseUrl,
+        launchTokenString: identifiers!.agentToken,
+        captureId: identifiers!.captureId,
+      });
+
+      const runner = new AgentCliTaskRunner(persistenceManager);
+      const cliTaskSession = new CliTaskSession(runner);
+      const config: IApiCliConfig = {
+        name: '',
+        ignoreRequests: opticConfig.ignoreRequests,
+        tasks: {
+          start: {
+            command: flags.command,
+            baseUrl: flags.listen,
+          },
         },
-      },
-    };
-    await cliTaskSession.start(this, config, 'start');
+      };
+      await cliTaskSession.start(this, config, 'start');
+    } else {
+      this.log(
+        fromOptic(
+          'Optic monitoring token is missing. Starting your API normally'
+        )
+      );
+
+      class DoNothingCapture implements ICaptureSaver {
+        cleanup(): Promise<void> {
+          return Promise.resolve(undefined);
+        }
+
+        init(): Promise<void> {
+          return Promise.resolve(undefined);
+        }
+
+        save(sample: IHttpInteraction): Promise<void> {
+          return Promise.resolve(undefined);
+        }
+      }
+
+      const runner = new AgentCliTaskRunner(new DoNothingCapture());
+      const cliTaskSession = new CliTaskSession(runner);
+      const config: IApiCliConfig = {
+        name: '',
+        ignoreRequests: opticConfig.ignoreRequests,
+        tasks: {
+          start: {
+            command: flags.command,
+            baseUrl: flags.listen,
+          },
+        },
+      };
+      await cliTaskSession.start(this, config, 'start');
+    }
   }
 }
