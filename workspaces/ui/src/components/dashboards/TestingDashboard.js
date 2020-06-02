39a53c7cd41fc6f60747fe66fe3a804284955fd5
Jaap Rood <jaap@jaaprood.nl>
2020-06-02 12:28:07 +0200

    Remove debug code

--- workspaces/ui/src/components/dashboards/TestingDashboard.js
+++ workspaces/ui/src/components/dashboards/TestingDashboard.js
@@ -77,7 +77,7 @@ export default function TestingDashboardPage(props) {
               />
             </div>
 
-            {hasCaptures && false ? ( // TODO: remove debug disable
+            {hasCaptures ? (
               <div className={classes.reportContainer}>
                 <Switch>
                   <Route
