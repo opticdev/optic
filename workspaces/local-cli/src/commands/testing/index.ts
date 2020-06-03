468aa207f5b857f31b254ac136e6afb581bcae09
Jaap Rood <jaap@jaaprood.nl>
2020-06-03 17:16:34 +0200

    Hide testing commands for now

--- workspaces/local-cli/src/commands/testing/index.ts
+++ workspaces/local-cli/src/commands/testing/index.ts
@@ -1,7 +1,9 @@
 import Command from '@oclif/command';
+import Config from '../../config';
 
 export default class Testing extends Command {
   static description = 'Manage Live Contract Testing for your API';
+  static hidden: boolean = Config.hiddenFeatures.includes('testing');
 
   async run() {
     this._help();
