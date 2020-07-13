3eada208051e41b4ed0a2c87ca3783ae90ce9848
Ronak Shah <ronakshahultimate@gmail.com>
2020-07-13 16:49:48 -0700

    stuck

--- .github/deployDebian/entrypoint.sh
+++ .github/deployDebian/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Installing Essentials"
+echo $INPUT_NPM_PACKAGE_NAME
 apt update
 apt install -y git default-jre sudo apt-utils apt-transport-https
 npm install -g @oclif/dev-cli
@@ -8,9 +9,10 @@ echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.li
 curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
 apt update
 apt install -y sbt
+
 echo "Cloning Optic"
 cd /tmp
-git clone https://github.com/opticdev/optic
+git clone https://github.com/trulyronak/optic
 cd optic
 echo "Checking out specific branch (eventually this will just be release)"
 git checkout feature/debian-release-flow
@@ -27,6 +29,7 @@ gem install deb-s3
 cd /tmp/optic/workspaces/local-cli/dist/deb/
 export PATH_TO_DEB="/tmp/optic/workspaces/local-cli/dist/deb/api_$(npm view $INPUT_NPM_PACKAGE_NAME version)-1_amd64.deb"
 echo $PATH_TO_DEB
-ls $PATH_TO_DEB
-du $PATH_TO_DEB
-deb-s3 upload -e --access-key-id=$INPUT_AWS_ACCESS_KEY_ID --secret-access-key=$INPUT_AWS_SECRET_ACCESS_KEY --bucket $INPUT_BUCKET_NAME $INPUT_PACKAGE_NAME --preserve-versions $PATH_TO_DEB
+# ls $PATH_TO_DEB
+# du $PATH_TO_DEB
+/bin/bash
+# deb-s3 upload -e --access-key-id=$INPUT_AWS_ACCESS_KEY_ID --secret-access-key=$INPUT_AWS_SECRET_ACCESS_KEY --bucket $INPUT_BUCKET_NAME $INPUT_PACKAGE_NAME --preserve-versions $PATH_TO_DEB
