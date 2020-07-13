4122d7f7a3f4db25bfda68e4f9031896bb1b15f0
Ronak Shah <ronakshahultimate@gmail.com>
2020-07-13 15:46:19 -0700

    debugging

--- .github/deployDebian/entrypoint.sh
+++ .github/deployDebian/entrypoint.sh
@@ -14,8 +14,6 @@ git clone https://github.com/opticdev/optic
 cd optic
 echo "Checking out specific branch (eventually this will just be release)"
 git checkout feature/debian-release-flow
-export PATH_TO_DEB="/tmp/optic/workspaces/local-cli/dist/deb/api_$(npm view $INPUT_NPM_PACKAGE_NAME version)-1_amd64.deb"
-echo $PATH_TO_DEB
 echo "Building Optic"
 source sourceme.sh
 optic_build
@@ -27,5 +25,8 @@ echo "Installing Ruby"
 apt install ruby-full -y
 gem install deb-s3
 cd /tmp/optic/workspaces/local-cli/dist/deb/
-ls
+export PATH_TO_DEB="/tmp/optic/workspaces/local-cli/dist/deb/api_$(npm view $INPUT_NPM_PACKAGE_NAME version)-1_amd64.deb"
+echo $PATH_TO_DEB
+ls $PATH_TO_DEB
+du $PATH_TO_DEB
 deb-s3 upload -e --access-key-id=$INPUT_AWS_ACCESS_KEY_ID --secret-access-key=$INPUT_AWS_SECRET_ACCESS_KEY --bucket $INPUT_BUCKET_NAME $INPUT_PACKAGE_NAME --preserve-versions $PATH_TO_DEB
