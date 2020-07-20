ba6c87c55a8892b8796a5f0e7fcb588bbff1b973
Aidan Cunniffe <aidan@useoptic.com>
2020-07-20 11:28:16 -0400

    reverted

--- workspaces/ui/src/components/diff/SimulatedCommandContext.js
+++ workspaces/ui/src/components/diff/SimulatedCommandContext.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import {InitialRfcCommandsStore} from '../../contexts/InitialRfcCommandsContext';
-import {LocalDiffRfcStore} from '../../contexts/RfcContext';
-import {commandsToJson} from '@useoptic/domain';
-import {withSpecServiceContext} from '../../contexts/SpecServiceContext';
+import { InitialRfcCommandsStore } from '../../contexts/InitialRfcCommandsContext';
+import { LocalDiffRfcStore } from '../../contexts/RfcContext';
+import { commandsToJson } from '@useoptic/domain-utilities';
+import { withSpecServiceContext } from '../../contexts/SpecServiceContext';
 import compose from 'lodash.compose';
 import sha1 from 'node-sha1';
 
 class SimulatedCommandContext extends React.Component {
   render() {
-    const {rfcId, eventStore, commands, shouldSimulate, specService} = this.props;
+    const {
+      rfcId,
+      eventStore,
+      commands,
+      shouldSimulate,
+      specService,
+    } = this.props;
     const initialEventStore = eventStore.getCopy(rfcId);
     const initialEventsString = eventStore.serializeEvents(rfcId);
-    const initialCommandsString = shouldSimulate ? JSON.stringify(commandsToJson(commands)) : null;
+    const initialCommandsString = shouldSimulate
+      ? JSON.stringify(commandsToJson(commands))
+      : null;
     const hash = sha1(`${initialEventsString}+${initialCommandsString}`);
     return (
       <InitialRfcCommandsStore
         instance="the one in simulated command context"
         rfcId={rfcId}
         initialEventsString={initialEventsString}
-        initialCommandsString={initialCommandsString}>
+        initialCommandsString={initialCommandsString}
+      >
         <LocalDiffRfcStore
           specService={specService}
           initialEventStore={initialEventStore}
