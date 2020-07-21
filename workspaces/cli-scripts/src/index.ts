8fa73585df7a6667f1c073826d301187ce53e1b1
Aidan Cunniffe <aidan@useoptic.com>
2020-07-21 09:21:23 -0400

    fix node popup on windows

--- workspaces/cli-scripts/src/index.ts
+++ workspaces/cli-scripts/src/index.ts
@@ -19,7 +19,10 @@ export function runManagedScript(modulePath: string, ...args: string[]) {
   const isDebuggingEnabled =
     process.env.OPTIC_DAEMON_ENABLE_DEBUGGING === 'yes';
   // const execArgv = isDebuggingEnabled ? ['--inspect=63694'] : []; // not in spawn
-  const child = cp.spawn(modulePath, args, { windowsHide: true });
+  const child = cp.spawn(process.argv0, [modulePath, ...args], {
+    windowsHide: true,
+    stdio: ['ipc'],
+  });
   return child;
 }
 
