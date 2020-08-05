e319cdfaa0ad5e44c1688ecec95c2fb21b694837
Jaap Rood <jaap@jaaprood.nl>
2020-08-05 12:22:58 +0200

    Implement the rendering of rows field names and values

--- workspaces/ui/src/components/diff/v2/shape_viewers/ShapeViewer.js
+++ workspaces/ui/src/components/diff/v2/shape_viewers/ShapeViewer.js
@@ -1,7 +1,7 @@
 import React, { useMemo } from 'react';
 import classNames from 'classnames';
 import { makeStyles } from '@material-ui/core/styles';
-import { useShapeViewerStyles } from './styles';
+import { useColor, useShapeViewerStyles, SymbolColor } from './styles';
 
 export default function ShapeViewer({ shape }) {
   const generalClasses = useShapeViewerStyles();
@@ -21,9 +21,10 @@ export default function ShapeViewer({ shape }) {
       <Row
         indent={2}
         fieldName="circuitName"
-        type="string"
         fieldValue="Albert Park Grand Prix Circuit"
+        type="string"
       />
+      <Row indent={2} fieldName="circuitTimeZone" missing={true} />
       <Row indent={2} fieldName="Location" type="object_open" />
       <Row
         indent={3}
@@ -39,8 +40,11 @@ export default function ShapeViewer({ shape }) {
 }
 
 export function Row(props) {
+  const classes = useStyles();
   const generalClasses = useShapeViewerStyles();
-  const { exampleOnly, onLeftClick } = props;
+  const { indent, fieldName, type, fieldValue, onLeftClick, missing } = props;
+
+  const indentPadding = ' '.repeat(indent * 2);
 
   return (
     <div
@@ -50,14 +54,149 @@ export function Row(props) {
       })}
     >
       <div className={generalClasses.left} onClick={onLeftClick}>
-        {props.type}
+        <div className={classes.rowContent}>
+          {indentPadding}
+          <RowFieldName type={type} name={fieldName} missing={!!missing} />
+          {!missing && <RowValue type={type} value={fieldValue} />}
+        </div>
       </div>
     </div>
   );
 }
 Row.displayName = 'ShapeViewer/Row';
 
-const useStyles = makeStyles((theme) => ({}));
+function RowValue({ type, value }) {
+  const generalClasses = useShapeViewerStyles();
+  const classes = useStyles();
+
+  if (type === 'null') {
+    return (
+      <span
+        className={classNames(generalClasses.symbols, classes.symbolContent)}
+      >
+        {'null'}
+      </span>
+    );
+  }
+
+  if (type === 'array_open') {
+    return (
+      <span
+        className={classNames(generalClasses.symbols, classes.symbolContent)}
+      >
+        {'['}
+      </span>
+    );
+  }
+
+  if (type === 'array_close') {
+    return (
+      <span
+        className={classNames(generalClasses.symbols, classes.symbolContent)}
+      >
+        {']'}
+      </span>
+    );
+  }
+
+  if (type === 'object_open') {
+    return (
+      <span
+        className={classNames(generalClasses.symbols, classes.symbolContent)}
+      >
+        {'{'}
+      </span>
+    );
+  }
+
+  if (type === 'object_close') {
+    return (
+      <span
+        className={classNames(generalClasses.symbols, classes.symbolContent)}
+      >
+        {'}'}
+      </span>
+    );
+  }
+
+  if (type === 'string') {
+    return <span className={classes.stringContent}>"{value}"</span>;
+  }
+
+  if (type === 'boolean') {
+    return <span>{value ? 'true' : 'false'}</span>;
+  }
+
+  if (type === 'number') {
+    return <span>{value}</span>;
+  }
+
+  throw new Error(`Cannot render RowValue for type '${type}'`);
+}
+RowValue.displayName = 'ShapeViewer/RowValue';
+
+function RowFieldName({ type, name, missing }) {
+  const classes = useStyles();
+  if (!name) return null;
+  return (
+    <span
+      className={classNames(classes.fieldName, {
+        [classes.isMissing]: !!missing,
+      })}
+    >
+      {name}:{' '}
+    </span>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  rowContent: {
+    fontSize: 12,
+    fontFamily: "'Source Code Pro', monospace",
+    whiteSpace: 'pre',
+    color: SymbolColor,
+  },
+
+  booleanContent: {
+    fontWeight: 600,
+    fontFamily: "'Source Code Pro', monospace",
+    color: useColor.BooleanColor,
+  },
+
+  symbolContent: {
+    color: SymbolColor,
+  },
+
+  numberContent: {
+    fontWeight: 600,
+    fontFamily: "'Source Code Pro', monospace",
+    color: useColor.NumberColor,
+  },
+
+  stringContent: {
+    fontWeight: 600,
+    whiteSpace: 'pre-line',
+    wordBreak: 'break-all',
+    overflowWrap: 'break-word',
+    fontFamily: "'Source Code Pro', monospace",
+    color: useColor.StringColor,
+  },
+
+  fieldName: {
+    fontWeight: 600,
+    color: '#cfcfcf',
+    fontSize: 12,
+    fontFamily: "'Source Code Pro', monospace",
+
+    opacity: 1,
+
+    '&$isMissing': {
+      opacity: 0.4,
+    },
+  },
+
+  isMissing: {},
+}));
 
 function createRows(shape) {}
 
