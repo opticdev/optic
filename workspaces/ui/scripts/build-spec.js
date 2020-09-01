22261aaa5c2ba70ac79c6d0e4d0459a1367b8aaa
Ronak Shah <ronakshahultimate@gmail.com>
2020-09-01 10:57:23 -0700

    cleaned up spec viewer page

--- workspaces/ui/scripts/build-spec.js
+++ workspaces/ui/scripts/build-spec.js
@@ -3,5 +3,6 @@
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 process.env.REACT_APP_TESTING_DASHBOARD = 'false';
+process.env.REACT_APP_DONT_SHOW_DIFF = 'true';
 process.env.CUSTOM_INDEX_JS = 'src/specIndex';
 require('./build');
--- workspaces/ui/scripts/start-local-spec.js
+++ workspaces/ui/scripts/start-local-spec.js
@@ -4,5 +4,8 @@
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 process.env.REACT_APP_ENABLE_ANALYTICS = 'no';
+process.env.REACT_APP_TESTING_DASHBOARD = 'false';
+process.env.REACT_APP_DONT_SHOW_DIFF = 'true';
 process.env.CUSTOM_INDEX_JS = 'src/specIndex';
+process.env.REACT_APP_API_URL = 'https://staging.infra.opticdev.com';
 require('./start');
