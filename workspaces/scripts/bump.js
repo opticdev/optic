79e6403161e6dd0396099aa36c7c5ef28548f119
Ronak Shah <ronakshahultimate@gmail.com>
2020-09-15 11:36:35 -0700

    updated bump.js to enable silent logging mode

--- workspaces/scripts/bump.js
+++ workspaces/scripts/bump.js
@@ -3,10 +3,11 @@ const path = require('path');
 const fs = require('fs-extra');
 
 async function main(targetVersion) {
+  const silentMode = JSON.parse(process.env.npm_config_argv).original.includes('--silent');
   const packageJson = await fs.readJson('./package.json');
   const { workspaces } = packageJson;
   if ([undefined, "minor", "major", "patch"].includes(targetVersion)) {
-    bumpAllPackages(targetVersion);
+    bumpAllPackages(targetVersion, silentMode);
     return;
   } else {
     console.log(`setting workspace versions to ${targetVersion}`);
@@ -55,12 +56,16 @@ async function main(targetVersion) {
 }
 
 // this is called if there is no specified target version
-async function bumpAllPackages(type = "patch") {
+async function bumpAllPackages(type = "patch", silentMode) {
+  let log = console.log;
+  if (silentMode) {
+    console.log = () => {} // disable logging
+  }
   console.log(`performing a ${type} bump`)
   const packageJson = await fs.readJson('./package.json');
   const { workspaces } = packageJson;
   const versions = {}
-
+  let version = ""
   console.log(workspaces.map((x) => ` - ${x}\n`).join(''));
   const tasks = workspaces.map((workspace) => {
     const task = new Promise(async (resolve, reject) => {
@@ -71,7 +76,7 @@ async function bumpAllPackages(type = "patch") {
         targetPackage.version = bumpVersion(targetPackage.version, type);
         console.log(`bumping ${old} to ${targetPackage.version}`)
         versions[targetPackage.name] = targetPackage.version;
-
+        version = targetPackage.version;
         resolve({
           workspace,
           package: targetPackage,
@@ -83,7 +88,7 @@ async function bumpAllPackages(type = "patch") {
     return task;
   });
   const results = await Promise.all(tasks);
-  
+  log(version);
   const packageNames = results.map((result) => result.package.name);
   console.log(packageNames.map((x) => ` - ${x}\n`).join(''));
   const updateVersionTasks = results.map((result) => {
