c065b50e6536e9a0d227a106cf6f7a2dc6fa1b9d
Jaap Rood <jaap@jaaprood.nl>
2020-09-16 12:15:36 +0200

    Publish @useoptic/diff-engine regardless of whether it's functional, as other production packages rely on it

--- sourceme.sh
+++ sourceme.sh
@@ -3,7 +3,6 @@
 export OPTIC_SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 echo "Optic development scripts will run from $OPTIC_SRC_DIR"
 export OPTIC_DEBUG_ENV_FILE="$OPTIC_SRC_DIR/.env"
-export OPTIC_SKIP_CSV="@useoptic/diff-engine" #prevent diff-engine from being published for now
 
 alias apidev="OPTIC_DAEMON_ENABLE_DEBUGGING=yes OPTIC_DEVELOPMENT=yes OPTIC_UI_HOST=http://localhost:3000 OPTIC_AUTH_UI_HOST=http://localhost:4005 $OPTIC_SRC_DIR/workspaces/local-cli/bin/run"
 alias apistage="OPTIC_DAEMON_ENABLE_DEBUGGING=yes $OPTIC_SRC_DIR/workspaces/local-cli/bin/run"
