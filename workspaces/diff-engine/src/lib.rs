9ccf2f85db1b7235bc3e590f93b69c58f808133e
Jaap Rood <jaap@jaaprood.nl>
2020-09-17 16:56:24 +0200

    Add shape hash support

--- workspaces/diff-engine/src/lib.rs
+++ workspaces/diff-engine/src/lib.rs
@@ -3,6 +3,7 @@
 mod events;
 mod interactions;
 mod projections;
+mod protos;
 mod queries;
 mod shapes;
 mod state;
@@ -13,6 +14,7 @@ pub use events::{HttpInteraction, SpecEvent};
 pub use interactions::diff as diff_interaction;
 pub use interactions::result::InteractionDiffResult;
 pub use projections::{EndpointProjection, ShapeProjection, SpecProjection};
+pub use protos::shapehash;
 pub use shapes::diff as diff_shape;
 
 pub mod errors {
