a8e9999520e524a221bb4bea11d57efbd323c114
Dev Doshi <dev@useoptic.com>
2020-11-23 13:37:35 -0500

    experiment: interactions buffer size

--- workspaces/diff-engine/src/streams/http_interaction.rs
+++ workspaces/diff-engine/src/streams/http_interaction.rs
@@ -4,7 +4,7 @@ pub fn json_lines<R>(source: R) -> Lines<BufReader<R>>
 where
   R: AsyncRead,
 {
-  // 10 megabytes of capacity, to deal with unbound nature of request and response bodies
-  let reader = BufReader::with_capacity(10 * 1024 * 1024, source);
+  // 8kb of capacity
+  let reader = BufReader::with_capacity(8 * 1024, source);
   reader.lines()
 }
