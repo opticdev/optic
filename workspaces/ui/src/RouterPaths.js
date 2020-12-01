ccfb0006a5988b6da430ba2232f1ce5adeddb1f2
Aidan Cunniffe <aidan@useoptic.com>
2020-12-01 07:58:34 -0500

    dashboard page

--- workspaces/ui/src/RouterPaths.js
+++ workspaces/ui/src/RouterPaths.js
@@ -1,30 +1,29 @@
-import {useBaseUrl} from './contexts/BaseUrlContext';
+import { useBaseUrl } from './contexts/BaseUrlContext';
 
 const routerPaths = {
   testingDashboard: (base = '') => `${base}/testing`,
   testingCapture: (base = '') => `${base}/testing/captures/:captureId`,
-  testingEndpointDetails: (base = '') => `${base}/testing/captures/:captureId/endpoints/:endpointId`,
+  testingEndpointDetails: (base = '') =>
+    `${base}/testing/captures/:captureId/endpoints/:endpointId`,
   docsRoot: (base = '') => `${base}/documentation`,
-  requestDocsRoot: (base = '') => `${routerPaths.docsRoot(base)}/paths/:pathId/methods/:method`,
+  dashboardRoot: (base = '') => `${base}/dashboard`,
+  requestDocsRoot: (base = '') =>
+    `${routerPaths.docsRoot(base)}/paths/:pathId/methods/:method`,
   diffsRoot: (base = '') => `${base}/diffs`,
   captureRoot: (base = '') => `${routerPaths.diffsRoot(base)}/:captureId`,
-  captureRequestDiffsRoot: (base = '') => `${routerPaths.captureRoot(base)}/paths/:pathId/methods/:method`,
+  captureRequestDiffsRoot: (base = '') =>
+    `${routerPaths.captureRoot(base)}/paths/:pathId/methods/:method`,
 };
 
 export function useRouterPaths() {
   const baseUrl = useBaseUrl();
 
-  return Object
-    .entries(routerPaths)
-    .reduce(
-      (routesByName, entry) => {
-        const [routeName, route] = entry;
-        if (typeof route === 'function') {
-          routesByName[routeName] = route(baseUrl);
-        }
+  return Object.entries(routerPaths).reduce((routesByName, entry) => {
+    const [routeName, route] = entry;
+    if (typeof route === 'function') {
+      routesByName[routeName] = route(baseUrl);
+    }
 
-        return routesByName;
-      },
-      {}
-    );
+    return routesByName;
+  }, {});
 }
--- workspaces/ui/src/routes.js
+++ workspaces/ui/src/routes.js
@@ -4,6 +4,7 @@ import { useRouterPaths } from './RouterPaths';
 import { CaptureManagerPage } from './components/diff/v2/CaptureManagerPage';
 import { DocsPage } from './components/docs/DocsPage';
 import Loading from './components/navigation/Loading';
+import { ApiPage } from './components/api-page/ApiPage';
 
 const TestingDashboardLoader = React.lazy(() =>
   import('./components/loaders/TestingDashboardLoader')
@@ -15,11 +16,14 @@ const TestindDashboardTeaserPage = React.lazy(() =>
 
 export function ApiRoutes(props) {
   const routerPaths = useRouterPaths();
-  const defaultRoute = props.getDefaultRoute ? props.getDefaultRoute(routerPaths) : routerPaths.docsRoot
+  const defaultRoute = props.getDefaultRoute
+    ? props.getDefaultRoute(routerPaths)
+    : routerPaths.docsRoot;
 
   return (
     <Suspense fallback={<Loading />}>
       <Switch>
+        <Route strict path={routerPaths.dashboardRoot} component={ApiPage} />
         <Route strict path={routerPaths.docsRoot} component={DocsPage} />
         <Route
           strict
