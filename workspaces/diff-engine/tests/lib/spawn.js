716acdfd100ad9a5f19c98cf54a4059380efa1fc
Jaap Rood <jaap@jaaprood.nl>
2020-12-01 16:51:11 +0100

    Verify that non-zero exits of DiffEngine results in DiffEngineError being propagated

--- workspaces/diff-engine/tests/lib/spawn.js
+++ workspaces/diff-engine/tests/lib/spawn.js
@@ -2,6 +2,7 @@ const Tap = require('tap');
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
+const { DiffEngineError } = require('../../lib');
 
 Tap.test('diff-engine.spawn', async (test) => {
   await test.test('can diff a stream of interactions', async (t) => {
@@ -40,6 +41,40 @@ Tap.test('diff-engine.spawn', async (test) => {
 
     t.matchSnapshot(diffs, 'generated diffs');
   });
+
+  await test.test(
+    'will reject result with errors for diff-engine process faults',
+    async (t) => {
+      const interactions = fs.createReadStream(
+        path.join(
+          __dirname,
+          '..',
+          'fixtures',
+          'ergast-captures',
+          'ergast-simulated-traffic.jsonl'
+        )
+      );
+
+      const DiffEngine = require('../../lib');
+
+      const diff = DiffEngine.spawn({
+        specPath: path.join(__dirname, 'spawn.js'), // not a spec file
+      });
+
+      interactions.pipe(diff.input);
+      let outputLines = bufferLines(diff.output);
+      let errorLines = bufferLines(diff.error);
+
+      let engineError;
+      try {
+        await diff.result;
+      } catch (err) {
+        engineError = err;
+      }
+
+      t.type(engineError, DiffEngineError);
+    }
+  );
 });
 
 async function bufferLines(stream) {
