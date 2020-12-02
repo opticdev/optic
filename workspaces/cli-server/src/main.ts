988822eb38b54c03699e6ee9573f5aae4407adbb
Jaap Rood <jaap@jaaprood.nl>
2020-12-02 13:25:37 +0100

    Create CLI config for cli-server and use that to drive how Sentry is enabled

--- workspaces/cli-server/src/main.ts
+++ workspaces/cli-server/src/main.ts
@@ -1,9 +1,10 @@
 import { CliDaemon } from './daemon';
 import fs from 'fs-extra';
-import { userDebugLogger } from '@useoptic/cli-shared';
-import { getSentryWrapper } from './sentry';
+import { isEnvTrue, userDebugLogger } from '@useoptic/cli-shared';
 import dotenv from 'dotenv';
 import path from 'path';
+import Config from './config';
+import * as Errors from './errors';
 
 const envPath =
   process.env.OPTIC_DEBUG_ENV_FILE || path.join(__dirname, '..', '.env');
@@ -12,8 +13,10 @@ dotenv.config({
   path: envPath,
 });
 
-const sentry = getSentryWrapper();
-sentry.init();
+if (Config.errors.sentry) {
+  Errors.trackWithSentry(Config.errors.sentry);
+  console.log('Remote error tracking with Sentry enabled');
+}
 
 console.log('starting daemon', process.argv, process.env.DEBUG);
 console.log(process.cwd(), __dirname, __filename);
