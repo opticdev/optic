90316051d86afe653c840cd36b7b044a182b0060
Dev Doshi <dev@useoptic.com>
2020-12-07 11:01:03 -0500

    daemon should retry acquiring lock when the lock is stale. per proper-lockfile's documentation: stale: Duration in milliseconds in which the lock is considered stale, defaults to 10000 (minimum value is 5000) update: The interval in milliseconds in which the lockfile's mtime will be updated, defaults to stale/2 (minimum value is 1000, maximum value is stale/2)
    So, when a daemon terminates non-gracefully, there's a window in which a new daemon will think the lock is still being held by the old daemon and will fail to acquire it.

--- workspaces/local-cli/src/commands/daemon/start.ts
+++ workspaces/local-cli/src/commands/daemon/start.ts
@@ -2,12 +2,18 @@ import { Command } from '@oclif/command';
 import { ensureDaemonStarted } from '@useoptic/cli-server';
 import { lockFilePath } from '../../shared/paths';
 import { Config } from '../../config';
+import { cleanupAndExit } from '@useoptic/cli-shared';
 export default class DaemonStop extends Command {
   static description = 'ensures the Optic daemon has been started';
   static hidden: boolean = true;
 
   async run() {
-    await ensureDaemonStarted(lockFilePath, Config.apiBaseUrl);
-    this.log('Done!');
+    try {
+      await ensureDaemonStarted(lockFilePath, Config.apiBaseUrl);
+      this.log('Done!');
+      cleanupAndExit();
+    } catch (e) {
+      this.error(e);
+    }
   }
 }
