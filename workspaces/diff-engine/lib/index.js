52cdba970e03466a567c87a502798c2f07073f20
Jaap Rood <jaap@jaaprood.nl>
2021-01-08 13:21:05 +0100

    Verify with test that diff engine errors that occur in worker threads are not propagated

--- workspaces/diff-engine/lib/index.js
+++ workspaces/diff-engine/lib/index.js
@@ -61,7 +61,9 @@ function spawn({ specPath }) {
 
   // get a clean result promise, so we stay in control of the exact API we're exposing
   const result = diffProcess.then(
-    (childResult) => {},
+    (childResult) => {
+      console.log('spawned diff process completed without issue', childResult);
+    },
     (childResult) => {
       throw new DiffEngineError(childResult);
     }
