e25946ca82900e283e89fc155b24a06f04191490
devdoshi <devdoshi@users.noreply.github.com>
2021-01-14 13:52:02 -0500

    Feature/capture diffing should use wasm (#506)
    * capture diffing should use wasm domain logic for diffing
    Coverage should only run when requested, but should still use scalajs domain logic
    
    * capture diffing should be enabled by default but controlled by flag
    
    * options should be passed through

--- workspaces/local-cli/src/shared/local-cli-task-runner.ts
+++ workspaces/local-cli/src/shared/local-cli-task-runner.ts
@@ -51,9 +51,15 @@ export const runCommandFlags = {
     default: false,
     required: false,
   }),
+  'collect-diffs': flags.boolean({
+    char: 'd',
+    default: true,
+    required: false,
+  }),
 };
 interface LocalCliTaskFlags {
   'collect-coverage'?: boolean;
+  'collect-diffs'?: boolean;
 }
 
 export async function LocalTaskSessionWrapper(
@@ -74,7 +80,10 @@ export async function LocalTaskSessionWrapper(
 
   const { paths, config } = await loadPathsAndConfig(cli);
   const captureId = uuid.v4();
-  const runner = new LocalCliTaskRunner(captureId, paths);
+  const runner = new LocalCliTaskRunner(captureId, paths, {
+    shouldCollectCoverage: flags['collect-coverage'] !== false,
+    shouldCollectDiffs: flags['collect-diffs'] !== false,
+  });
   const session = new CliTaskSession(runner);
 
   const task = config.tasks[taskName];
@@ -114,7 +123,11 @@ export async function LocalTaskSessionWrapper(
 }
 
 export class LocalCliTaskRunner implements IOpticTaskRunner {
-  constructor(private captureId: string, private paths: IPathMapping) {}
+  constructor(
+    private captureId: string,
+    private paths: IPathMapping,
+    private options: { shouldCollectCoverage: boolean, shouldCollectDiffs: boolean }
+  ) {}
 
   async run(
     cli: Command,
@@ -192,6 +205,8 @@ ${blockers.map((x) => `[pid ${x.pid}]: ${x.cmd}`).join('\n')}
       {
         captureBaseDirectory: capturesPath,
         captureId,
+        shouldCollectCoverage: this.options.shouldCollectCoverage,
+        shouldCollectDiffs: this.options.shouldCollectDiffs
       },
       config,
       specServiceClient
