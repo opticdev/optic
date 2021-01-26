1e32457372d5d6d28f837d0ab4ef40ce8d4c81b8
lou <lou@useoptic.com>
2021-01-26 10:00:02 -0500

    update-notifier: add

--- workspaces/local-cli/src/index.ts
+++ workspaces/local-cli/src/index.ts
@@ -1,6 +1,17 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+ 
+updateNotifier({pkg}).notify();
+const notifier = updateNotifier({
+  pkg,
+  updateCheckInterval: 1000 
+});
+
+console.log(notifier.fetchInfo());
+
 const envPath =
   process.env.OPTIC_DEBUG_ENV_FILE || path.join(__dirname, '..', '.env');
 
