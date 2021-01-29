e08c499782be0ee722b7758e2df67e127526388f
Dev Doshi <dev@useoptic.com>
2021-01-29 13:59:33 -0500

    file reference should match renamed file

--- workspaces/ui/src/demoIndex.js
+++ workspaces/ui/src/demoIndex.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import DemoTopLevelRoutes from './entrypoints/demoEntrypoints';
+import DemoTopLevelRoutes from './entrypoints/demo-sessions';
 
 global.opticDebug = {};
 
-ReactDOM.render(<App topLevelRoutes={DemoTopLevelRoutes} />, document.getElementById('root'));
+ReactDOM.render(
+  <App topLevelRoutes={DemoTopLevelRoutes} />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
