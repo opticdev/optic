a06f16574510887f221c1eff648355e0531c9c50
Jaap van Hardeveld <jaap@jaaprood.nl>
2021-02-02 17:06:09 +0100

    Use enum to encode different types of SpecChunkEvents so we can handle them exhaustively

--- workspaces/diff-engine/src/events/spec_chunk.rs
+++ workspaces/diff-engine/src/events/spec_chunk.rs
@@ -3,43 +3,90 @@ use cqrs_core::Event;
 use serde_json;
 
 #[derive(Debug)]
-pub struct SpecChunkEvent {
-  pub id: Option<String>,
-  pub name: String,
-  pub is_root: bool,
-  pub parent_id: Option<String>,
-  pub events: Vec<SpecEvent>,
+pub enum SpecChunkEvent {
+  Root(RootChunkEvent),
+  Batch(BatchChunkEvent),
+  Unknown(UnknownChunkEvent),
+}
+
+#[derive(Debug)]
+pub struct RootChunkEvent {
+  id: String,
+  name: String,
+  events: Vec<SpecEvent>,
+}
+
+#[derive(Debug)]
+pub struct BatchChunkEvent {
+  id: String,
+  name: String,
+  parent_id: String,
+  events: Vec<SpecEvent>,
+}
+
+#[derive(Debug)]
+pub struct UnknownChunkEvent {
+  name: String,
+  events: Vec<SpecEvent>,
 }
 
 impl Event for SpecChunkEvent {
   fn event_type(&self) -> &'static str {
-    "spec_chunk_event"
+    match self {
+      SpecChunkEvent::Root(evt) => evt.event_type(),
+      SpecChunkEvent::Batch(evt) => evt.event_type(),
+      SpecChunkEvent::Unknown(evt) => evt.event_type(),
+    }
+  }
+}
+
+impl Event for RootChunkEvent {
+  fn event_type(&self) -> &'static str {
+    "RootChunkEvent"
+  }
+}
+
+impl Event for BatchChunkEvent {
+  fn event_type(&self) -> &'static str {
+    "BatchChunkEvent"
+  }
+}
+
+impl Event for UnknownChunkEvent {
+  fn event_type(&self) -> &'static str {
+    "UnknownChunkEvent"
   }
 }
 
 impl From<(String, bool, Vec<SpecEvent>)> for SpecChunkEvent {
   fn from((name, is_root, events): (String, bool, Vec<SpecEvent>)) -> Self {
-    let (id, parent_id) = if is_root {
-      (Some(String::from("root")), None)
+    if is_root {
+      Self::Root(RootChunkEvent {
+        id: String::from("root"),
+        name,
+        events,
+      })
     } else {
       let first_event = events.first();
-
-      first_event
+      let batch_ids = first_event
         .map(|first_event| match first_event {
           SpecEvent::RfcEvent(RfcEvent::BatchCommitStarted(e)) => {
             (Some(e.batch_id.clone()), e.parent_id.clone())
           }
           _ => (None, None),
         })
-        .unwrap_or((None, None))
-    };
+        .unwrap_or((None, None));
 
-    Self {
-      id,
-      name,
-      is_root,
-      events,
-      parent_id,
+      if let (Some(id), Some(parent_id)) = batch_ids {
+        Self::Batch(BatchChunkEvent {
+          id,
+          name,
+          parent_id,
+          events,
+        })
+      } else {
+        Self::Unknown(UnknownChunkEvent { name, events })
+      }
     }
   }
 }
@@ -95,14 +142,17 @@ mod test {
     assert_eq!(
       spec_chunk_events
         .iter()
-        .map(|chunk| chunk.parent_id.is_some())
+        .map(|chunk| matches!(chunk, SpecChunkEvent::Root(_)))
         .collect::<Vec<_>>(),
-      vec![false, true, true]
+      vec![true, false, false]
     );
 
     assert_eq!(
-      spec_chunk_events.iter().all(|chunk| chunk.id.is_some()),
-      true
-    )
+      spec_chunk_events
+        .iter()
+        .map(|chunk| matches!(chunk, SpecChunkEvent::Batch(_)))
+        .collect::<Vec<_>>(),
+      vec![false, true, true]
+    );
   }
 }
