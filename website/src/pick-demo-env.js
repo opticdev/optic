0fabc83bc10e13bbe4d902da086ae6708ddaa05a
Aidan Cunniffe <aidan@useoptic.com>
2021-02-03 16:14:24 -0500

    fix env

--- website/src/pick-demo-env.js
+++ website/src/pick-demo-env.js
@@ -1,8 +1,3 @@
 export function buildDemoUrl(path) {
-  const host = window.location.hostname.includes('useoptic.com')
-    ? 'demo.useoptic.com'
-    : 'demo.o3c.info';
-  const url = `https://${host}${path}`;
-  console.log(url);
-  return url;
+  return window.location.hostname.split('.').slice(1).join('.');
 }
