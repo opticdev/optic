4b667f02cfc2a3c42dae7ecd9d38de5e803a227f
lou <lou@useoptic.com>
2021-02-09 16:00:46 -0500

    docs and cli: updated link to ignore rules documentation, fixed examples to meet current format

--- workspaces/cli-config/src/helpers/default-ignore-rules.ts
+++ workspaces/cli-config/src/helpers/default-ignore-rules.ts
@@ -17,6 +17,6 @@ export const defaultIgnoreRules = [
 
 export const defaultIgnoreFile = `
 # Default Ignore Rules
-# Learn to configure your own at https://useoptic.com/docs
+# Learn to configure your own at http://localhost:4000/docs/using/advanced-configuration#ignoring-api-paths
 ${defaultIgnoreRules.join('\n')}
 `.trim();
