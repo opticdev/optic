b483f7d607d9b60281d45143c8f41d017495a182
Dev Doshi <dev@useoptic.com>
2021-02-15 14:59:15 -0500

    sourceme should not leak env

--- sourceme.sh
+++ sourceme.sh
@@ -8,10 +8,29 @@ alias apidev="OPTIC_DAEMON_ENABLE_DEBUGGING=yes OPTIC_DEVELOPMENT=yes OPTIC_UI_H
 alias apistage="OPTIC_DAEMON_ENABLE_DEBUGGING=yes $OPTIC_SRC_DIR/workspaces/local-cli/bin/run"
 alias cidev="$OPTIC_SRC_DIR/workspaces/ci-cli/bin/run"
 alias agentdev="$OPTIC_SRC_DIR/workspaces/agent-cli/bin/run"
+
+optic_export_env() {
+  ENV_FILE=$1
+  if [ ! -f "$ENV_FILE" ]
+  then
+    echo "could not find env file '$ENV_FILE'"
+    exit 1
+  fi
+
+  VARS_TO_EXPORT=$(grep -v '^#' "$ENV_FILE")
+  VARS_WITHOUT_WHITESPACE=$(echo $VARS_TO_EXPORT | sed s/\n// | sed s/\ //)
+  if [ "$VARS_WITHOUT_WHITESPACE" = "" ]
+  then
+    echo "'$ENV_FILE' is empty. You might want to put some variables there"
+  else
+    export $VARS_TO_EXPORT
+  fi
+}
+
 optic_workspace_clean() {
   (
     set -o errexit
-    export $(grep -v '^#' $OPTIC_DEBUG_ENV_FILE | xargs) # export all in .env file
+    optic_export_env "$OPTIC_DEBUG_ENV_FILE"
     cd "$OPTIC_SRC_DIR"
     yarn wsrun --stages --report --fast-exit ws:clean
   )
@@ -19,7 +38,7 @@ optic_workspace_clean() {
 optic_workspace_build() {
   (
     set -o errexit
-    export $(grep -v '^#' $OPTIC_DEBUG_ENV_FILE | xargs) # export all in .env file
+    optic_export_env "$OPTIC_DEBUG_ENV_FILE"
     cd "$OPTIC_SRC_DIR"
     yarn wsrun --stages --report --fast-exit --exclude-missing ws:build
   )
@@ -27,7 +46,7 @@ optic_workspace_build() {
 optic_workspace_binaries_build() {
   (
     set -o errexit
-    export $(grep -v '^#' $OPTIC_DEBUG_ENV_FILE | xargs) # export all in .env file
+    optic_export_env "$OPTIC_DEBUG_ENV_FILE"
     cd "$OPTIC_SRC_DIR"
     yarn wsrun --stages --report --fast-exit --exclude-missing ws:build-binaries
   )
@@ -42,7 +61,7 @@ optic_watch() {
   (
     set -o errexit
     cd "$OPTIC_SRC_DIR"
-    export $(grep -v '^#' $OPTIC_DEBUG_ENV_FILE | xargs) # export all in .env file
+    optic_export_env "$OPTIC_DEBUG_ENV_FILE"
     optic_workspace_clean
     yarn run watch --filter=workspace-scripts/watch-filter.js "source sourceme.sh && optic_workspace_build_with_notification"
   )
