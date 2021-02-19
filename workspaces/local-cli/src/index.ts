71514db9f2b4e63b85781737b2ded505ddd39add
lou <lou@useoptic.com>
2021-02-19 18:41:35 -0500

    update-notifier addition

--- workspaces/local-cli/src/index.ts
+++ workspaces/local-cli/src/index.ts
@@ -3,14 +3,20 @@ import path from 'path';
 
 const updateNotifier = require('update-notifier');
 const pkg = require('../package.json');
- 
+
 updateNotifier({pkg}).notify();
 const notifier = updateNotifier({
   pkg,
-  updateCheckInterval: 1000 
+  updateCheckInterval: 1000 * 60 * 60 * 24
 });
 
-console.log(notifier.fetchInfo());
+//console.log(notifier.fetchInfo());
+  
+//const notifier = updateNotifier();
+if (notifier.update) {
+  notifier.notify();
+}
+
 
 const envPath =
   process.env.OPTIC_DEBUG_ENV_FILE || path.join(__dirname, '..', '.env');
