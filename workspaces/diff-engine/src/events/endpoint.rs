af0882225481445d220c091bb577b2f5a2c7872e
Jaap Rood <jaap@jaaprood.nl>
2021-03-12 16:41:05 +0100

    Allow cqrs_event to only be used for event group implementations as well

--- workspaces/diff-engine/src/events/endpoint.rs
+++ workspaces/diff-engine/src/events/endpoint.rs
@@ -46,8 +46,7 @@ pub enum EndpointEvent {
 }
 
 macro_rules! cqrs_event {
-
-  // Single Event => event_name
+  // Single Event => event_name. Impls for a single event
   ($event_type: ty => $name: expr) => {
     impl ::cqrs_core::Event for $event_type {
       fn event_type(&self) -> &'static str {
@@ -62,11 +61,8 @@ macro_rules! cqrs_event {
     }
   };
 
-  // Group (enum) { Event => event_name }
-  ($group_type: ident { $($event_type: ident => $name: expr),+ }) => {
-
-    $( cqrs_event!($event_type => $name); )+
-
+  // Group (enum) { Event }. Impls for a group of events
+  ($group_type: ident { $($event_type: ident),+ }) => {
     impl ::cqrs_core::Event for $group_type {
       fn event_type(&self) -> &'static str {
         match self {
@@ -93,6 +89,13 @@ macro_rules! cqrs_event {
       }
     )+
   };
+
+  // Group (enum) { Event => event_name }, combining impls for group and it's single events
+  ($group_type: ident { $($event_type: ident => $name: expr),+ }) => {
+    $( cqrs_event!($event_type => $name); )+
+
+    cqrs_event!($group_type { $($event_type),+});
+  }
 
 }
 
