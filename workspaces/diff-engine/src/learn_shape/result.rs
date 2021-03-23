597c2025348f6e9efda685f4f7ffefa28a268aa8
Jaap van Hardeveld <jaap@jaaprood.nl>
2021-03-23 12:34:29 +0100

    Implement aggregating trail observations results through creating a union per json trail

--- workspaces/diff-engine/src/learn_shape/result.rs
+++ workspaces/diff-engine/src/learn_shape/result.rs
@@ -9,7 +9,14 @@ pub struct TrailObservationsResult {
 
 impl TrailObservationsResult {
   pub fn union(&mut self, new_result: TrailObservationsResult) {
-    for (json_trail, trail_values) in new_result.values_by_trail {}
+    for (json_trail, new_trail_values) in new_result.values_by_trail {
+      let existing_trail_values = self
+        .values_by_trail
+        .entry(json_trail)
+        .or_insert_with_key(|json_trail| TrailValues::new(json_trail));
+
+      existing_trail_values.union(new_trail_values);
+    }
   }
 }
 
@@ -45,5 +52,15 @@ impl TrailValues {
       was_object: false,
       field_set: Default::default(),
     }
+  }
+
+  pub fn union(&mut self, new_values: TrailValues) {
+    self.was_string = self.was_string || new_values.was_string;
+    self.was_number = self.was_number || new_values.was_number;
+    self.was_boolean = self.was_boolean || new_values.was_boolean;
+    self.was_null = self.was_null || new_values.was_null;
+    self.was_array = self.was_array || new_values.was_array;
+    self.was_object = self.was_object || new_values.was_object;
+    // TODO: figure out what to do about field sets
   }
 }
