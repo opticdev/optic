6317e28e08d3effd1e1ab337ac7e6d756dc80d96
Aidan Cunniffe <aidan@useoptic.com>
2021-04-07 16:58:13 -0400

    fix import in test

--- workspaces/cli-config/tests/conflict-specs/ReadSpecification.test.ts
+++ workspaces/cli-config/tests/conflict-specs/ReadSpecification.test.ts
@@ -1,9 +1,7 @@
 import * as path from 'path';
-import {resolveConflictsIfPresent} from "../../src/helpers/read-specification-json";
+import {resolveConflictsIfPresent, getSpecEventsFrom} from "../../src/helpers/read-specification-json";
 // @ts-ignore
 import fs from "fs-extra";
-import {getSpecEventsFrom} from "../../build/helpers/read-specification-json";
-
 test('can parse a specification.json with a git conflict', async () => {
   const specStorePath = path.join(__dirname, 'examples', 'conflict1.json')
   const rawSpecFileJson = await fs.readFile(path.join(__dirname, 'examples', 'conflict1.json'),  'utf8');
