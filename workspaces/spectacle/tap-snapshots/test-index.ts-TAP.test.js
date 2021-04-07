6a5eff3eb65d071246fb95091321ca565369092a
Stephen Mizell <smizell@gmail.com>
2021-04-07 10:47:33 -0500

    Add initial tests for changelog query

---
+++ workspaces/spectacle/tap-snapshots/test-index.ts-TAP.test.js
@@ -0,0 +1,104 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/index.ts TAP spectacle batchCommits query > must match snapshot 1`] = `
+Object {
+  "data": Null Object {
+    "batchCommits": Array [],
+  },
+}
+`
+
+exports[`test/index.ts TAP spectacle changelog query add new endpoint > must match snapshot 1`] = `
+Object {
+  "data": Null Object {
+    "endpointChanges": Null Object {
+      "endpoints": Array [
+        Null Object {
+          "change": Null Object {
+            "category": "added",
+          },
+          "method": "GET",
+          "path": "/user",
+        },
+      ],
+    },
+  },
+}
+`
+
+exports[`test/index.ts TAP spectacle changelog query add optional response field > must match snapshot 1`] = `
+Object {
+  "data": Null Object {
+    "endpointChanges": Null Object {
+      "endpoints": Array [
+        Null Object {
+          "change": Null Object {
+            "category": "updated",
+          },
+          "method": "GET",
+          "path": "/user",
+        },
+      ],
+    },
+  },
+}
+`
+
+exports[`test/index.ts TAP spectacle changelog query add required response field > must match snapshot 1`] = `
+Object {
+  "data": Null Object {
+    "endpointChanges": Null Object {
+      "endpoints": Array [
+        Null Object {
+          "change": Null Object {
+            "category": "updated",
+          },
+          "method": "GET",
+          "path": "/user",
+        },
+      ],
+    },
+  },
+}
+`
+
+exports[`test/index.ts TAP spectacle changelog query add response status code > must match snapshot 1`] = `
+Object {
+  "data": Null Object {
+    "endpointChanges": Null Object {
+      "endpoints": Array [
+        Null Object {
+          "change": Null Object {
+            "category": "updated",
+          },
+          "method": "GET",
+          "path": "/user",
+        },
+      ],
+    },
+  },
+}
+`
+
+exports[`test/index.ts TAP spectacle changelog query update optional response field > must match snapshot 1`] = `
+Object {
+  "data": Null Object {
+    "endpointChanges": Null Object {
+      "endpoints": Array [
+        Null Object {
+          "change": Null Object {
+            "category": "updated",
+          },
+          "method": "GET",
+          "path": "/user",
+        },
+      ],
+    },
+  },
+}
+`
--- workspaces/spectacle/test/index.ts
+++ workspaces/spectacle/test/index.ts
@@ -1,6 +1,40 @@
+import * as fs from 'fs';
 import Tap from 'tap';
 import { makeSpectacle } from '../src';
 import * as OpticEngine from '@useoptic/diff-engine-wasm/engine/build';
+
+const specs = [
+  {
+    name: "add new endpoint",
+    file: "./test/specs/add-new-endpoint.json",
+    sinceCreatedAt: null,
+    sinceBatchCommitId: null
+  },
+  {
+    name: "add optional response field",
+    file: "./test/specs/add-optional-res-field.json",
+    sinceCreatedAt: "2021-04-07T15:13:51.698Z",
+    sinceBatchCommitId: "9c60b8fb-faec-43f3-a0fb-171306f98d61"
+  },
+  {
+    name: "add required response field",
+    file: "./test/specs/add-required-res-field.json",
+    sinceCreatedAt: "2021-04-07T15:11:49.282Z",
+    sinceBatchCommitId: "6c785ae0-ac1a-4c36-9fc9-e15c587459f8"
+  },
+  {
+    name: "add response status code",
+    file: "./test/specs/add-res-status-code.json",
+    sinceCreatedAt: "2021-04-07T15:16:53.719Z",
+    sinceBatchCommitId: "9ebee98a-77a0-4dfb-b240-966eb610274b"
+  },
+  {
+    name: "update optional response field",
+    file: "./test/specs/update-optional-res-field.json",
+    sinceCreatedAt: "2021-04-07T15:15:16.190Z",
+    sinceBatchCommitId: "5fb6357e-fc7a-4fac-8f00-db1589061e85"
+  }
+]
 
 Tap.test('spectacle batchCommits query', async (test) => {
   const spectacle = await makeSpectacle(OpticEngine, {
@@ -21,3 +55,40 @@ Tap.test('spectacle batchCommits query', async (test) => {
   });
   test.matchSnapshot(results);
 });
+
+specs.forEach(async (spec) => {
+  Tap.test(`spectacle changelog query ${spec.name}`, async (test) => {
+    const spectacle = await makeSpectacle(OpticEngine, {
+      specRepository: {
+        async listEvents(): Promise<any[]> {
+          return JSON.parse(fs.readFileSync(spec.file).toString('utf-8'));
+        }
+      }
+    });
+    const query = spec.sinceCreatedAt
+      ? `{
+        endpointChanges(since: "${spec.sinceCreatedAt}") {
+          endpoints {
+            change {
+              category
+            }
+            path
+            method
+          }
+        }
+      }`
+      : `{
+        endpointChanges {
+          endpoints {
+            change {
+              category
+            }
+            path
+            method
+          }
+        }
+      }`
+    const results = await spectacle({ query, variables: {} });
+    test.matchSnapshot(results);
+  })
+})
