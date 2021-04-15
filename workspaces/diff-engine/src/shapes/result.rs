f7cf9095f6f6c959de257599fb2a4c012f34bbd2
Jaap van Hardeveld <jaap@jaaprood.nl>
2021-04-15 18:32:31 +0200

    Lock ShapeDiffResult's fingerprint result in a snapshot test

--- workspaces/diff-engine/src/shapes/result.rs
+++ workspaces/diff-engine/src/shapes/result.rs
@@ -24,3 +24,95 @@ impl ShapeDiffResult {
     format!("{:x}", hash_state.finish())
   }
 }
+
+#[cfg(test)]
+mod test {
+  use super::*;
+  use insta::assert_debug_snapshot;
+
+  #[test]
+  fn can_produce_stable_fingerprint() {
+    let shape_diffs = vec![
+      ShapeDiffResult::UnspecifiedShape {
+        json_trail: JsonTrail::empty().with_array_item(0),
+        shape_trail: ShapeTrail {
+          root_shape_id: String::from("some-shape-id"),
+          path: vec![],
+        },
+      },
+      ShapeDiffResult::UnspecifiedShape {
+        json_trail: JsonTrail::empty().with_array_item(1),
+        shape_trail: ShapeTrail {
+          root_shape_id: String::from("some-shape-id"),
+          path: vec![],
+        },
+      },
+      ShapeDiffResult::UnspecifiedShape {
+        json_trail: JsonTrail::empty().with_array_item(2),
+        shape_trail: ShapeTrail {
+          root_shape_id: String::from("some-shape-id"),
+          path: vec![],
+        },
+      },
+      ShapeDiffResult::UnspecifiedShape {
+        json_trail: JsonTrail::empty().with_array_item(0),
+        shape_trail: ShapeTrail {
+          root_shape_id: String::from("other-shape-id"),
+          path: vec![],
+        },
+      },
+      ShapeDiffResult::UnmatchedShape {
+        json_trail: JsonTrail::empty().with_array_item(0),
+        shape_trail: ShapeTrail {
+          root_shape_id: String::from("some-shape-id"),
+          path: vec![],
+        },
+      },
+      ShapeDiffResult::UnmatchedShape {
+        json_trail: JsonTrail::empty().with_array_item(2),
+        shape_trail: ShapeTrail {
+          root_shape_id: String::from("some-shape-id"),
+          path: vec![],
+        },
+      },
+      ShapeDiffResult::UnmatchedShape {
+        json_trail: JsonTrail::empty().with_array_item(0),
+        shape_trail: ShapeTrail {
+          root_shape_id: String::from("other-shape-id"),
+          path: vec![],
+        },
+      },
+    ];
+
+    let fingerprints = shape_diffs
+      .iter()
+      .map(|shape_diff| shape_diff.fingerprint())
+      .collect::<Vec<_>>();
+
+    assert_eq!(
+      fingerprints[0], fingerprints[1],
+      "json trail of unspecified variant is normalized"
+    );
+    assert_eq!(
+      fingerprints[0], fingerprints[2],
+      "json trail of unspecified variant is normalized"
+    );
+    assert_ne!(
+      fingerprints[0], fingerprints[3],
+      "shape trail of unspecified variant is not normalized"
+    );
+    assert_ne!(
+      fingerprints[0], fingerprints[4],
+      "variants produce different hashes"
+    );
+    assert_eq!(
+      fingerprints[4], fingerprints[5],
+      "json trail of unmatched variant is normalized"
+    );
+    assert_ne!(
+      fingerprints[4], fingerprints[6],
+      "shape trail of unmatched variant is not normalized"
+    );
+    assert_debug_snapshot!("can_produce_stable_fingerprint__fingerprints", fingerprints);
+  }
+}
