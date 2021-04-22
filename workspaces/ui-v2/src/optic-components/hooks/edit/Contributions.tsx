b1b28bad22dc0c4e70ea61709451abee2d814d2f
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2021-04-22 16:44:57 -0700

    Modify PendingContributions state to handle multiple ids

--- workspaces/ui-v2/src/optic-components/hooks/edit/Contributions.tsx
+++ workspaces/ui-v2/src/optic-components/hooks/edit/Contributions.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useContext, useState } from 'react';
+import { useCallback, useContext, useState } from 'react';
 
 export const ContributionEditContext = React.createContext({});
 
@@ -19,64 +19,70 @@ type ContributionEditContextValue = {
   ) => string | undefined;
 };
 
-interface IContribution {
-  id: string;
-  contributionKey: string;
-  value: string;
-}
-
 type ContributionEditingStoreProps = {
   initialIsEditingState?: boolean;
   children?: any;
 };
 
+// Keyed by id -> contributionKey -> value
+type PendingContributions = Map<string, Map<string, string>>;
+
 export const ContributionEditingStore = (
   props: ContributionEditingStoreProps
 ) => {
-  //@TODO: replace with spectacle
-  /////////////////////////////////////////
-
   const [isEditing, setIsEditing] = useState(
     props.initialIsEditingState || false
   );
-  const [pendingContributions, setPendingContributions]: [
-    IContribution[],
-    any
-  ] = useState([]);
 
-  const stagePendingContribution = (
-    id: string,
-    contributionKey: string,
-    value: string
-  ) => {
-    setPendingContributions((current: IContribution[]) => {
-      const items = [...current];
-      // remove previous changes for same id/key
-      return [
-        ...items.filter(
-          (previous) =>
-            previous.id !== id && previous.contributionKey !== contributionKey
-        ),
-        {
-          id,
-          contributionKey,
-          value,
-        },
-      ];
-    });
-  };
+  const [pendingContributions, setPendingContributions] = useState<PendingContributions>(new Map());
+
+  const stagePendingContribution = useCallback(
+    (
+      newId: string,
+      newContributionKey: string,
+      newValue: string
+    ) => {
+      setPendingContributions((previousState) => {
+        const newState: PendingContributions = new Map(previousState);
+        if (previousState.has(newId)) {
+          const contributionsMap = previousState.get(newId)!;
+          const contributionsToSave = [...contributionsMap].filter(([k, _]) => {
+            const isCurrentContribution = k === newContributionKey;
+            // @nic TODO filter out if this is the old state that's saved in our BE
+            // i.e. we should only issue commands when something has actually changed
+            return !isCurrentContribution
+          }).concat([[
+            newContributionKey, newValue
+          ]]);
+
+          if (contributionsToSave.length > 0) {
+            newState.set(newId, new Map(contributionsToSave))
+          }
+        } else {
+          newState.set(newId, new Map([[newContributionKey, newValue]]))
+        }
+
+        return newState;
+      });
+    }, []
+  );
 
   const value: ContributionEditContextValue = {
     isEditing,
     save: () => {
       setIsEditing(false);
+      // @nic TODO - implement at spectacle
+      // TODO mutate this into the correct mutation for spectacle
+      console.log(pendingContributions)
       alert(JSON.stringify(pendingContributions));
-      setPendingContributions(() => []);
+      setPendingContributions(() => new Map());
     },
-    pendingCount: pendingContributions.length,
+    pendingCount: [...pendingContributions].reduce((acc, [, map]) => acc + map.size, 0),
     setEditing: (bool) => setIsEditing(bool),
     stagePendingContribution,
     lookupContribution: (id, contributionKey) => {
+      // @nic TODO implement fetch for store to load this information
+      // TODO handle loading state when this isn't loaded
       return undefined;
     },
   };
