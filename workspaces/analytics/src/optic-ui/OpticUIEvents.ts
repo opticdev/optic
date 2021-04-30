b255447766514e5de72d3f9c9b222c0f37073386
lou <lou@useoptic.com>
2021-04-30 10:00:35 -0400

    analytics: stubbed events

--- workspaces/analytics/src/optic-ui/OpticUIEvents.ts
+++ workspaces/analytics/src/optic-ui/OpticUIEvents.ts
@@ -13,5 +13,47 @@ export class OpticUIEvents {
     });
   }
 
+  // proposed
+  reviewPageLoaded(diffsPerEndpoint: number[], undocumentedUrls: number, 
+    diffEngineDurationSeconds: number, totalObservations: number, endpointsInSpec: number) {
+    
+    this.dispatch({
+      name: 'review_page_loaded',
+      properties: { diffsPerEndpoint, undocumentedUrls, diffEngineDurationSeconds, totalObservations, endpointsInSpec },
+    });
+  } 
+
+  userApprovedAll(diffsPerEndpoint: number[], durationSeconds: number) {
+    
+    this.dispatch({
+      name: 'user_approved_all',
+      properties: { diffsPerEndpoint, durationSeconds },
+    });
+  } 
+
+  userDocumentedEndpoint(bodies: [], isBulkMode: boolean, isDiscarded: boolean) {
+    
+    this.dispatch({
+      name: 'user_documented_endpoint',
+      properties: { bodies, isBulkMode, isDiscarded },
+    });
+  } 
+
+  userDocumentedDiff(valuesPresented: [], isIgnored: boolean, bugReportCreated: boolean) {
+    
+    this.dispatch({
+      name: 'user_documented_diff',
+      properties: { valuesPresented, isIgnored, bugReportCreated },
+    });
+  } 
+
+  userSavedChanges(undocumentedUrls: number, diffsPerEndpoint: number[] ) {
+
+    this.dispatch({
+      name: 'user_saved_changes',
+      properties: { undocumentedUrls, diffsPerEndpoint }
+    });
+  }
+
   // deprecated
 }
