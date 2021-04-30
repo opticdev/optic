01c4d0864f23d06b36e786cd57c4f52df6061d5c
Nicholas <18374483+niclim@users.noreply.github.com>
2021-04-30 14:51:01 -0700

    Add source mapping to jest

--- workspaces/ui-v2/jest.config.js
+++ workspaces/ui-v2/jest.config.js
@@ -22,6 +22,7 @@ module.exports = {
   moduleNameMapper: {
     '^react-native$': 'react-native-web',
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '^<src>/(.*)$': '<rootDir>/src/$1',
   },
   moduleFileExtensions: [
     'web.js',
