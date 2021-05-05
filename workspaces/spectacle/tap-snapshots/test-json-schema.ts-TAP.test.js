3126b6c1781fddb094c9a77ca10b8728d9ae2a68
Stephen Mizell <smizell@gmail.com>
2021-05-05 15:18:49 -0500

    Initial commit of JSON Schema support

---
+++ workspaces/spectacle/tap-snapshots/test-json-schema.ts-TAP.test.js
@@ -0,0 +1,42 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict';
+exports[
+  `test/json-schema.ts TAP generate JSON schemas for objects > must match snapshot 1`
+] = `
+Object {
+  "properties": Object {
+    "address": Object {
+      "properties": Object {
+        "street": Object {
+          "type": "string",
+        },
+      },
+      "required": Array [],
+      "type": "object",
+    },
+    "age": Object {
+      "oneOf": Array [
+        Object {
+          "type": "string",
+        },
+        Object {
+          "type": "number",
+        },
+      ],
+    },
+    "name": Object {
+      "type": "string",
+    },
+  },
+  "required": Array [
+    "address",
+    "age",
+  ],
+  "type": "object",
+}
+`;
