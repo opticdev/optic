dc90d84205ad255ac2a76ff997b43320f2bfb22d
Nicholas <18374483+niclim@users.noreply.github.com>
2021-05-18 14:23:07 -0700

    fix ui snapshots

--- workspaces/ui-v2/src/lib/__tests/diff-helpers/new-regions-interpretations.test.ts
+++ workspaces/ui-v2/src/lib/__tests/diff-helpers/new-regions-interpretations.test.ts
@@ -1,5 +1,33 @@
 import { newRegionPreview, testCase } from './fixture';
 
+// TODO remove this and figure out how to deterministically set the seed in rust id generation
+// This means we aren't checking for IDs in our snapshots
+const removeIdsFromSnapshot = <T extends Record<string, any>>(
+  unprocessed: T
+): T => {
+  const keysToRemove = new Set([
+    'requestId',
+    'responseId',
+    'shapeId',
+    'fieldId',
+  ]);
+
+  const processed: any = {};
+  for (const [key, value] of Object.entries(unprocessed)) {
+    if (!keysToRemove.has(key)) {
+      if (Array.isArray(value)) {
+        processed[key] = value.map(removeIdsFromSnapshot);
+      } else if (typeof value === 'object') {
+        processed[key] = removeIdsFromSnapshot(value);
+      } else {
+        processed[key] = value;
+      }
+    }
+  }
+
+  return processed;
+};
+
 test('empty spec produced no body diffs', async () => {
   const universe = await testCase('empty-todo')('universe');
   const diffs = universe.diffSet.groupedByEndpointAndShapeTrail();
@@ -27,9 +55,9 @@ test('new request body has correct commands', async () => {
     universe.universe
   );
 
-  expect(preview).toMatchSnapshot();
+  expect(removeIdsFromSnapshot(preview)).toMatchSnapshot();
 });
-//
+
 test('new response body has correct commands', async () => {
   const universe = await testCase('simple-todo')('partial-universe');
   const diffs = universe.diffSet.newRegions().iterator();
@@ -38,12 +66,11 @@ test('new response body has correct commands', async () => {
     '123'
   );
 
-  console.log(diffs[0]);
   const preview = await newRegionPreview(
     diffs.find((i) => i.diffHash === '2d42a681976a8151')!,
     diffService,
     universe.universe
   );
 
-  expect(preview).toMatchSnapshot();
+  expect(removeIdsFromSnapshot(preview)).toMatchSnapshot();
 });
