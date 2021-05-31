7260484dc54f47fd0ca58b33cd916f9ca358a43c
Aidan Cunniffe <aidan@useoptic.com>
2021-05-31 15:03:15 -0400

    debug captures

--- workspaces/local-cli/src/commands/debug/capture.ts
+++ workspaces/local-cli/src/commands/debug/capture.ts
@@ -4,30 +4,30 @@ import { getPathsRelativeToConfig, readApiConfig } from '@useoptic/cli-config';
 import OS from 'os';
 import cli from 'cli-ux';
 import path from 'path';
+import colors from 'colors';
 import fs from 'fs-extra';
 import { IgnoreFileHelper } from '@useoptic/cli-config/build/helpers/ignore-file-interface';
 import { getOrCreateAnonId } from '@useoptic/cli-config/build/opticrc/optic-rc';
-import {getSpecEventsFrom} from "@useoptic/cli-config/build/helpers/read-specification-json";
+import { getSpecEventsFrom } from '@useoptic/cli-config/build/helpers/read-specification-json';
+import { CapturesHelpers } from '@useoptic/cli-server/build/routers/spec-router';
 const pJson = require('../../../package.json');
 
 export default class DebugCapture extends Command {
   static description =
-    'produce a debug file (full of shape hashed interactions) that can be shared with Optic maintainers';
+    'produce a debug file (with of sanitized interactions) that can be shared with Optic maintainers';
   static hidden: boolean = true;
 
   static args = [
     {
       name: 'captureId',
-      description: 'the captureId, see the address bar /review/___{this id}___',
-      required: true,
+      description: 'the captureId, see the address bar /diffs/local/{this-id}',
+      required: false,
     },
   ];
 
   async run() {
     const { args } = this.parse(DebugCapture);
     const { captureId } = args;
-
-    cli.action.start('sanitizing json bodies...');
 
     const {
       capturesPath,
@@ -36,8 +36,28 @@ export default class DebugCapture extends Command {
       opticIgnorePath,
       specStorePath,
     } = await getPathsRelativeToConfig();
+
+    const capturesHelper = new CapturesHelpers(capturesPath);
+    const captures = await capturesHelper.listCaptureIds();
+
+    if (!captureId || !captures.includes(captureId)) {
+      if (captures.length === 0) {
+        return this.error('no captures found');
+      }
+
+      this.log(
+        colors.red.bold(
+          'run api debug:capture <captureId>     | with one of these IDs:\n\n'
+        )
+      );
+      captures.forEach((i) => this.log(i));
+      return;
+    }
+
+    cli.action.start('sanitizing json bodies...');
+
     const configJson = await readApiConfig(configPath);
-    const events = getSpecEventsFrom(specStorePath)
+    const events = await getSpecEventsFrom(specStorePath);
     const ignoreRules = await new IgnoreFileHelper(
       opticIgnorePath,
       configPath
