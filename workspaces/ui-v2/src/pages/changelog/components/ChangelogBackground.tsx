ec4e1de6e8170ad2fd1cc1269e9e67732a0f274d
Nicholas <18374483+niclim@users.noreply.github.com>
2021-07-23 13:41:21 -0700

    Darken background on focus for changelog items

--- workspaces/ui-v2/src/pages/changelog/components/ChangelogBackground.tsx
+++ workspaces/ui-v2/src/pages/changelog/components/ChangelogBackground.tsx
@@ -1,5 +1,6 @@
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles, darken } from '@material-ui/core';
 import {
+  AddedGreenBackground,
   AddedGreen,
   ChangedYellow,
   ChangedYellowBackground,
@@ -7,17 +8,27 @@ import {
   RemovedRedBackground,
 } from '<src>/styles';
 
+// TODO generalize this style and move this into theme
 export const useChangelogStyles = makeStyles(() => ({
   added: {
-    backgroundColor: `rgba(0,196,70,0.2)`,
+    backgroundColor: AddedGreenBackground,
     borderLeft: `2px solid ${AddedGreen}`,
+    '&.Mui-focusVisible, &:hover': {
+      backgroundColor: darken(AddedGreenBackground, 0.2),
+    },
   },
   updated: {
     backgroundColor: ChangedYellowBackground,
     borderLeft: `2px solid ${ChangedYellow}`,
+    '&.Mui-focusVisible, &:hover': {
+      backgroundColor: darken(AddedGreenBackground, 0.2),
+    },
   },
   removed: {
     backgroundColor: RemovedRedBackground,
     borderLeft: `2px solid ${RemovedRed}`,
+    '&.Mui-focusVisible, &:hover': {
+      backgroundColor: darken(AddedGreenBackground, 0.2),
+    },
   },
 }));
