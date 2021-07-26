62eb9789f4d54bb1126ad89d4a62142386055643
Nicholas <18374483+niclim@users.noreply.github.com>
2021-07-26 10:56:52 -0700

    Make removedItems filter a generic function

--- workspaces/ui-v2/src/store/selectors/endpointSelectors.ts
+++ workspaces/ui-v2/src/store/selectors/endpointSelectors.ts
@@ -8,8 +8,39 @@ import { getEndpointId } from '<src>/utils';
 
 import { RootState } from '../root';
 
+const filterRemovedItems = <T extends { isRemoved: boolean }>(
+  removableItems: T[]
+): T[] => removableItems.filter((removableItem) => !removableItem.isRemoved);
+
+const filterRemovableItemsForChangelogAndMapChanges = <
+  T extends { isRemoved: boolean }
+>(
+  removableItems: T[],
+  changes: Record<string, ChangeType>,
+  getId: (item: T) => string
+): (T & { changes: ChangeType | null })[] => {
+  // Filter removed endpoints that have no changes to display
+  const filteredItems = removableItems.filter((item) => {
+    const itemId = getId(item);
+
+    // Assumption - if an endpoint is removed, but this batch of changes contains _any_ change for this endpoint
+    // we should show this endpoint - the last change you could make is a removal
+    // Two assumptions:
+    // - You cannot un-remove a specific endpoint (with history, you can undo batches)
+    // - You must always view changes compared to the latest version
+    return !(item.isRemoved && !changes[itemId]);
+  });
+
+  const itemsWithChanges = filteredItems.map((item) => ({
+    ...item,
+    changes: changes[getId(item)] || null,
+  }));
+
+  return itemsWithChanges;
+};
+
 export const filterRemovedEndpoints = (endpoints: IEndpoint[]): IEndpoint[] =>
-  endpoints.filter((endpoint) => !endpoint.isRemoved);
+  filterRemovedItems(endpoints);
 
 /**
  * Filters removed endpoints not removed in changes and joins changelog information
@@ -18,24 +49,11 @@ export const filterRemovedEndpointsForChangelogAndMapChanges = (
   endpoints: IEndpoint[],
   endpointChanges: Record<string, ChangeType>
 ): IEndpointWithChanges[] => {
-  // Filter removed endpoints that have no changes to display
-  const filteredEndpoints = endpoints.filter((endpoint) => {
-    const endpointId = getEndpointId(endpoint);
-
-    // Assumption - if an endpoint is removed, but this batch of changes contains _any_ change for this endpoint
-    // we should show this endpoint - the last change you could make is a removal
-    // Two assumptions:
-    // - You cannot un-remove a specific endpoint (with history, you can undo batches)
-    // - You must always view changes compared to the latest version
-    return !(endpoint.isRemoved && !endpointChanges[endpointId]);
-  });
-
-  const endpointsWithChanges = filteredEndpoints.map((endpoint) => ({
-    ...endpoint,
-    changes: endpointChanges[getEndpointId(endpoint)] || null,
-  }));
-
-  return endpointsWithChanges;
+  return filterRemovableItemsForChangelogAndMapChanges(
+    endpoints,
+    endpointChanges,
+    (endpoint) => getEndpointId(endpoint)
+  );
 };
 
 export const getEndpoint = ({
