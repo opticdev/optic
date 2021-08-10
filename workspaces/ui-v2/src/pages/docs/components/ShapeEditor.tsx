a64119810d1210fc470785397fe781f99126dafe
Jaap van Hardeveld <jaap@jaaprood.nl>
2021-08-10 18:32:58 +0200

    Highlight the currently selected and hovered fields

--- workspaces/ui-v2/src/pages/docs/components/ShapeEditor.tsx
+++ workspaces/ui-v2/src/pages/docs/components/ShapeEditor.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useMemo } from 'react';
-import { makeStyles, ThemeProvider } from '@material-ui/core';
+import { makeStyles, darken, lighten } from '@material-ui/core';
+import ClassNames from 'classnames';
 
 import { IFieldDetails, IShapeRenderer } from '<src>/types';
 import * as Theme from '<src>/styles/theme';
@@ -19,7 +20,7 @@ export const ShapeEditor: FC<{
             <li className={classes.rowListItem}>
               <Row
                 field={field}
-                selected={selectedFieldId == field.fieldId}
+                selected={selectedFieldId === field.fieldId}
                 onSelect={setSelectedField}
               />
             </li>
@@ -52,6 +53,7 @@ const Row: FC<{
         name={field.name}
         required={field.required}
         shapes={field.shapes}
+        selected={selected}
         onClickHeader={onClickFieldHeader}
       >
         {selected && <FieldEditor field={field} />}
@@ -80,6 +82,11 @@ const useStyles = makeStyles((theme) => ({
   editor: {
     padding: theme.spacing(2, 3),
     minHeight: theme.spacing(10),
+    marginBottom: theme.spacing(2),
+
+    color: '#6b7384',
+
+    boxShadow: 'inset 0px 13px 10px -10px rgba(0, 0, 0, 0.07)',
   },
 }));
 
@@ -88,6 +95,7 @@ const Field: FC<{
   depth: number;
   name: string;
   required: boolean;
+  selected: boolean;
   shapes: IShapeRenderer[];
 
   onClickHeader?: () => void;
@@ -97,6 +105,7 @@ const Field: FC<{
   required,
   depth,
   children,
+  selected,
   onClickHeader,
 }) {
   const classes = useFieldStyles();
@@ -110,11 +119,19 @@ const Field: FC<{
   );
 
   return (
-    <div className={classes.container}>
+    <div
+      className={ClassNames(classes.container, {
+        [classes.isSelected]: selected,
+        [classes.isIndented]: depth > 0,
+      })}
+    >
       <header
         className={classes.header}
         onClick={onClickHeaderHandler}
-        style={{ marginLeft: depth * INDENT_WIDTH }}
+        style={{
+          paddingLeft: Math.max(depth - 1, 0) * INDENT_WIDTH,
+          backgroundImage: `url("${indentsImageUrl(depth - 1)}")`,
+        }}
       >
         <div className={classes.description}>
           <div className={classes.fieldName}>{name}</div>
@@ -142,22 +159,74 @@ function summarizeTypes(shapes: IShapeRenderer[], required: boolean) {
 
 const INDENT_WIDTH = 8 * 3;
 const INDENT_MARKER_WIDTH = 1;
+const INDENT_COLOR = '#E4E8ED';
+function indentsImageUrl(depth: number = 0) {
+  let range = Array(Math.max(depth, 0))
+    .fill(0)
+    .map((val, n) => n);
+  console.log(range);
+  let width = INDENT_WIDTH * depth;
+  return (
+    'data:image/svg+xml,' +
+    encodeURIComponent(
+      `<svg xmlns='http://www.w3.org/2000/svg' width='${width}' height='1' viewBox='0 0 ${width} 1'>
+        ${range
+          .map(
+            (n) => `
+            <rect fill="${INDENT_COLOR}" x="${
+              INDENT_WIDTH * n
+            }" y="0" height="1" width="${INDENT_MARKER_WIDTH}" />;
+          `
+          )
+          .join('')}
+      </svg>`
+    )
+  );
+}
+
 const useFieldStyles = makeStyles((theme) => ({
   container: {
     fontFamily: Theme.FontFamily,
     padding: theme.spacing(0, 0, 0, 1),
-    backgroundColor: '#fafafa',
-    backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='${INDENT_WIDTH}' height='1' viewBox='0 0 ${INDENT_WIDTH} 1'%3E%3Cg stroke='%23CCC' stroke-width='0' %3E%3Crect fill='%23E4E8ED' x='0' y='0' width='${INDENT_MARKER_WIDTH}' height='1'/%3E%3C/g%3E%3C/svg%3E")`,
-    backgroundPositionX: theme.spacing(1) + 1,
   },
+  isSelected: {},
+  isIndented: {},
+
   header: {
     display: 'flex',
-    backgroundColor: '#fafafa',
-    padding: theme.spacing(1, 0),
+    backgroundPositionX: 0,
+    backgroundRepeat: 'repeat-y',
+    padding: 0,
+    cursor: 'pointer',
+
+    '$isSelected &': {
+      borderBottom: `1px solid ${lighten(Theme.OpticBlueReadable, 0.5)}`,
+
+      '&:hover': {
+        borderBottom: `1px solid ${lighten(Theme.OpticBlueReadable, 0.2)}`,
+      },
+    },
   },
   description: {
     display: 'flex',
+    padding: theme.spacing(1, 0),
     alignItems: 'baseline',
+    borderLeft: `1px solid #E4E8ED`,
+    borderLeftWidth: 0,
+
+    '$isIndented &': {
+      paddingLeft: INDENT_WIDTH,
+      borderLeftWidth: '1px',
+    },
+
+    '$isIndented $header:hover &': {
+      borderLeftColor: darken(INDENT_COLOR, 0.3),
+    },
+
+    '$isIndented$isSelected &': {
+      borderLeftWidth: '1px',
+      borderLeftColor: lighten(Theme.OpticBlueReadable, 0.5),
+    },
   },
 
   fieldName: {
@@ -171,7 +240,5 @@ const useFieldStyles = makeStyles((theme) => ({
     color: '#8792a2',
   },
 
-  stage: {
-    background: '#fafafa',
-  },
+  stage: {},
 }));
