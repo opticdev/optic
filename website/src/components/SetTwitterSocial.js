c22c2c552967fc06a3a1bb379c3a81776db1ed5d
Aidan Cunniffe <aidan@useoptic.com>
2021-08-11 16:40:50 -0400

    fix social again

--- website/src/components/SetTwitterSocial.js
+++ website/src/components/SetTwitterSocial.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { NoSsr } from '@material-ui/core';
 import Helmet from 'react-helmet';
+import BrowserOnly from '@docusaurus/core/lib/client/exports/BrowserOnly';
 
 export function SetTwitterSocial() {
   return (
-    <NoSsr>
-      <Helmet>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site	" content="@useoptic" />
-        <meta name="twitter:creator	" content="@aidandcunniffe" />
-      </Helmet>
-    </NoSsr>
+    <BrowserOnly
+      children={() => {
+        <Helmet>
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:site	" content="@useoptic" />
+          <meta property="twitter:creator	" content="@aidandcunniffe" />
+        </Helmet>;
+      }}
+    />
   );
 }
