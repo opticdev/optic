43cae36b0416013c51517ee4e0c4799ab69bf561
lou <lou@useoptic.com>
2021-08-26 10:56:25 -0400

    tweak property name

--- website/analytics/src/index.js
+++ website/analytics/src/index.js
@@ -8,7 +8,7 @@ module.exports = function (context, options) {
         headTags: [
           {
             tagName: 'script',
-            children: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="UZiWcK9DrdcEaZ7ko4Sd0bZu45P7bol4";analytics.SNIPPET_VERSION="4.13.2"; analytics.load(${segmentToken}); analytics.page();}}();`,
+            innerHTML: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="UZiWcK9DrdcEaZ7ko4Sd0bZu45P7bol4";analytics.SNIPPET_VERSION="4.13.2"; analytics.load(${segmentToken}); analytics.page();}}();`,
             attributes: {
               type: 'text/javascript',
             },
@@ -21,7 +21,7 @@ module.exports = function (context, options) {
           },
           {
             tagName: 'script',
-            children: `window['_fs_debug'] = false;
+            innerHTML: `window['_fs_debug'] = false;
 window['_fs_host'] = 'fullstory.com';
 window['_fs_script'] = 'edge.fullstory.com/s/fs.js';
 window['_fs_org'] = 'KJ58X';
