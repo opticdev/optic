19f9a2441020816f680c702902e5e367b9b1cf69
Aidan Cunniffe <aidan@useoptic.com>
2021-11-03 13:50:45 -0400

    update rules for proper describe format

--- projects/snyk-rules/src/rulesets/tests/operations.test.ts
+++ projects/snyk-rules/src/rulesets/tests/operations.test.ts
@@ -8,64 +8,70 @@ const { compare } = createDslFixture<SnykApiCheckDsl, SynkApiCheckContext>(
   }
 );
 
-const baseForOperationIdTests = {
-  openapi: "3.0.1",
-  paths: {
-    "/example": {
-      get: {
-        responses: {},
+describe("operationId", () => {
+  const baseForOperationIdTests = {
+    openapi: "3.0.1",
+    paths: {
+      "/example": {
+        get: {
+          responses: {},
+        },
       },
     },
-  },
-  info: { version: "0.0.0", title: "Empty" },
-};
+    info: { version: "0.0.0", title: "Empty" },
+  };
 
-it("empty string operationId is invalid", async () => {
-  const result = await compare(baseForOperationIdTests)
-    .to((spec) => {
-      spec.paths!["/example"]!.get!.operationId = "";
-      return spec;
-    })
-    .withRule(rules.operationId, {});
+  describe("missing", () => {
+    it("fails if empty string", async () => {
+      const result = await compare(baseForOperationIdTests)
+        .to((spec) => {
+          spec.paths!["/example"]!.get!.operationId = "";
+          return spec;
+        })
+        .withRule(rules.operationId, {});
 
-  expect(result.results[0].passed).toBeFalsy();
-  expect(result).toMatchSnapshot();
-});
+      expect(result.results[0].passed).toBeFalsy();
+      expect(result).toMatchSnapshot();
+    });
 
-it("undefined operationId is invalid", async () => {
-  const result = await compare(baseForOperationIdTests)
-    .to((spec) => {
-      delete spec.paths!["/example"]!.get!.operationId;
-      return spec;
-    })
-    .withRule(rules.operationId, {});
+    it("fails if undefined", async () => {
+      const result = await compare(baseForOperationIdTests)
+        .to((spec) => {
+          delete spec.paths!["/example"]!.get!.operationId;
+          return spec;
+        })
+        .withRule(rules.operationId, {});
 
-  expect(result.results[0].passed).toBeFalsy();
-  expect(result).toMatchSnapshot();
-});
+      expect(result.results[0].passed).toBeFalsy();
+      expect(result).toMatchSnapshot();
+    });
+  });
 
-it("invalid operationId prefix should fail", async () => {
-  const result = await compare(baseForOperationIdTests)
-    .to((spec) => {
-      spec.paths!["/example"]!.get!.operationId = "findHelloWorld";
-      return spec;
-    })
-    .withRule(rules.operationId, {});
+  describe("when set", () => {
+    it("fails if prefix is wrong", async () => {
+      const result = await compare(baseForOperationIdTests)
+        .to((spec) => {
+          spec.paths!["/example"]!.get!.operationId = "findHelloWorld";
+          return spec;
+        })
+        .withRule(rules.operationId, {});
 
-  expect(result.results[0].passed).toBeFalsy();
-  expect(result).toMatchSnapshot();
-});
+      expect(result.results[0].passed).toBeFalsy();
+      expect(result).toMatchSnapshot();
+    });
 
-it("invalid operationId case should fail", async () => {
-  const result = await compare(baseForOperationIdTests)
-    .to((spec) => {
-      spec.paths!["/example"]!.get!.operationId = "get-hello-world";
-      return spec;
-    })
-    .withRule(rules.operationId, {});
+    it("fails if not camel case", async () => {
+      const result = await compare(baseForOperationIdTests)
+        .to((spec) => {
+          spec.paths!["/example"]!.get!.operationId = "get-hello-world";
+          return spec;
+        })
+        .withRule(rules.operationId, {});
 
-  expect(result.results[0].passed).toBeFalsy();
-  expect(result).toMatchSnapshot();
+      expect(result.results[0].passed).toBeFalsy();
+      expect(result).toMatchSnapshot();
+    });
+  });
 });
 
 const baseForOperationMetadataTests = {
@@ -83,47 +89,53 @@ const baseForOperationMetadataTests = {
   info: { version: "0.0.0", title: "Empty" },
 };
 
-it("missing summary should fail", async () => {
-  const result = await compare(baseForOperationMetadataTests)
-    .to((spec) => {
-      delete spec.paths!["/example"]!.get!.summary;
-      return spec;
-    })
-    .withRule(rules.summary, {});
+describe("operation metadata", () => {
+  describe("summary", () => {
+    it("fails if missing", async () => {
+      const result = await compare(baseForOperationMetadataTests)
+        .to((spec) => {
+          delete spec.paths!["/example"]!.get!.summary;
+          return spec;
+        })
+        .withRule(rules.summary, {});
 
-  expect(result.results[0].passed).toBeFalsy();
-  expect(result).toMatchSnapshot();
-});
+      expect(result.results[0].passed).toBeFalsy();
+      expect(result).toMatchSnapshot();
+    });
 
-it("with summary passes", async () => {
-  const result = await compare(baseForOperationMetadataTests)
-    .to((spec) => {
-      spec.paths!["/example"]!.get!.summary = "I have a summary";
-      return spec;
-    })
-    .withRule(rules.summary, {});
+    it("passes if provided", async () => {
+      const result = await compare(baseForOperationMetadataTests)
+        .to((spec) => {
+          spec.paths!["/example"]!.get!.summary = "I have a summary";
+          return spec;
+        })
+        .withRule(rules.summary, {});
 
-  expect(result.results[0].passed).toBeTruthy();
-  expect(result).toMatchSnapshot();
-});
+      expect(result.results[0].passed).toBeTruthy();
+      expect(result).toMatchSnapshot();
+    });
+  });
 
-it("with tags passes", async () => {
-  const result = await compare(baseForOperationMetadataTests)
-    .to((spec) => spec)
-    .withRule(rules.tags, {});
+  describe("tags", () => {
+    it("passes if > 1 tag provided", async () => {
+      const result = await compare(baseForOperationMetadataTests)
+        .to((spec) => spec)
+        .withRule(rules.tags, {});
 
-  expect(result.results[0].passed).toBeTruthy();
-  expect(result).toMatchSnapshot();
-});
+      expect(result.results[0].passed).toBeTruthy();
+      expect(result).toMatchSnapshot();
+    });
 
-it("missing tags should fail", async () => {
-  const result = await compare(baseForOperationMetadataTests)
-    .to((spec) => {
-      delete spec.paths!["/example"]!.get!.tags;
-      return spec;
-    })
-    .withRule(rules.tags, {});
+    it("fail is not provided", async () => {
+      const result = await compare(baseForOperationMetadataTests)
+        .to((spec) => {
+          delete spec.paths!["/example"]!.get!.tags;
+          return spec;
+        })
+        .withRule(rules.tags, {});
 
-  expect(result.results[0].passed).toBeFalsy();
-  expect(result).toMatchSnapshot();
+      expect(result.results[0].passed).toBeFalsy();
+      expect(result).toMatchSnapshot();
+    });
+  });
 });
