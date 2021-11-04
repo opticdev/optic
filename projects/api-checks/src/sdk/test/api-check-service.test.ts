fb9a6f740ebb7ff988870d1d7a72b4fd14d383a7
Aidan Cunniffe <aidan@useoptic.com>
2021-11-04 16:31:43 -0400

    fix tests

--- projects/api-checks/src/sdk/test/api-check-service.test.ts
+++ projects/api-checks/src/sdk/test/api-check-service.test.ts
@@ -2,8 +2,6 @@ import { ApiCheckService } from "../api-check-service";
 import { ExampleDsl, ExampleDslContext } from "./example-dsl";
 import { OpenAPIV3 } from "@useoptic/openapi-utilities";
 import { doc } from "prettier";
-const { expect } = require("chai"); // Using Assert style
-
 export const defaultEmptySpec: OpenAPIV3.Document = {
   openapi: "3.0.1",
   paths: {},
@@ -14,6 +12,8 @@ function completenessApiRules(dsl: ExampleDsl) {
   dsl.operations.changed.must(
     "have consistent operationIds",
     (current, next, context, docs) => {
+      const { expect } = require("chai"); // Using Assert style
+
       expect(current.operationId).to.equal(
         next.operationId || "",
         "operation ids must be consistent"
