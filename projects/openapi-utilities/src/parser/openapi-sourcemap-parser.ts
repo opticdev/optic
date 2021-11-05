ad897029a5e67543133e74bf07842d04eccfdba9
Aidan Cunniffe <aidan@useoptic.com>
2021-11-05 10:22:34 -0400

    fix non-deterministic test re: promise returns

--- projects/openapi-utilities/src/parser/openapi-sourcemap-parser.ts
+++ projects/openapi-utilities/src/parser/openapi-sourcemap-parser.ts
@@ -7,16 +7,19 @@ import { YAMLMapping, YAMLNode, YAMLSequence } from "yaml-ast-parser";
 // @ts-ignore
 import { dereference } from "./insourced-dereference";
 import * as pointer from "json-ptr";
-import path from 'path'
-import fetch from 'node-fetch';
-import {OpenAPIV3} from "openapi-types";
+import path from "path";
+import fetch from "node-fetch";
+import { OpenAPIV3 } from "openapi-types";
+import sortBy from "lodash.sortby";
 
 export type ParseOpenAPIResult = {
-  jsonLike: OpenAPIV3.Document,
-  sourcemap: JsonSchemaSourcemapOutput
-}
+  jsonLike: OpenAPIV3.Document;
+  sourcemap: JsonSchemaSourcemapOutput;
+};
 
-export async function parseOpenAPIWithSourcemap(path: string): Promise<ParseOpenAPIResult> {
+export async function parseOpenAPIWithSourcemap(
+  path: string
+): Promise<ParseOpenAPIResult> {
   const resolver = new $RefParser();
 
   const sourcemap = new JsonSchemaSourcemap();
@@ -24,17 +27,17 @@ export async function parseOpenAPIWithSourcemap(path: string): Promise<ParseOpen
     resolve: {
       http: {
         headers: {
-          "accept": "*/*"
-        }
-      }
-    }
+          accept: "*/*",
+        },
+      },
+    },
   });
 
   // parse all asts
   await Promise.all(
     resolverResults
       .paths()
-      .map((filePath) => sourcemap.addFileIfMissing(filePath))
+      .map((filePath, index) => sourcemap.addFileIfMissing(filePath, index))
   );
 
   dereference(
@@ -46,28 +49,40 @@ export async function parseOpenAPIWithSourcemap(path: string): Promise<ParseOpen
   return { jsonLike: resolver.schema as any, sourcemap: sourcemap.serialize() };
 }
 
-export async function parseOpenAPIFromRepoWithSourcemap(name: string, repoPath: string, branch: string): Promise<ParseOpenAPIResult> {
-  const newGitBranchResolver = require("./git-branch-file-resolver.js")
+export async function parseOpenAPIFromRepoWithSourcemap(
+  name: string,
+  repoPath: string,
+  branch: string
+): Promise<ParseOpenAPIResult> {
+  const newGitBranchResolver = require("./git-branch-file-resolver.js");
 
-  const inGitResolver = newGitBranchResolver(repoPath, branch)
+  const inGitResolver = newGitBranchResolver(repoPath, branch);
   const resolver = new $RefParser();
-  const fileName = path.join(repoPath, name)
+  const fileName = path.join(repoPath, name);
 
   const sourcemap = new JsonSchemaSourcemap();
-  const resolverResults: $RefParser.$Refs = await resolver.resolve(fileName, { resolve: { file: inGitResolver } });
+  const resolverResults: $RefParser.$Refs = await resolver.resolve(fileName, {
+    resolve: { file: inGitResolver },
+  });
 
   // parse all asts
   await Promise.all(
-    resolverResults
-      .paths()
-      .map(async (filePath) => {
-        return await sourcemap.addFileIfMissingFromContents(filePath, await inGitResolver.read({ url: filePath }))
-      })
+    resolverResults.paths().map(async (filePath, index) => {
+      return await sourcemap.addFileIfMissingFromContents(
+        filePath,
+        await inGitResolver.read({ url: filePath }),
+        index
+      );
+    })
   );
 
   dereference(
     resolver,
-    { ...$RefParserOptions.defaults, path: fileName, resolve: { file: inGitResolver } },
+    {
+      ...$RefParserOptions.defaults,
+      path: fileName,
+      resolve: { file: inGitResolver },
+    },
     sourcemap
   );
 
@@ -96,8 +111,7 @@ export class JsonSchemaSourcemap {
 
   private _mappings: Array<DerefToSource> = [];
 
-  async addFileIfMissing(filePath: string) {
-
+  async addFileIfMissing(filePath: string, fileIndex: number) {
     if (filePath.startsWith("http")) {
       const response = await fetch(filePath);
       const asText = await response.text();
@@ -106,10 +120,9 @@ export class JsonSchemaSourcemap {
 
       this._files.push({
         path: filePath,
-        index: this._files.length,
+        index: fileIndex,
         ast: yamlAst,
       });
-
     } else {
       if (!this._files.find((i) => i.path === filePath)) {
         // add the ast to the cache
@@ -119,23 +132,25 @@ export class JsonSchemaSourcemap {
 
         this._files.push({
           path: filePath,
-          index: this._files.length,
+          index: fileIndex,
           ast: yamlAst,
         });
       }
     }
   }
 
-  async addFileIfMissingFromContents(filePath: string, contents: string) {
+  async addFileIfMissingFromContents(
+    filePath: string,
+    contents: string,
+    fileIndex: number
+  ) {
     if (!this._files.find((i) => i.path === filePath)) {
       // add the ast to the cache
-      const yamlAst: YAMLNode = YAML.safeLoad(
-        contents
-      );
+      const yamlAst: YAMLNode = YAML.safeLoad(contents);
 
       this._files.push({
         path: filePath,
-        index: this._files.length,
+        index: fileIndex,
         ast: yamlAst,
       });
     }
@@ -159,7 +174,10 @@ export class JsonSchemaSourcemap {
 
   public serialize(): JsonSchemaSourcemapOutput {
     return {
-      files: this._files.map((i) => ({ path: i.path, index: i.index })),
+      files: sortBy(
+        this._files.map((i) => ({ path: i.path, index: i.index })),
+        "index"
+      ),
       map: this._mappings,
     };
   }
@@ -175,12 +193,14 @@ export function resolveJsonPointerInYamlAst(
   const isEmpty =
     decoded.length === 0 || (decoded.length === 1 && decoded[0] === "");
 
-  if (isEmpty) return { node: [node.startPosition, node.endPosition], file: file };
+  if (isEmpty)
+    return { node: [node.startPosition, node.endPosition], file: file };
 
   const found: YAMLNode | undefined = decoded.reduce((current, path) => {
     if (!current) return undefined;
     const node: YAMLNode = current.key ? current.value : current;
-    const isNumericalKey = !isNaN(Number(path)) && (node as any).hasOwnProperty("items");
+    const isNumericalKey =
+      !isNaN(Number(path)) && (node as any).hasOwnProperty("items");
 
     if (isNumericalKey) {
       return (node as YAMLSequence).items[Number(path)];
