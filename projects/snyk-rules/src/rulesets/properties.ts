89e80a5f307818baf025ea62c12c416bcc1f01de
Stephen Mizell <smizell@gmail.com>
2021-11-05 09:45:36 -0500

    Fix naming mistake

--- projects/snyk-rules/src/rulesets/properties.ts
+++ projects/snyk-rules/src/rulesets/properties.ts
@@ -12,13 +12,13 @@ chai.use(function (_chai, _) {
 
 export const rules = {
   propertyKey: ({ bodyProperties }: SnykApiCheckDsl) => {
-    bodyProperties.requirement.must("be camel case", ({ key }) => {
+    bodyProperties.requirement.must("have camel case keys", ({ key }) => {
       const camelCaseRe = /^[a-z]+([A-Z][a-z]+)+$/g;
       expect(camelCaseRe.test(key)).to.be.ok;
     });
   },
   propertyExample: ({ bodyProperties }: SnykApiCheckDsl) => {
-    bodyProperties.requirement.must("be camel case", ({ flatSchema }) => {
+    bodyProperties.requirement.must("have an example", ({ flatSchema }) => {
       expect(flatSchema.example).to.exist;
     });
   },
