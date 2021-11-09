0e7077090b73ac8b51d4d03d0ff7984e062cf741
Stephen Mizell <smizell@gmail.com>
2021-11-09 12:39:47 -0600

    Add rule for checking response headers

--- projects/snyk-rules/src/rulesets/tests/headers.test.ts
+++ projects/snyk-rules/src/rulesets/tests/headers.test.ts
@@ -82,4 +82,51 @@ describe("headers", () => {
       expect(result).toMatchSnapshot();
     });
   });
+
+  describe("responses", () => {
+    it("fails if it's missing headers", async () => {
+      const result = await compare(baseOpenAPI)
+        .to((spec) => {
+          spec.paths!["/example"]!.get!.responses = {
+            "200": {
+              description: "No headers",
+            },
+          };
+          return spec;
+        })
+        .withRule(rules.responseHeaders, emptyContext);
+
+      // for (const ruleResult of result.results) {
+      //   expect(ruleResult.passed).toBe(true);
+      // }
+      expect(result.results[0].passed).toBeFalsy();
+      expect(result).toMatchSnapshot();
+    });
+  });
+
+  it("passes if it has all the headers", async () => {
+    const result = await compare(baseOpenAPI)
+      .to((spec) => {
+        spec.paths!["/example"]!.get!.responses = {
+          "200": {
+            description: "No headers",
+            headers: {
+              "snyk-request-id": {},
+              "snyk-version-lifecycle-stage": {},
+              "snyk-version-requested": {},
+              "snyk-version-served": {},
+              sunset: {},
+            },
+          },
+        };
+        return spec;
+      })
+      .withRule(rules.responseHeaders, emptyContext);
+
+    // for (const ruleResult of result.results) {
+    //   expect(ruleResult.passed).toBe(true);
+    // }
+    expect(result.results[0].passed).toBeTruthy();
+    expect(result).toMatchSnapshot();
+  });
 });
