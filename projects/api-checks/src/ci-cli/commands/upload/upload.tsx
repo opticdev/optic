78ffa0514e14c21454598b6ab7d42e8d2da67c37
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2021-11-16 14:12:25 -0800

    Implement getting github context (#61)
    * Implement getting github context
    
    * Type out the event in context
    
    * Change loading from env variable to file load
    
    * Add comment to check run behavior
    
    * fmt

--- projects/api-checks/src/ci-cli/commands/upload/upload.tsx
+++ projects/api-checks/src/ci-cli/commands/upload/upload.tsx
@@ -10,49 +10,119 @@ export const registerUpload = (
   cli
     .command("upload")
     .requiredOption("--from <from>", "from file or rev:file")
-    .requiredOption("--to <to>", "from file or rev:file")
+    .requiredOption("--to <to>", "to file or rev:file")
+    .requiredOption("--context <context>", "file with github context")
     // .requiredOption("--rules <rules>", "path to rules output") // TODO - uncomment and implement - also figure out how to make this
-    // TODO figure out what optic domain routing information to send (organization, PR, runs)
-    // TODO figure out what github context to send
-    .action(async ({ from, to }: { from: string; to: string }) => {
-      if (!opticToken) {
-        console.error("Upload token was not included");
-        return process.exit(1);
-      }
+    .action(
+      async ({
+        from,
+        to,
+        context,
+      }: {
+        from: string;
+        to: string;
+        context: string;
+      }) => {
+        if (!opticToken) {
+          console.error("Upload token was not included");
+          return process.exit(1);
+        }
 
-      const backendWebBase =
-        process.env.OPTIC_ENV === "staging"
-          ? "https://api.o3c.info"
-          : "https://api.useoptic.com";
+        const backendWebBase =
+          process.env.OPTIC_ENV === "staging"
+            ? "https://api.o3c.info"
+            : "https://api.useoptic.com";
 
-      const opticClient = new OpticBackendClient(backendWebBase, () =>
-        Promise.resolve(opticToken)
-      );
-      console.log("Loading files...");
-      try {
-        const fileBuffers = await Promise.all([loadFile(from), loadFile(to)]);
-
-        console.log("Uploading OpenAPI files to Optic...");
-
-        const [fromFileS3Path, toFileS3Path] = await Promise.all(
-          fileBuffers.map((fileBuffer) =>
-            uploadFileToS3(opticClient, fileBuffer)
-          )
+        const opticClient = new OpticBackendClient(backendWebBase, () =>
+          Promise.resolve(opticToken)
         );
+        try {
+          console.log("Loading files...");
+          const [githubContextFileBuffer, ...fileBuffers] = await Promise.all([
+            loadFile(context),
+            loadFile(from),
+            loadFile(to),
+          ]);
 
-        await opticClient.saveCiRun();
+          // TODO change this for different providers
+          const { organization, pull_request, run } =
+            readAndValidateGithubContext(githubContextFileBuffer);
 
-        console.log("Successfully uploaded files to Optic");
-        const opticUploadUrl = "todo add url";
-        console.log(
-          `You can view the results of this run at: ${opticUploadUrl}`
-        );
-        // TODO post comment to github
-      } catch (e) {
-        console.error(e);
-        return process.exit(1);
+          console.log("Uploading OpenAPI files to Optic...");
+
+          const [githubContextFileS3Path, fromFileS3Path, toFileS3Path] =
+            await Promise.all(
+              fileBuffers.map((fileBuffer) =>
+                uploadFileToS3(opticClient, fileBuffer)
+              )
+            );
+
+          // TODO check whether the `run` numbers are reused, and if they are, should that overwrite, or create
+          // i.e. there are run number, and run attempt, so retrying on the same commit hash may trigger the same run number
+          // May need to try catch this block (or decide on product behavior)
+          await opticClient.saveCiRun();
+
+          console.log("Successfully uploaded files to Optic");
+          const opticUploadUrl = "todo add url";
+          console.log(
+            `You can view the results of this run at: ${opticUploadUrl}`
+          );
+          // TODO post comment to github
+        } catch (e) {
+          console.error(e);
+          return process.exit(1);
+        }
       }
-    });
+    );
+};
+
+type UploadContext = {
+  organization: string;
+  pull_request: number;
+  run: number;
+};
+
+// This is specifically from the GITHUB_CONTEXT object
+const readAndValidateGithubContext = (
+  unvalidatedContextFile: Buffer
+): UploadContext => {
+  // expected shape should is from `echo $GITHUB_CONTEXT > file`
+  // full event payload https://github.com/octokit/webhooks/blob/master/payload-types/schema.d.ts#L248
+  const parsedContext = JSON.parse(unvalidatedContextFile.toString());
+
+  if (parsedContext.event_name !== "pull_request") {
+    throw new Error(
+      "Upload expects to be triggered with a pull_request github workflow action"
+    );
+  }
+
+  const organization: string | undefined =
+    parsedContext.event?.repository?.owner?.login;
+  const pull_request: number | undefined =
+    parsedContext.event?.pull_request?.number;
+  const run: number | undefined = parsedContext.run_number;
+
+  if (!organization) {
+    throw new Error(
+      "Expected a respository owner at context.event.repository.owner.login"
+    );
+  }
+
+  if (!pull_request) {
+    throw new Error(
+      "Expected a pull_request number at context.event.pull_request.number"
+    );
+  }
+
+  if (!run) {
+    throw new Error("Expected a run_number at context.run_number");
+  }
+
+  return {
+    organization,
+    pull_request,
+    run,
+  };
 };
 
 // TODO created shared instance to import from (optic cloud fe + here)
