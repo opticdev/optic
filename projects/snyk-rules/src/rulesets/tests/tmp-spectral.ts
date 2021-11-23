1555f00382ec27524cdca70049dc932fcd7a6a7a
Aidan Cunniffe <aidan@useoptic.com>
2021-11-23 10:11:28 -0500

    manual test file

---
+++ projects/snyk-rules/src/rulesets/tests/tmp-spectral.ts
@@ -0,0 +1,82 @@
+import { SynkApiCheckContext } from "../../dsl";
+import { parseSpecVersion } from "@useoptic/api-checks/build/ci-cli/input-helpers/compare-input-parser";
+import { defaultEmptySpec } from "@useoptic/api-checks/build/ci-cli/constants";
+import { specFromInputToResults } from "@useoptic/api-checks";
+import { newSnykApiCheckService } from "../../service";
+import { sourcemapReader } from "@useoptic/openapi-io";
+import { ResultWithSourcemap } from "@useoptic/api-checks/build/sdk/types";
+import path from "path";
+
+async function snapshotScenario(
+  from: string | undefined,
+  to: string | undefined,
+  workingDir: string,
+  context: SynkApiCheckContext,
+  shouldPass: boolean
+) {
+  const fromSpecSig = parseSpecVersion(from, defaultEmptySpec);
+  const fromSpec = await specFromInputToResults(fromSpecSig, workingDir);
+  const toSpecSig = parseSpecVersion(to, defaultEmptySpec);
+  const toSpec = await specFromInputToResults(toSpecSig, workingDir);
+
+  const checkService = newSnykApiCheckService();
+  const checkResults = await checkService.runRules(
+    fromSpec.jsonLike,
+    toSpec.jsonLike,
+    context
+  );
+
+  const { findFileAndLines } = sourcemapReader(toSpec.sourcemap);
+  const result: ResultWithSourcemap[] = await Promise.all(
+    checkResults.map(async (checkResult) => {
+      const sourcemap = await findFileAndLines(
+        checkResult.change.location.jsonPath
+      );
+
+      const filePath = sourcemap?.filePath.split("end-end-tests")[1];
+
+      // if (!filePath) {
+      //   console.log(checkResult.change.location.jsonPath);
+      //   console.log("not found");
+      // }
+
+      return {
+        ...checkResult,
+        sourcemap: {
+          ...sourcemap,
+          preview: "",
+          filePath,
+        },
+        change: null as any,
+      } as ResultWithSourcemap;
+    })
+  );
+  return result;
+}
+const resourceDate = (resource: string, date: string) =>
+  path.join(inputsDir, "resources", resource, date);
+
+const inputsDir = path.resolve(
+  path.join(__dirname, "../../../end-end-tests/api-standards")
+);
+
+async function main() {
+  const results = await snapshotScenario(
+    "000-baseline.yaml",
+    "002-ok-add-operation.yaml",
+    resourceDate("thing", "2021-11-10"),
+    {
+      changeDate: "2021-11-11",
+      changeResource: "thing",
+      changeVersion: {
+        date: "2021-11-10",
+        stability: "beta",
+      },
+      resourceVersions: {},
+    },
+    true
+  );
+  console.log(results);
+}
+
+main();
