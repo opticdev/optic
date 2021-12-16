2a629a661e432af703534472647d3e500674fd0e
Aidan Cunniffe <aidan@useoptic.com>
2021-12-16 08:23:09 -0500

    add to exports

--- projects/api-checks/src/index.ts
+++ projects/api-checks/src/index.ts
@@ -1,13 +1,19 @@
-import { makeCiCli } from "./ci-cli/make-cli";
-import { OpenAPIV3 } from "@useoptic/openapi-utilities";
-import { ApiCheckService, DslConstructorInput } from "./sdk/api-check-service";
-import { ApiCheckDsl, Result, Passed, Failed, EntityRule } from "./sdk/types";
-import { runCheck, newDocsLinkHelper, DocsLinkHelper } from "./sdk/types";
-import { createTestDslFixture } from "./sdk/test-rule-fixture";
-import { createSelectJsonPathHelper } from "./sdk/select-when-rule";
-import { specFromInputToResults } from "./ci-cli/input-helpers/load-spec";
-import { genericEntityRuleImpl } from "./sdk/generic-entity-rule-impl";
-import { SpectralDsl } from "./sdk/spectral/dsl";
+import { makeCiCli } from './ci-cli/make-cli';
+import { OpenAPIV3 } from '@useoptic/openapi-utilities';
+import { ApiCheckService, DslConstructorInput } from './sdk/api-check-service';
+import { ApiCheckDsl, Result, Passed, Failed, EntityRule } from './sdk/types';
+import { runCheck, newDocsLinkHelper, DocsLinkHelper } from './sdk/types';
+import { createTestDslFixture } from './sdk/test-rule-fixture';
+import { createSelectJsonPathHelper } from './sdk/select-when-rule';
+import { specFromInputToResults } from './ci-cli/input-helpers/load-spec';
+import { genericEntityRuleImpl } from './sdk/generic-entity-rule-impl';
+import { SpectralDsl } from './sdk/spectral/dsl';
+import {
+  disableRules,
+  mergeRulesets,
+  OpticCINamedRulesets,
+  OpticCIRuleset,
+} from './sdk/ruleset';
 export {
   ApiCheckService,
   DslConstructorInput,
@@ -26,4 +32,8 @@ export {
   createSelectJsonPathHelper,
   specFromInputToResults,
   SpectralDsl,
+  OpticCIRuleset,
+  mergeRulesets,
+  disableRules,
+  OpticCINamedRulesets,
 };
