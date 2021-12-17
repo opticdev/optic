5352e7c9a2ef7c905ef5c75d1198cbe5bf26f393
Aidan Cunniffe <aidan@useoptic.com>
2021-12-17 10:33:20 -0500

    update index.ts

--- projects/openapi-utilities/src/index.ts
+++ projects/openapi-utilities/src/index.ts
@@ -1,6 +1,6 @@
-import { OpenAPITraverser } from "./openapi3/implementations/openapi3/openapi-traverser";
-import { factsToChangelog } from "./openapi3/sdk/facts-to-changelog";
-import { OpenAPIV3 } from "openapi-types";
+import { OpenAPITraverser } from './openapi3/implementations/openapi3/openapi-traverser';
+import { factsToChangelog } from './openapi3/sdk/facts-to-changelog';
+import { OpenAPIV3 } from 'openapi-types';
 import {
   ConceptualLocation,
   IChange,
@@ -23,7 +23,8 @@ import {
   BodyLocation,
   FieldLocation,
   ChangeType,
-} from "./openapi3/sdk/types";
+} from './openapi3/sdk/types';
+import { queryChangelog } from './openapi3/sdk/selectors/changelog-selector';
 export { defaultEmptySpec } from './openapi3/constants';
 
 export {
@@ -51,4 +52,5 @@ export {
   BodyLocation,
   FieldLocation,
   ChangeType,
+  queryChangelog,
 };
