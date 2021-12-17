a9aeaf171adffbbacf3424cb0fe63814f353d94e
Aidan Cunniffe <aidan@useoptic.com>
2021-12-17 10:04:00 -0500

    removed optic-ci

--- projects/optic-ci/src/cli-index.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import { makeCiCliWithNamedRules, rulesets } from '@useoptic/api-checks';
-
-const cli = makeCiCliWithNamedRules('optic-ci', rulesets);
-
-cli.parse(process.argv);
