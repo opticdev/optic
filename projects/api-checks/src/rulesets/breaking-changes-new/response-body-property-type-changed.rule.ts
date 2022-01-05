8de5819cb265b9bc435a508370347a876f573804
Stephen Mizell <smizell@gmail.com>
2022-01-05 13:59:48 -0600

    Add response and status code rules

--- projects/api-checks/src/rulesets/breaking-changes-new/response-body-property-type-changed.rule.ts
+++ projects/api-checks/src/rulesets/breaking-changes-new/response-body-property-type-changed.rule.ts
@@ -1,22 +1,35 @@
 import { check } from '../../sdk/define-check-test-dsl/define-check';
 import { scenario } from '../../sdk/define-check-test-dsl/scenarios';
 
-const responseBodyPropertyTypeChanged = check(
-  'prevent changing property type'
-).failingExample(
-  scenario('changing type').responseBodySchema.changed(
-    {
-      type: 'object',
-      properties: {
-        name: { type: 'string' },
-      },
-      required: [],
-    },
-    (schema) => {
-      if (schema.properties && !('$ref' in schema.properties.name)) {
-        schema.properties.name.type = 'number';
+const responseBodyPropertyTypeChanged = check('prevent changing property type')
+  .implementation(({ bodyProperties }) => {
+    const { expect } = require('chai');
+    bodyProperties.changed.must(
+      'not allow changing types',
+      (beforeProperty, afterProperty, context) => {
+        if (
+          context.isInResponse &&
+          beforeProperty.flatSchema.type !== afterProperty.flatSchema.type
+        ) {
+          expect.fail('expected property to not change type');
+        }
       }
-      return schema;
-    }
-  )
-);
+    );
+  })
+  .failingExample(
+    scenario('changing type').responseBodySchema.changed(
+      {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+        },
+        required: [],
+      },
+      (schema) => {
+        if (schema.properties && !('$ref' in schema.properties.name)) {
+          schema.properties.name.type = 'number';
+        }
+        return schema;
+      }
+    )
+  );
