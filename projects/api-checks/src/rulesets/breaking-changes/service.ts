f1316950069867d91a201d09eebd66f67294fe3c
Aidan Cunniffe <acunniffe@gmail.com>
2022-01-11 17:08:28 -0500

    fix export

--- projects/api-checks/src/rulesets/breaking-changes/service.ts
+++ projects/api-checks/src/rulesets/breaking-changes/service.ts
@@ -7,38 +7,42 @@ export function breakingChangeRules(config: BreakingChecksConfig = {}) {
   const service = new ApiCheckService<ApiCheckDslContext>();
 
   // removing top level entities
-  service.useRulesFrom(require('./operation-removed.rule').runner());
-  service.useRulesFrom(require('./status-code-removed.rule').runner());
+  service.useRulesFrom(require('./operation-removed.rule').default.runner());
+  service.useRulesFrom(require('./status-code-removed.rule').default.runner());
 
   // query params
   service.useRulesFrom(
-    require('./query-param-optional-to-required-changed.rule').runner()
+    require('./query-param-optional-to-required-changed.rule').default.runner()
   );
-  service.useRulesFrom(require('./query-param-required-added.rule').runner());
-  service.useRulesFrom(require('./query-param-type-changed.rule').runner());
+  service.useRulesFrom(
+    require('./query-param-required-added.rule').default.runner()
+  );
+  service.useRulesFrom(
+    require('./query-param-type-changed.rule').default.runner()
+  );
 
   // body parameters
   service.useRulesFrom(
-    require('./request-body-property-optional-to-required-changed.rule').runner()
+    require('./request-body-property-optional-to-required-changed.rule').default.runner()
   );
 
   service.useRulesFrom(
-    require('./request-body-property-required-added.rule').runner()
+    require('./request-body-property-required-added.rule').default.runner()
   );
 
   service.useRulesFrom(
-    require('./request-body-property-type-changed.rule').runner()
+    require('./request-body-property-type-changed.rule').default.runner()
   );
 
   service.useRulesFrom(
-    require('./response-body-property-removed.rule').runner()
+    require('./response-body-property-removed.rule').default.runner()
   );
   service.useRulesFrom(
-    require('./response-body-property-required-to-optional-changed.rule').runner()
+    require('./response-body-property-required-to-optional-changed.rule').default.runner()
   );
 
   service.useRulesFrom(
-    require('./response-body-property-type-changed.rule').runner()
+    require('./response-body-property-type-changed.rule').default.runner()
   );
 
   return service;
