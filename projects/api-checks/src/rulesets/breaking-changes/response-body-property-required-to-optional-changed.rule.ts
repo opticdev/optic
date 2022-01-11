b9ddff86c5aecb8f8c28e83ae340f103d65915bd
Stephen Mizell <smizell@gmail.com>
2022-01-11 14:00:13 -0600

    Update descriptions for rules

--- projects/api-checks/src/rulesets/breaking-changes/response-body-property-required-to-optional-changed.rule.ts
+++ projects/api-checks/src/rulesets/breaking-changes/response-body-property-required-to-optional-changed.rule.ts
@@ -1,18 +1,20 @@
 import { check } from '../../sdk/define-check-test-dsl/define-check';
 import { scenario } from '../../sdk/define-check-test-dsl/scenarios';
 
-export default check('prevent changing required to optional')
+export default check(
+  'prevent changing response body property from required to optional'
+)
   .implementation(({ bodyProperties }) => {
     const { expect } = require('chai');
     bodyProperties.changed.must(
-      'not change from optional to required',
+      'not change from required to optional',
       (beforeProp, afterProp, context) => {
         if (
           context.isInResponse &&
           beforeProp.required &&
           !afterProp.required
         ) {
-          expect.fail('expected body property to be optional');
+          expect.fail('expected body property to be required');
         }
       }
     );
