ba0f68029ee431ab1563dd36151b479cc1dbbd5e
Nicholas <18374483+niclim@users.noreply.github.com>
2022-01-14 14:08:14 -0500

    remove NormalizedCiContext from ci-types

--- projects/openapi-utilities/src/ci-types.ts
+++ projects/openapi-utilities/src/ci-types.ts
@@ -1,14 +1,6 @@
 import { ResultWithSourcemap } from './types';
 import { IChange, OpenApiFact } from './openapi3/sdk/types';
 
-export type NormalizedCiContext = {
-  organization: string;
-  pull_request: number;
-  run: number;
-  commit_hash: string;
-  repo: string;
-};
-
 export type CompareFileJson = {
   results: ResultWithSourcemap[];
   changes: IChange<OpenApiFact>[];
