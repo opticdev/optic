3f486e57350d0c7b2a2571057d1a30947d10bc49
Jaap Rood <jaap@jaaprood.nl>
2022-02-04 15:27:17 +0100

    Scaffold update command and read spec + sourcemap into memory

---
+++ projects/openapi-cli/src/update.ts
@@ -0,0 +1,28 @@
+import { Command } from 'commander';
+import Path from 'path';
+import * as fs from 'fs-extra';
+
+import {
+  JsonSchemaSourcemap,
+  parseOpenAPIWithSourcemap,
+} from '@useoptic/openapi-io';
+
+export function registerUpdateCommand(cli: Command) {
+  cli
+    .command('update')
+    .usage('openapi.yml')
+    .argument('<openapi-file>', 'an OpenAPI spec file to update')
+    .description(
+      'update an OpenAPI specification from examples or observed traffic'
+    )
+    .action(async (specPath) => {
+      const absoluteSpecPath = Path.resolve(specPath);
+      if (!(await fs.pathExists(absoluteSpecPath))) {
+        return cli.error('OpenAPI specification file could not be found');
+      }
+
+      const { jsonLike: spec, sourcemap } = await parseOpenAPIWithSourcemap(
+        absoluteSpecPath
+      );
+    });
+}
