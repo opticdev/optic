117a273b176135b48b2e50c17576b855514e4ac7
Aidan Cunniffe <acunniffe@gmail.com>
2022-02-24 10:33:40 -0500

    fixed query param rule overfiring

--- projects/api-checks/src/rulesets/breaking-changes/query-param-required-added.rule.ts
+++ projects/api-checks/src/rulesets/breaking-changes/query-param-required-added.rule.ts
@@ -4,7 +4,8 @@ import { scenario } from '../../sdk/define-check-test-dsl/scenarios';
 export default check('prevent adding required query parameter')
   .implementation(({ request }) => {
     const { expect } = require('chai');
-    request.queryParameter.added.must('not be allowed', (param) => {
+    request.queryParameter.added.must('not be allowed', (param, context) => {
+      if (context.operationAdded) return; // rule doesn't apply for new operations
       if (param.required) {
         expect.fail('expected added query parameter to not be required');
       }
