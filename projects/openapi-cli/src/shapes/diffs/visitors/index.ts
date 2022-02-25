f145aac898f71a69427ab7d8739e246b31f08560
Jaap van Hardeveld <jaap@jaaprood.nl>
2022-02-25 13:45:42 +0100

    Deal with what diffs should be visited in the diff traverser

--- projects/openapi-cli/src/shapes/diffs/visitors/index.ts
+++ projects/openapi-cli/src/shapes/diffs/visitors/index.ts
@@ -23,65 +23,10 @@ const visitors: ShapeDiffVisitor[] = [
 ];
 
 export function* diffVisitors(
-  validationErrors: Iterable<ErrorObject>,
+  validationError: ErrorObject,
   example: any
 ): IterableIterator<ShapeDiffResult> {
-  let oneOfs: Map<string, ErrorObject> = new Map();
-  let oneOfBranchType: [string, ErrorObject][] = [];
-  let oneOfBranchOther: [string, ErrorObject][] = [];
-
-  for (let validationError of validationErrors) {
-    if (validationError.keyword === JsonSchemaKnownKeyword.oneOf) {
-      let schemaPath = validationError.schemaPath.substring(1); // valid json pointer
-      oneOfs.set(schemaPath, validationError);
-    } else if (validationError.schemaPath.indexOf('oneOf') > -1) {
-      // probably has a oneof ancestor
-
-      let schemaPath = jsonPointerHelpers.decode(
-        validationError.schemaPath.substring(1)
-      );
-      let oneOfPath = schemaPath.slice(0, schemaPath.lastIndexOf('oneOf') + 1);
-      let branchPath = schemaPath.slice(oneOfPath.length);
-
-      if (
-        branchPath.length == 2 &&
-        branchPath[1] === JsonSchemaKnownKeyword.type
-      ) {
-        oneOfBranchType.push([
-          jsonPointerHelpers.compile(oneOfPath),
-          validationError,
-        ]);
-      } else if (branchPath.length >= 2) {
-        oneOfBranchOther.push([
-          jsonPointerHelpers.compile(oneOfPath),
-          validationError,
-        ]);
-      }
-    } else {
-      // not related to one-of? visit right away
-      for (let visitor of visitors) {
-        yield* visitor(validationError, example);
-      }
-    }
-  }
-
-  for (let [oneOfPath, otherBranchError] of oneOfBranchOther) {
-    // any nested errors are all safe to visit
-    for (let visitor of visitors) {
-      yield* visitor(otherBranchError, example);
-    }
-
-    // once a nested error has been visited, we consider this a branch type match
-    oneOfs.delete(oneOfPath);
-    oneOfBranchType = oneOfBranchType.filter(
-      ([branchOneOfPath, _]) => oneOfPath !== branchOneOfPath
-    );
-  }
-
-  // visit any left over one ofs
-  for (let oneOfError of oneOfs.values()) {
-    for (let visitor of visitors) {
-      yield* visitor(oneOfError, example);
-    }
+  for (let visitor of visitors) {
+    yield* visitor(validationError, example);
   }
 }
