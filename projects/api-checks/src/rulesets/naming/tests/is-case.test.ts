b06c9d9a03878e9900f695cdee4bd3799ef30c33
lou <lou@useoptic.com>
2022-03-01 12:18:17 -0500

    fix[api-checks]: add specific casing tests per PR 1079

--- projects/api-checks/src/rulesets/naming/tests/is-case.test.ts
+++ projects/api-checks/src/rulesets/naming/tests/is-case.test.ts
@@ -28,6 +28,35 @@ describe('isCase', () => {
     }
   });
 
+  test('Header-Param-Case', () => {
+    const passingCases = [
+      'Accept',
+      'Accept-Encoding',
+      'Authorization',
+      'Cache-Control',
+      'Connection',
+      'Content-Type',
+      'Host',
+      'Referer',
+      'supports-kebab-or-param-case',
+      'User-Agent'
+    ];
+    const failingCases = [
+      '',
+      'camelCaseForFun',
+      'some-kebab-someCamel',
+      'snake_case_for_fun',
+    ];
+
+    for (const passingCase of passingCases) {
+      expect(isCase(passingCase, NameMustBe.headerParamCase)).toBe(true);
+    }
+
+    for (const failingCase of failingCases) {
+      expect(isCase(failingCase, NameMustBe.headerParamCase)).toBe(false);
+    }
+  });
+
   test('param-case', () => {
     const passingCases = [
       'kebab-case-for-fun',
