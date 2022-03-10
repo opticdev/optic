dd0837370308c5e2a4b068ad19afbb183eafe2f6
Jaap van Hardeveld <jaap@jaaprood.nl>
2022-03-10 16:45:38 +0100

    Express workflows as straight async functions and use applyTemplate to runs the templates

--- projects/openapi-cli/src/example-scripts/snyk/workflows.ts
+++ projects/openapi-cli/src/example-scripts/snyk/workflows.ts
@@ -1,108 +1,107 @@
-import {
-  script,
-  scriptWithoutOpenAPI,
-} from '../../../../../scripts/define-script';
-import commander from 'commander';
 import { OpenAPIV3 } from '@useoptic/openapi-utilities';
 import { writeYaml } from '@useoptic/openapi-io';
 import * as fs from 'fs';
 import * as path from 'path';
-import { refs } from './common';
-import { addCreateOperation } from './operations/create';
-import { addListOperation } from './operations/list';
-import { addGetOperation } from './operations/get';
-import { addUpdateOperation } from './operations/update';
-import { addDeleteOperation } from './operations/delete';
+import { refs } from './templates/common';
+import { addCreateOperation } from './templates/operations/create';
+import { addListOperation } from './templates/operations/list';
+import { addGetOperation } from './templates/operations/get';
+import { addUpdateOperation } from './templates/operations/update';
+import { addDeleteOperation } from './templates/operations/delete';
+import { applyTemplate } from '../../sdk';
 
-export const newResource = scriptWithoutOpenAPI(
-  'new-resource',
-  async (args) => {
-    const [resourceName, pluralResourceName] = args;
-    const titleResourceName = titleCase(resourceName);
-    const version = getResourceVersion();
-    const collectionPath = `/${pluralResourceName}`;
-    const itemPath = `${collectionPath}/{${resourceName}_id}`;
-    if (!fs.existsSync(path.join('.', 'resources')))
-      throw new Error('Resource directory does not exist');
-    await fs.mkdirSync(
-      path.join('.', 'resources', pluralResourceName, version),
-      { recursive: true }
-    );
-    const spec = buildNewResourceSpec(titleResourceName);
-    const specYaml = writeYaml(spec);
-    fs.writeFileSync(
-      path.join('.', 'resources', pluralResourceName, version, 'spec.yaml'),
-      specYaml
-    );
-  }
-)
-  .addArgument(new commander.Argument('<resource-name>', '[resource-name]'))
-  .addArgument(
-    new commander.Argument('<plural-resource-name>', '[plural-resource-name]')
+export async function newResource(resourceName, pluralResourceName) {
+  const titleResourceName = titleCase(resourceName);
+  const version = getResourceVersion();
+  const collectionPath = `/${pluralResourceName}`;
+  const itemPath = `${collectionPath}/{${resourceName}_id}`;
+  if (!fs.existsSync(path.join('.', 'resources')))
+    throw new Error('Resource directory does not exist');
+  await fs.mkdirSync(path.join('.', 'resources', pluralResourceName, version), {
+    recursive: true,
+  });
+  const spec = buildNewResourceSpec(titleResourceName);
+  const specYaml = writeYaml(spec);
+  fs.writeFileSync(
+    path.join('.', 'resources', pluralResourceName, version, 'spec.yaml'),
+    specYaml
   );
+}
 
-export const addOperation = script(
-  'add-operation',
-  async (patcher, args, flags) => {
-    const [operation, resourceName, pluralResourceName] = args;
-    const titleResourceName = titleCase(resourceName);
-    const collectionPath = `/${pluralResourceName}`;
-    const itemPath = `${collectionPath}/{${resourceName}_id}`;
-    await patcher.update((spec) => {
-      switch (operation) {
-        case 'all':
-          addCreateOperation(
-            spec,
-            collectionPath,
-            resourceName,
-            titleResourceName
-          );
-          addListOperation(
-            spec,
-            collectionPath,
-            resourceName,
-            titleResourceName
-          );
-          addGetOperation(spec, itemPath, resourceName, titleResourceName);
-          addUpdateOperation(spec, itemPath, resourceName, titleResourceName);
-          addDeleteOperation(spec, itemPath, resourceName, titleResourceName);
-          break;
-        case 'create':
-          addCreateOperation(
-            spec,
-            collectionPath,
-            resourceName,
-            titleResourceName
-          );
-          break;
-        case 'get-many':
-          addListOperation(
-            spec,
-            collectionPath,
-            resourceName,
-            titleResourceName
-          );
-          break;
-        case 'get-one':
-          addGetOperation(spec, itemPath, resourceName, titleResourceName);
-          break;
-        case 'update':
-          addUpdateOperation(spec, itemPath, resourceName, titleResourceName);
-          break;
-        case 'delete':
-          addDeleteOperation(spec, itemPath, resourceName, titleResourceName);
-          break;
-      }
-    });
-
-    await patcher.flush();
+export async function addOperation(
+  specFilePath,
+  operation,
+  resourceName,
+  pluralResourceName
+) {
+  const titleResourceName = titleCase(resourceName);
+  const collectionPath = `/${pluralResourceName}`;
+  const itemPath = `${collectionPath}/{${resourceName}_id}`;
+  switch (operation) {
+    case 'all':
+      await applyTemplate(addCreateOperation, specFilePath, {
+        collectionPath,
+        resourceName,
+        titleResourceName,
+      });
+      await applyTemplate(addListOperation, specFilePath, {
+        collectionPath,
+        resourceName,
+        titleResourceName,
+      });
+      await applyTemplate(addGetOperation, specFilePath, {
+        itemPath,
+        resourceName,
+        titleResourceName,
+      });
+      await applyTemplate(addUpdateOperation, specFilePath, {
+        itemPath,
+        resourceName,
+        titleResourceName,
+      });
+      await applyTemplate(addDeleteOperation, specFilePath, {
+        itemPath,
+        resourceName,
+        titleResourceName,
+      });
+      break;
+    case 'create':
+      await applyTemplate(addCreateOperation, specFilePath, {
+        collectionPath,
+        resourceName,
+        titleResourceName,
+      });
+      break;
+    case 'get-many':
+      await applyTemplate(addListOperation, specFilePath, {
+        collectionPath,
+        resourceName,
+        titleResourceName,
+      });
+      break;
+    case 'get-one':
+      await applyTemplate(addGetOperation, specFilePath, {
+        itemPath,
+        resourceName,
+        titleResourceName,
+      });
+      break;
+    case 'update':
+      await applyTemplate(addUpdateOperation, specFilePath, {
+        itemPath,
+        resourceName,
+        titleResourceName,
+      });
+      break;
+    case 'delete':
+      await applyTemplate(addDeleteOperation, specFilePath, {
+        itemPath,
+        resourceName,
+        titleResourceName,
+      });
+      break;
   }
-)
-  .addArgument(new commander.Argument('<operation>', '[operation]'))
-  .addArgument(new commander.Argument('<resource-name>', '[resource-name]'))
-  .addArgument(
-    new commander.Argument('<plural-resource-name>', '[plural-resource-name]')
-  );
+}
 
 function buildNewResourceSpec(titleResourceName: string): OpenAPIV3.Document {
   const spec: OpenAPIV3.Document = baseOpenApiSpec(titleResourceName);
