31156b84e3d29c9f2673b02bd7d96f068f51a7a7
Jaap van Hardeveld <jaap@jaaprood.nl>
2022-03-14 12:35:13 +0100

    Add 'debug-workflows' command to openapi-cli, allowing snyk workflows to be run

--- projects/openapi-cli/src/cli.ts
+++ projects/openapi-cli/src/cli.ts
@@ -2,14 +2,17 @@ import { program as cli } from 'commander';
 
 import { registerUpdateCommand } from './commands/update';
 import { registerDebugTemplateCommand } from './commands/debug-template';
+import { debugWorkflowsCommand } from './commands/debug-workflows';
 import { CliConfig } from './config';
 const packageJson = require('../package.json');
 
 export function makeCli(config: CliConfig) {
   cli.version(packageJson.version);
 
-  registerUpdateCommand(cli);
+  registerUpdateCommand(cli); // TODO: apply a standalone Command instance instead
   registerDebugTemplateCommand(cli);
+
+  cli.addCommand(debugWorkflowsCommand());
 
   return cli;
 }
