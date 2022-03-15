5fc26e6adb9cbe54a95275d82e02079c91979015
Jaap van Hardeveld <jaap@jaaprood.nl>
2022-03-15 13:18:24 +0100

    Expose update command through snyk-rules CLI

--- projects/snyk-rules/src/index.ts
+++ projects/snyk-rules/src/index.ts
@@ -1,10 +1,12 @@
 import { makeCiCli } from '@useoptic/api-checks/build/ci-cli/make-cli';
 import { newSnykApiCheckService } from './service';
 import { runCommand } from './workflows/commands';
+import { updateCommand } from '@useoptic/openapi-cli';
 
 const apiCheckService = newSnykApiCheckService();
 const cli = makeCiCli('sweater-comb', apiCheckService);
 
 cli.addCommand(runCommand());
+cli.addCommand(updateCommand());
 
 cli.parse(process.argv);
