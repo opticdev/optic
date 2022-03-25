2503951afd0266b1b4c1aaac90286f82a7bfaf12
Jaap van Hardeveld <jaap@jaaprood.nl>
2022-03-25 11:13:43 +0100

    Split update action from command and I/O, so it could be tested in-memory

--- projects/openapi-cli/src/commands/update.ts
+++ projects/openapi-cli/src/commands/update.ts
@@ -15,13 +15,14 @@ import {
   SpecPatch,
   SpecPatches,
   SpecFiles,
+  SpecFilesAsync,
   BodyExampleFact,
   ComponentSchemaExampleFact,
 } from '../specs';
+import { Ok, Err, Result } from 'ts-results';
 
 import { DocumentedBody } from '../shapes/body';
 import { flushEvents, trackEvent } from '../segment';
-import { ComponentSchemaExampleFacts } from '../specs/streams/facts';
 
 export function updateCommand(): Command {
   const command = new Command('update');
@@ -33,100 +34,13 @@ export function updateCommand(): Command {
       'update an OpenAPI specification from examples or observed traffic'
     )
     .action(async (specPath) => {
-      const absoluteSpecPath = Path.resolve(specPath);
-      if (!(await fs.pathExists(absoluteSpecPath))) {
-        return command.error('OpenAPI specification file could not be found');
+      const updateResult = await update(specPath);
+
+      if (updateResult.err) {
+        return command.error(updateResult.val);
       }
 
-      const { jsonLike: spec, sourcemap } = await readDeferencedSpec(
-        absoluteSpecPath
-      );
-      const specFiles = [...SpecFiles.fromSourceMap(sourcemap)];
-
-      const logger = tap(console.log.bind(console));
-
-      const stats = {
-        examplesCount: 0,
-        externalExamplesCount: 0,
-        patchesCount: 0,
-        updatedFilesCount: 0,
-        filesWithOverwrittenYamlComments: new Set<string>(),
-
-        observeBodyExamples: tap<BodyExampleFact>((exampleFact) => {
-          stats.examplesCount++;
-          if (exampleFact.value.externalValue) stats.externalExamplesCount++;
-        }),
-        observeComponentSchemaExamples: tap<ComponentSchemaExampleFact>(
-          (_exampleFact) => {
-            stats.examplesCount++;
-          }
-        ),
-        observePatches: tap<SpecPatch>((_patch) => {
-          stats.patchesCount++;
-        }),
-        observeFileOperations: tap<SpecFileOperation>((op) => {
-          const file = specFiles.find(({ path }) => path === op.filePath);
-          if (file && SpecFile.containsYamlComments(file))
-            stats.filesWithOverwrittenYamlComments.add(file.path);
-        }),
-        observeUpdatedFiles: tap<SpecFile>((_file) => {
-          stats.updatedFilesCount++;
-        }),
-      };
-
-      const facts = forkable(SpecFacts.fromOpenAPISpec(spec));
-      const bodyExampleFacts = stats.observeBodyExamples(
-        SpecFacts.bodyExamples(facts.fork())
-      );
-      const componentExampleFacts = stats.observeComponentSchemaExamples(
-        SpecFacts.componentSchemaExamples(facts.fork())
-      );
-      facts.start();
-
-      const exampleBodies = merge(
-        DocumentedBodies.fromBodyExampleFacts(bodyExampleFacts, spec),
-        DocumentedBodies.fromComponentSchemaExampleFacts(
-          componentExampleFacts,
-          spec
-        )
-      );
-
-      const specPatches = (async function* (documentedBodies): SpecPatches {
-        const updatedSchemasByPath: Map<string, SchemaObject> = new Map();
-
-        for await (let documentedBody of documentedBodies) {
-          let { specJsonPath, shapeLocation } = documentedBody;
-
-          if (updatedSchemasByPath.has(specJsonPath)) {
-            documentedBody.schema = updatedSchemasByPath.get(specJsonPath)!;
-          }
-
-          for (let patch of ShapePatches.generateBodyAdditions(
-            documentedBody
-          )) {
-            documentedBody = DocumentedBody.applyShapePatch(
-              documentedBody,
-              patch
-            );
-            yield SpecPatch.fromShapePatch(patch, specJsonPath, shapeLocation!);
-          }
-
-          updatedSchemasByPath.set(specJsonPath, documentedBody.schema!);
-        }
-      })(exampleBodies);
-
-      // additions only, so we only safely extend the spec
-      const specAdditions = stats.observePatches(
-        SpecPatches.additions(specPatches)
-      );
-
-      const fileOperations = stats.observeFileOperations(
-        SpecFileOperations.fromSpecPatches(specAdditions, sourcemap)
-      );
-
-      const updatedSpecFiles = stats.observeUpdatedFiles(
-        SpecFiles.patch(specFiles, fileOperations)
-      );
+      let { stats, results: updatedSpecFiles } = updateResult.val;
 
       for await (let writtenFilePath of SpecFiles.writeFiles(
         updatedSpecFiles
@@ -161,4 +75,113 @@ export function updateCommand(): Command {
     });
 
   return command;
+}
+
+export async function update(specPath: string): Promise<
+  Result<
+    {
+      stats: {
+        examplesCount: number;
+        externalExamplesCount: number;
+        patchesCount: number;
+        updatedFilesCount: number;
+        filesWithOverwrittenYamlComments: Set<string>;
+      };
+      results: SpecFilesAsync;
+    },
+    string
+  >
+> {
+  const absoluteSpecPath = Path.resolve(specPath);
+  if (!(await fs.pathExists(absoluteSpecPath))) {
+    return Err('OpenAPI specification file could not be found');
+  }
+
+  const { jsonLike: spec, sourcemap } = await readDeferencedSpec(
+    absoluteSpecPath
+  );
+  const specFiles = [...SpecFiles.fromSourceMap(sourcemap)];
+
+  const stats = {
+    examplesCount: 0,
+    externalExamplesCount: 0,
+    patchesCount: 0,
+    updatedFilesCount: 0,
+    filesWithOverwrittenYamlComments: new Set<string>(),
+
+    observeBodyExamples: tap<BodyExampleFact>((exampleFact) => {
+      stats.examplesCount++;
+      if (exampleFact.value.externalValue) stats.externalExamplesCount++;
+    }),
+    observeComponentSchemaExamples: tap<ComponentSchemaExampleFact>(
+      (_exampleFact) => {
+        stats.examplesCount++;
+      }
+    ),
+    observePatches: tap<SpecPatch>((_patch) => {
+      stats.patchesCount++;
+    }),
+    observeFileOperations: tap<SpecFileOperation>((op) => {
+      const file = specFiles.find(({ path }) => path === op.filePath);
+      if (file && SpecFile.containsYamlComments(file))
+        stats.filesWithOverwrittenYamlComments.add(file.path);
+    }),
+    observeUpdatedFiles: tap<SpecFile>((_file) => {
+      stats.updatedFilesCount++;
+    }),
+  };
+
+  const facts = forkable(SpecFacts.fromOpenAPISpec(spec));
+  const bodyExampleFacts = stats.observeBodyExamples(
+    SpecFacts.bodyExamples(facts.fork())
+  );
+  const componentExampleFacts = stats.observeComponentSchemaExamples(
+    SpecFacts.componentSchemaExamples(facts.fork())
+  );
+  facts.start();
+
+  const exampleBodies = merge(
+    DocumentedBodies.fromBodyExampleFacts(bodyExampleFacts, spec),
+    DocumentedBodies.fromComponentSchemaExampleFacts(
+      componentExampleFacts,
+      spec
+    )
+  );
+
+  const specPatches = (async function* (documentedBodies): SpecPatches {
+    const updatedSchemasByPath: Map<string, SchemaObject> = new Map();
+
+    for await (let documentedBody of documentedBodies) {
+      let { specJsonPath, shapeLocation } = documentedBody;
+
+      if (updatedSchemasByPath.has(specJsonPath)) {
+        documentedBody.schema = updatedSchemasByPath.get(specJsonPath)!;
+      }
+
+      for (let patch of ShapePatches.generateBodyAdditions(documentedBody)) {
+        documentedBody = DocumentedBody.applyShapePatch(documentedBody, patch);
+        yield SpecPatch.fromShapePatch(patch, specJsonPath, shapeLocation!);
+      }
+
+      updatedSchemasByPath.set(specJsonPath, documentedBody.schema!);
+    }
+  })(exampleBodies);
+
+  // additions only, so we only safely extend the spec
+  const specAdditions = stats.observePatches(
+    SpecPatches.additions(specPatches)
+  );
+
+  const fileOperations = stats.observeFileOperations(
+    SpecFileOperations.fromSpecPatches(specAdditions, sourcemap)
+  );
+
+  const updatedSpecFiles = stats.observeUpdatedFiles(
+    SpecFiles.patch(specFiles, fileOperations)
+  );
+
+  return Ok({
+    stats,
+    results: updatedSpecFiles,
+  });
 }
--- projects/openapi-cli/src/specs/streams/files.ts
+++ projects/openapi-cli/src/specs/streams/files.ts
@@ -91,3 +91,5 @@ export class SpecFiles {
     }
   }
 }
+
+export class SpecFilesAsync {}
