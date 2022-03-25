bb2c3b68ea2a0a93fbd2c19f92a3e97c070a15d5
Jaap van Hardeveld <jaap@jaaprood.nl>
2022-03-25 11:43:17 +0100

    Verify updating of spec for request / responses with examples and partial schemas

--- projects/openapi-cli/src/tests/commands/update.test.ts
+++ projects/openapi-cli/src/tests/commands/update.test.ts
@@ -28,4 +28,22 @@ describe('update command', () => {
     expect(specFiles.map((file) => file.contents)).toMatchSnapshot();
     expect(stats).toMatchSnapshot();
   });
+
+  it('can generate update spec files for request / response examples with partial schemas', async () => {
+    const path = Path.join(
+      __dirname,
+      '../../../../openapi-utilities/inputs/openapi3/operation-examples-with-partial-schemas.json'
+    );
+
+    const results = await updateAction(path);
+    const { stats, results: updatedSpecFiles } = results.expect(
+      'example spec can be read and processed'
+    );
+
+    let specFiles = await collect(updatedSpecFiles);
+
+    expect(specFiles).toHaveLength(1);
+    expect(specFiles.map((file) => file.contents)).toMatchSnapshot();
+    expect(stats).toMatchSnapshot();
+  });
 });
