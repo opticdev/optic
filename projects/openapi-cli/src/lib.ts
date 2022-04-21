e32ff5641b2abae3e29af4794d40348a508829dc
Jaap van Hardeveld <jaap@jaaprood.nl>
2022-04-21 12:16:09 +0200

    Initialise analytics of SDK against compiled-in segment config

--- projects/openapi-cli/src/lib.ts
+++ projects/openapi-cli/src/lib.ts
@@ -1,4 +1,14 @@
+import { initSegment } from './segment';
+import { readConfig } from './config';
+
 export * from './workflows';
 export { updateCommand } from './commands/update';
 export { OpenAPIV3 } from './specs';
-export { initSegment as initAnalytics } from './segment';
+
+export function initAnalytics() {
+  const config = readConfig();
+
+  if (config.analytics.segment) {
+    initSegment({ key: config.analytics.segment.key });
+  }
+}
