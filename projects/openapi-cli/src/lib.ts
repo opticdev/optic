12a701fb348432ca53be07b710f37675f3a0c7a4
Jaap van Hardeveld <jaap@jaaprood.nl>
2022-04-21 15:36:24 +0200

    Auto-init analytics sdk as per @acunniffe

--- projects/openapi-cli/src/lib.ts
+++ projects/openapi-cli/src/lib.ts
@@ -5,10 +5,12 @@ export * from './workflows';
 export { updateCommand } from './commands/update';
 export { OpenAPIV3 } from './specs';
 
-export function initAnalytics() {
+function initAnalytics() {
   const config = readConfig();
 
   if (config.analytics.segment) {
     initSegment({ key: config.analytics.segment.key });
   }
 }
+
+initAnalytics(); // @acunniffe made the call on auto-init for now
