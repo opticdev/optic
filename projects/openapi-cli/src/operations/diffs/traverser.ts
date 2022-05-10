3027c57d3d337250e6db705ed6fea9b3d19298a7
Jaap van Hardeveld <jaap@jaaprood.nl>
2022-05-10 12:42:47 +0200

    Restrict Operation type to not include references for request and response bodies

--- projects/openapi-cli/src/operations/diffs/traverser.ts
+++ projects/openapi-cli/src/operations/diffs/traverser.ts
@@ -1,9 +1,6 @@
 import { CapturedInteraction } from '../../captures';
 import { Operation } from '..';
 import { OperationDiffResult } from './result';
-import { OpenAPIV3 } from '../../specs';
-import invariant from 'ts-invariant';
-
 import { visitRequestBody, visitResponses } from './visitors';
 
 export class OperationDiffTraverser {
@@ -20,28 +17,8 @@ export class OperationDiffTraverser {
 
     const { operation, interaction } = this;
 
-    const requestBody = operation.requestBody;
-    invariant(
-      !requestBody || isNotReferenceObject(requestBody),
-      `operation expected to not have any references, found in request body, pathPattern=${operation.pathPattern} method=${operation.method}`
-    );
-    yield* visitRequestBody(interaction.request, requestBody);
+    yield* visitRequestBody(interaction.request, operation.requestBody);
 
-    const responses = Object.fromEntries(
-      Object.entries(operation.responses).map(([code, response]) => {
-        invariant(
-          isNotReferenceObject(response),
-          `operation expected to not have any reference, found in response, statusCode=${code} pathPattern=${operation.pathPattern} method=${operation.method}`
-        );
-        return [code, response];
-      })
-    );
-    yield* visitResponses(interaction.response, responses);
+    yield* visitResponses(interaction.response, operation.responses);
   }
 }
-
-const isNotReferenceObject = <T extends {}>(
-  maybeReference: T | OpenAPIV3.ReferenceObject
-): maybeReference is Exclude<T, OpenAPIV3.ReferenceObject> => {
-  return !('$ref' in maybeReference);
-};
--- projects/openapi-cli/src/operations/index.ts
+++ projects/openapi-cli/src/operations/index.ts
@@ -1,14 +1,51 @@
 import { CapturedInteraction } from '../captures';
 import { OpenAPIV3 } from '../specs';
+import { jsonPointerHelpers } from '@useoptic/json-pointer-helpers';
+import invariant from 'ts-invariant';
 
 export { DocumentedInteractions } from './streams/documented-interactions';
 export { OperationPatches } from './streams/patches';
 export { OperationPatch } from './patches';
 
-export type Operation = OpenAPIV3.OperationObject & {
+export interface Operation extends OpenAPIV3.OperationObject {
   pathPattern: string;
   method: OpenAPIV3.HttpMethods;
-};
+
+  requestBody?: OpenAPIV3.RequestBodyObject;
+  responses: { [code: string]: OpenAPIV3.ResponseObject };
+}
+
+export class Operation {
+  static fromOperationObject(
+    pathPattern: string,
+    method: OpenAPIV3.HttpMethods,
+    operation: OpenAPIV3.OperationObject
+  ): Operation {
+    const requestBody = operation.requestBody;
+    invariant(
+      !requestBody || isNotReferenceObject(requestBody),
+      `operation expected to not have any references, found in request body, pathPattern=${pathPattern} method=${method}`
+    );
+
+    const responses = Object.fromEntries(
+      Object.entries(operation.responses).map(([code, response]) => {
+        invariant(
+          isNotReferenceObject(response),
+          `operation expected to not have any reference, found in response, statusCode=${code} pathPattern=${pathPattern} method=${method}`
+        );
+        return [code, response];
+      })
+    );
+
+    return {
+      pathPattern,
+      method,
+      ...operation,
+      requestBody,
+      responses,
+    };
+  }
+}
 
 export interface DocumentedInteraction {
   interaction: CapturedInteraction;
