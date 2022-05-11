64b3f3f668ce12ebc1162d63a2adcfc2f2b0b0cb
Jaap Rood <jaap@jaaprood.nl>
2022-05-11 12:05:19 +0200

    Remove dead code

--- projects/openapi-cli/src/operations/documented-interaction.ts
+++
@@ -1,9 +0,0 @@
-import { Operation } from '.'
-
-export interface DocumentedInteraction {
-  interaction: CapturedInteraction;
-  operation: Operation;
-  specJsonPath: string;
-}
-
-export class Documented
