764b8c1a000c19347d998fbb8a94a9fbfd31938f
Jaap van Hardeveld <jaap@jaaprood.nl>
2022-05-12 13:35:50 +0200

    Verify request and response bodies are included and can be parsed

--- projects/openapi-cli/src/captures/interaction.test.ts
+++ projects/openapi-cli/src/captures/interaction.test.ts
@@ -22,4 +22,34 @@ describe('CapturedIntearction.fromHarEntry', () => {
 
     expect(interaction).toMatchSnapshot();
   });
+
+  it('includes request bodies', () => {
+    let testEntry = testEntries.find(
+      (entry) =>
+        entry.request.postData &&
+        entry.request.postData.mimeType.startsWith('application/json')
+    );
+    expect(testEntry).toBeTruthy();
+
+    let interaction = CapturedInteraction.fromHarEntry(testEntry);
+    expect(interaction).toMatchSnapshot();
+
+    let parsingBody = CapturedBody.json(interaction.request.body);
+    expect(parsingBody).resolves.toMatchObject({});
+  });
+
+  it('includes response bodies', () => {
+    let testEntry = testEntries.find(
+      (entry) =>
+        entry.response.content.text &&
+        entry.response.content.mimeType.startsWith('application/json')
+    );
+    expect(testEntry).toBeTruthy();
+
+    let interaction = CapturedInteraction.fromHarEntry(testEntry);
+    expect(interaction).toMatchSnapshot();
+
+    let parsingBody = CapturedBody.json(interaction.response.body);
+    expect(parsingBody).resolves.toMatchObject({});
+  });
 });
