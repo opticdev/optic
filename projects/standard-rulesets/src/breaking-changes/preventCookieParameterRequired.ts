7cc064d4c22c5601e61d2823bb3556463c9b9e4e
Alexandre Hervé <alexandre.herve9@gmail.com>
2022-05-13 10:14:20 +0200

    chore(openapi-ws): remove debug logs

--- projects/standard-rulesets/src/breaking-changes/preventCookieParameterRequired.ts
+++ projects/standard-rulesets/src/breaking-changes/preventCookieParameterRequired.ts
@@ -3,11 +3,9 @@ import { OperationRule, RuleError } from '@useoptic/rulesets-base';
 export const preventCookieParameterRequired = new OperationRule({
   name: 'prevent new required cookie parameters',
   rule: (operationAssertions, ruleContext) => {
-    console.log('COOKIE rule');
     operationAssertions.cookieParameter.added(
       'not add required cookie parameter',
       (parameter) => {
-        console.log('COOKIE added');
         if (ruleContext.operation.change === 'added') return; // rule doesn't apply for new operations
         if (parameter.value.required) {
           throw new RuleError({
@@ -21,7 +19,6 @@ export const preventCookieParameterRequired = new OperationRule({
     operationAssertions.cookieParameter.changed(
       'not make an optional parameter required',
       (before, after) => {
-        console.log('COOKIE changed');
         if (!before.value.required && after.value.required) {
           throw new RuleError({
             message: 'cannot make an optional parameter required',
