d79c8cecfd40b0b5a120f6bb420f0363c1c0498f
Jaap Rood <jaap@jaaprood.nl>
2022-05-13 11:58:56 +0200

    Verify encoded HAR request / response bodies are supported

--- projects/openapi-cli/src/captures/interaction.test.ts
+++ projects/openapi-cli/src/captures/interaction.test.ts
@@ -52,4 +52,48 @@ describe('CapturedIntearction.fromHarEntry', () => {
     let parsingBody = CapturedBody.json(interaction.response.body);
     expect(parsingBody).resolves.toMatchObject({});
   });
+
+  it('supports request body encoding for supported Buffer encodings', () => {
+    let testEntry = testEntries.find(
+      (entry) =>
+        entry.request.postData &&
+        entry.request.postData.mimeType.startsWith('application/json') &&
+        !entry.request.postData.encoding
+    );
+
+    let encoding = 'base64';
+    testEntry.request.postData.text = Buffer.from(
+      testEntry.request.postData.text,
+      'utf-8'
+    ).toString(encoding as BufferEncoding);
+    testEntry.request.postData.encoding = encoding;
+
+    let interaction = CapturedInteraction.fromHarEntry(testEntry);
+    expect(interaction).toMatchSnapshot();
+
+    let parsingBody = CapturedBody.json(interaction.request.body);
+    expect(parsingBody).resolves.toMatchObject({});
+  });
+
+  it('supports response body encoding for supported Buffer encodings', () => {
+    let testEntry = testEntries.find(
+      (entry) =>
+        entry.response.content.text &&
+        entry.response.content.mimeType.startsWith('application/json') &&
+        !entry.response.content.encoding
+    );
+
+    let encoding = 'base64';
+    testEntry.response.content.text = Buffer.from(
+      testEntry.response.content.text,
+      'utf-8'
+    ).toString(encoding as BufferEncoding);
+    testEntry.response.content.encoding = encoding;
+
+    let interaction = CapturedInteraction.fromHarEntry(testEntry);
+    expect(interaction).toMatchSnapshot();
+
+    let parsingBody = CapturedBody.json(interaction.response.body);
+    expect(parsingBody).resolves.toMatchObject({});
+  });
 });
