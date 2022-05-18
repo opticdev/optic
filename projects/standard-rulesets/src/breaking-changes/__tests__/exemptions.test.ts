bc65d00ed99c2d25d76fd556446baef52b623d2a
Alexandre Hervé <alexandre.herve@useoptic.com>
2022-05-18 19:47:17 +0200

    chore(standard-rulesets): remove unused test file

--- projects/standard-rulesets/src/breaking-changes/__tests__/exemptions.test.ts
+++
@@ -1,8 +0,0 @@
-import { defaultEmptySpec, OpenAPIV3 } from '@useoptic/openapi-utilities';
-import { RuleRunner } from '../rule-runner';
-import { OperationRule, Ruleset } from '../rules';
-import { createRuleInputs } from '../test-helpers';
-
-describe('ruleset', () => {
-  test('exemptions', () => {});
-});
