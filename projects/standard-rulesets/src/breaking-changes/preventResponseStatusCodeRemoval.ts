d7791d3f36cd97a2d90d92ff2224a90d54538c28
Alexandre Hervé <alexandre.herve@useoptic.com>
2022-05-18 17:32:02 +0200

    feat(standard-ruleset): remove useless condition in preventResponseStatusCodeRemoval

--- projects/standard-rulesets/src/breaking-changes/preventResponseStatusCodeRemoval.ts
+++ projects/standard-rulesets/src/breaking-changes/preventResponseStatusCodeRemoval.ts
@@ -3,12 +3,10 @@ import { ResponseRule, RuleError } from '@useoptic/rulesets-base';
 export const preventResponseStatusCodeRemoval = new ResponseRule({
   name: 'prevent response status code removal',
   rule: (responseAssertions) => {
-    responseAssertions.removed('not remove response status code', (value) => {
-      if (value) {
-        throw new RuleError({
-          message: 'must not remove response status code',
-        });
-      }
+    responseAssertions.removed('not remove response status code', (_value) => {
+      throw new RuleError({
+        message: 'must not remove response status code',
+      });
     });
   },
 });
