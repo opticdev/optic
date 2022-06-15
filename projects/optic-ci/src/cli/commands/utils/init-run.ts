2df52af6fca00dc659326e67ca5e404e05ebcb43
Alan Johnson <alan.johnson@useoptic.com>
2022-06-15 08:36:36 -0400

    Add SpecInput type or initRun

--- projects/optic-ci/src/cli/commands/utils/init-run.ts
+++ projects/optic-ci/src/cli/commands/utils/init-run.ts
@@ -2,10 +2,15 @@ import { SpecFromInput } from './compare-input-parser';
 import { specFromInputToResults } from './load-spec';
 import { OpticBackendClient } from '../../clients/optic-client';
 
+export type SpecInput = {
+  from: SpecFromInput;
+  to: SpecFromInput;
+  id: string;
+};
+
 export async function initRun(
   client: OpticBackendClient,
-  fromSpec: SpecFromInput,
-  toSpec: SpecFromInput
+  specs: SpecInput[]
 ): Promise<string> {
   const [fromResults, toResults] = await Promise.all([
     specFromInputToResults(fromSpec),
