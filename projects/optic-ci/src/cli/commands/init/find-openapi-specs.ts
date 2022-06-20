23a685cb8eb7dcf19a34e2e36f555b4b85b39ba7
Alexandre Hervé <alexandre.herve@useoptic.com>
2022-06-20 17:12:56 +0200

    feat(optic-ci): find-openapi-specs include yaml extension

--- projects/optic-ci/src/cli/commands/init/find-openapi-specs.ts
+++ projects/optic-ci/src/cli/commands/init/find-openapi-specs.ts
@@ -4,7 +4,7 @@ const exec_p = util.promisify(exec);
 
 export const findOpenAPISpecs = async () => {
   // We search for yml or json files with a line containing "openapi" followed by a "3".
-  const command = `git grep --untracked --name-only 'openapi.*3' -- '*.yml' '*.json'`;
+  const command = `git grep --untracked --name-only 'openapi.*3' -- '*.yml' '*.json' '*.yaml'`;
   const res = await exec_p(command);
   if (res.stderr) throw new Error(res.stderr);
   return res.stdout.trim().split('\n');
