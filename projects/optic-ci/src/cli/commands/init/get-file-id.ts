67ff0f194c19992a70f963dbb6ca54f3bc8b8947
Alexandre Hervé <alexandre.herve@useoptic.com>
2022-06-21 12:52:32 +0200

    feat(optic-ci): init file id replace with \s

--- projects/optic-ci/src/cli/commands/init/get-file-id.ts
+++ projects/optic-ci/src/cli/commands/init/get-file-id.ts
@@ -1,7 +1,7 @@
 import { OpenAPIV3 } from '@useoptic/openapi-utilities';
 
-export const getFileId = (spec: OpenAPIV3.Document) => {
+export const getFileId = (spec: OpenAPIV3.Document, fileName: string) => {
   const hash = Math.floor(Math.random() * 100_000);
-  const title = (spec.info.title || 'openapi').replace(/\ /g, '-');
+  const title = (spec.info.title || fileName).replace(/\s/g, '-');
   return `${title}-${hash}`;
 };
