7bf7cda08355bd4040c22ca24a6496cb5948b52d
Alexandre Hervé <alexandre.herve@useoptic.com>
2022-06-21 11:19:39 +0200

    chore(optic-ci): init improve readability

--- projects/optic-ci/src/cli/commands/init/init.ts
+++ projects/optic-ci/src/cli/commands/init/init.ts
@@ -11,18 +11,23 @@ export const init = async (): Promise<void> => {
     console.error('Error: git must be available in PATH for "init" to work.');
     return;
   }
+
   if (!(await isInGitRepo())) {
     console.error('Error: "init" must be called from a git repository.');
     return;
   }
+
   if (fs.existsSync(configFile)) {
     console.error(`Error: a pre-existing "${configFile}" file was found.`);
     return;
   }
+
   const openApiSpecPaths = await findOpenAPISpecs();
   console.log(`Found ${openApiSpecPaths.length} candidate OpenAPI spec files.`);
+
   const validSpecs = await getValidSpecs(openApiSpecPaths);
   console.log(`${validSpecs.length} of which are valid OpenAPI files.`);
+
   const opticConfigYml = generateOpticConfigYml(validSpecs);
   await writeOpticConfig(opticConfigYml);
   console.log(`Optic onfiguration file was written to ${configFile}.`);
