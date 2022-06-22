2dd6e662b24dcf51629d3d54d9a79bc004406dd1
Alexandre Hervé <alexandre.herve@useoptic.com>
2022-06-22 11:20:10 +0200

    docs(optic-ci): documentate init command

--- projects/optic-ci/src/cli/commands/init/register-init.ts
+++ projects/optic-ci/src/cli/commands/init/register-init.ts
@@ -4,6 +4,9 @@ import { init } from './init';
 
 const initWithSentry = wrapActionHandlerWithSentry(init);
 
+const description = `Initializes Optic. The command searches for valid OpenAPI specification files in your project, generates a unique ID per file and stores the result in an optic.yml configuration file.
+These IDs are meant to be stable, but you can change them before committing the configuration file.`;
+
 export const registerInit = (cli: Command) => {
-  cli.command('init').action(initWithSentry);
+  cli.command('init').description(description).action(initWithSentry);
 };
