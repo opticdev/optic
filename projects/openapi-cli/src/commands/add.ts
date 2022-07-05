6d0d747839cad47fb071c543ee7b32965a2e6797
Jaap van Hardeveld <jaap@jaaprood.nl>
2022-07-05 16:37:33 +0200

    Draft add command structure

--- projects/openapi-cli/src/commands/add.ts
+++ projects/openapi-cli/src/commands/add.ts
@@ -1,8 +1,21 @@
 import { Command } from 'commander';
-import { HttpMethods } from '../operations/index';
 import { Result, Ok, Err } from 'ts-results';
 import Path from 'path';
 import * as fs from 'fs-extra';
+
+import { Subject, tap } from '../lib/async-tools';
+import { HttpMethods } from '../operations/index';
+import {
+  OpenAPIV3,
+  SpecFile,
+  SpecFileOperation,
+  SpecFileOperations,
+  SpecFiles,
+  SpecFilesAsync,
+  SpecFilesSourcemap,
+  SpecPatches,
+  readDeferencedSpec,
+} from '../specs';
 
 export function addCommand(): Command {
   const command = new Command('add');
@@ -23,7 +36,35 @@ export function addCommand(): Command {
       }
 
       let parsedOperations = parsedOperationsResult.unwrap();
-      console.log(parsedOperations);
+
+      const specReadResult = await readDeferencedSpec(absoluteSpecPath);
+      if (specReadResult.err) {
+        command.error(
+          `OpenAPI specification could not be fully resolved: ${specReadResult.val.message}`
+        );
+      }
+      const { jsonLike: spec, sourcemap } = specReadResult.unwrap();
+
+      let addResult = await addOperations(spec, parsedOperations);
+      if (addResult.err) {
+        return command.error(addResult.val);
+      }
+
+      let { results: addPatches, observations: updateObservations } =
+        addResult.unwrap();
+
+      let { results: updatedSpecFiles, observations: fileObservations } =
+        updateSpecFiles(addPatches, sourcemap);
+
+      const writingSpecFiles = (async function () {
+        for await (let writtenFilePath of SpecFiles.writeFiles(
+          updatedSpecFiles
+        )) {
+          // console.log(`Updated ${writtenFilePath}`);
+        }
+      })();
+
+      await writingSpecFiles;
     });
 
   return command;
@@ -34,13 +75,90 @@ interface ParsedOperation {
   pathPattern: string;
 }
 
+async function addOperations(
+  spec: OpenAPIV3.Document,
+  operations: ParsedOperation[]
+): Promise<
+  Result<
+    { results: SpecPatches; observations: AsyncIterable<AddObservation> },
+    string
+  >
+> {
+  return Err('unimplemented');
+}
+
+function updateSpecFiles(
+  updatePatches: SpecPatches,
+  sourcemap: SpecFilesSourcemap
+): {
+  results: SpecFilesAsync;
+  observations: AddObservations;
+} {
+  const stats = {
+    filesWithOverwrittenYamlComments: new Set<string>(),
+  };
+  const observing = new Subject<AddObservation>();
+  const observers = {
+    fileOperation(op: SpecFileOperation) {
+      const file = specFiles.find(({ path }) => path === op.filePath);
+      if (file && SpecFile.containsYamlComments(file))
+        stats.filesWithOverwrittenYamlComments.add(file.path);
+    },
+    updatedFile(file: SpecFile) {
+      observing.onNext({
+        kind: AddObservationKind.SpecFileUpdated,
+        path: file.path,
+        overwrittenComments: stats.filesWithOverwrittenYamlComments.has(
+          file.path
+        ),
+      });
+    },
+  };
+
+  const specFiles = [...SpecFiles.fromSourceMap(sourcemap)];
+
+  const fileOperations = tap(observers.fileOperation)(
+    SpecFileOperations.fromSpecPatches(updatePatches, sourcemap)
+  );
+
+  const updatedSpecFiles = tap(observers.updatedFile)(
+    SpecFiles.patch(specFiles, fileOperations)
+  );
+
+  // making sure we end observations once we're done generating patches
+  const observedResults = (async function* (): SpecFilesAsync {
+    yield* updatedSpecFiles;
+    observing.onCompleted();
+  })();
+
+  return {
+    results: observedResults,
+    observations: observing.iterator,
+  };
+}
+
+export enum AddObservationKind {
+  MatchedPath = 'matched-path',
+  MatchedOperation = 'matched-operation',
+  OperationPatchGenerated = 'operation-patch-generated',
+  SpecFileUpdated = 'spec-file-updated',
+}
+
+export type AddObservation = {
+  kind: AddObservationKind;
+} & {
+  kind: AddObservationKind.SpecFileUpdated;
+  path: string;
+  overwrittenComments: boolean;
+};
+
+export interface AddObservations extends AsyncIterable<AddObservation> {}
+
 function parseOperations(
   rawComponents: string[]
 ): Result<ParsedOperation[], string> {
   const components = rawComponents.filter((s) => s.length > 0);
   const pairs: ParsedOperation[] = [];
-
-  console.log({ components });
 
   for (let i = 0; i < Math.ceil(components.length / 2); i++) {
     let rawMethods = components[i * 2];
