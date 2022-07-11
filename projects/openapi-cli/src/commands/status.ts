1607b42d98a1246bf01b52093898f62c14064de4
Jaap Rood <jaap@jaaprood.nl>
2022-07-11 12:17:25 +0200

    Register status command and provide basic output

--- projects/openapi-cli/src/commands/status.ts
+++ projects/openapi-cli/src/commands/status.ts
@@ -53,20 +53,22 @@ export function statusCommand(): Command {
           `OpenAPI specification could not be fully resolved: ${specReadResult.val.message}`
         );
       }
-      const { jsonLike: spec, sourcemap } = specReadResult.unwrap();
+      const { jsonLike: spec } = specReadResult.unwrap();
 
       let interactions = AT.merge(...sources);
 
       let observations = matchInteractions(spec, interactions);
+
+      await renderStatus(observations);
     });
 
   return command;
 }
 
-export async function matchInteractions(
+export function matchInteractions(
   spec: OpenAPIV3.Document,
   interactions: CapturedInteractions
-): Promise<StatusObservations> {
+): StatusObservations {
   let currentInteraction: CapturedInteraction | null = null;
 
   const trackedInteractions = AT.tap<CapturedInteraction>((interaction) => {
@@ -90,7 +92,7 @@ export async function matchInteractions(
         yield {
           kind: StatusObservationKind.InteractionMatchedOperation,
           capturedPath: documentedInteraction.interaction.request.path,
-          pathPattern: documentedInteraction.operation.pathPattern,
+          path: documentedInteraction.operation.pathPattern,
           method: documentedInteraction.operation.method,
         };
       }
@@ -111,7 +113,7 @@ export type StatusObservation = {
   | {
       kind: StatusObservationKind.InteractionMatchedOperation;
       capturedPath: string;
-      pathPattern: string;
+      path: string;
       method: string;
     }
   | {
@@ -122,3 +124,73 @@ export type StatusObservation = {
 );
 
 export interface StatusObservations extends AsyncIterable<StatusObservation> {}
+
+async function renderStatus(observations: StatusObservations) {
+  let stats = {
+    interationsCount: 0,
+    matchedOperations: new Map<string, { path: string; method: string }>(),
+    matchedInteractionCountByOperation: new Map<string, number>(),
+    unmatchedOperations: new Map<string, { path: string; method: string }>(),
+  };
+
+  for await (let observation of observations) {
+    if (
+      observation.kind === StatusObservationKind.InteractionMatchedOperation
+    ) {
+      stats.interationsCount += 1;
+      let opId = operationId(observation);
+
+      if (!stats.matchedOperations.has(opId)) {
+        let { path, method } = observation;
+        stats.matchedOperations.set(opId, { path, method });
+        stats.matchedInteractionCountByOperation.set(opId, 1);
+      } else {
+        let interactionCount =
+          stats.matchedInteractionCountByOperation.get(opId)! + 1;
+        stats.matchedInteractionCountByOperation.set(opId, interactionCount);
+      }
+    } else if (
+      observation.kind === StatusObservationKind.InteractionUnmatchedOperation
+    ) {
+      stats.interationsCount += 1;
+      let opId = operationId(observation);
+
+      if (!stats.unmatchedOperations.has(opId)) {
+        const { path, method } = observation;
+        stats.unmatchedOperations.set(opId, { path, method });
+      }
+    }
+  }
+
+  console.log('');
+  console.log('Matched Operations');
+  console.log('==================');
+  for (let matched of stats.matchedOperations.values()) {
+    console.log(
+      `${matched.method.toUpperCase().padEnd(6, ' ')} ${matched.path}`
+    );
+  }
+
+  console.log('');
+  console.log('');
+  console.log('Undocumented paths');
+  console.log('==================');
+
+  const orderedUnmatched = [...stats.unmatchedOperations.entries()]
+    .sort((a, b) => {
+      let termA = a[1].path + a[1].method; // path first, method second
+      let termB = b[1].path + a[1].method;
+
+      return termA < termB ? -1 : termA > termB ? 1 : 0;
+    })
+    .map(([_key, op]) => op);
+  for (let unmatched of orderedUnmatched) {
+    console.log(
+      `${unmatched.method.toUpperCase().padEnd(6, ' ')}${unmatched.path}`
+    );
+  }
+
+  function operationId({ path, method }: { path: string; method: string }) {
+    return `${method}${path}`;
+  }
+}
