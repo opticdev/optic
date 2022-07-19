c3ec5b09c2ff145fb52b95ce98c2a66a10955cf5
Alexandre Hervé <alexandre.herve@useoptic.com>
2022-07-19 12:08:30 +0200

    refactor(openapi-utilities): generate changelog only consumes changes and toFile

--- projects/openapi-utilities/src/utilities/generate-changelog-data.ts
+++ projects/openapi-utilities/src/utilities/generate-changelog-data.ts
@@ -1,19 +1,18 @@
 import { groupChanges } from './group-changes';
-import { CompareFileJson } from '../ci-types';
 import { OpenAPIV3 } from 'openapi-types';
 import { traverseSpec } from './traverse-spec';
+import { IChange } from '../openapi3/sdk/types';
 
 /**
  * Takes raw outputs form a run and generates the data required to display a changelog.
  */
 export const generateChangelogData = ({
-  compareOutput,
+  changes,
   toFile,
 }: {
-  compareOutput: CompareFileJson;
+  changes: IChange[];
   toFile: OpenAPIV3.Document;
 }) => {
-  const { changes } = compareOutput;
   const toFacts = traverseSpec(toFile);
   return groupChanges({ toFacts, changes });
 };
