44d41b6b11dfca623bba3fdb26cb863cb7a3a9ba
Alan Johnson <alan.johnson@useoptic.com>
2022-07-21 10:02:38 -0400

    Add breaking changes to init rulesets

--- projects/optic/src/commands/init/generate-optic-config.ts
+++ projects/optic/src/commands/init/generate-optic-config.ts
@@ -38,5 +38,5 @@ export const generateOpticConfig = (specs: SpecWithPath[], gitRoot: string) => {
     files.push(file);
   }
 
-  return { files };
+  return { files, rulesets: ['breaking-changes'] };
 };
