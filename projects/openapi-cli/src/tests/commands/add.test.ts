e8b61712dee923b333a6c209bf2948fb015a5aee
Jaap van Hardeveld <jaap@jaaprood.nl>
2022-07-21 12:30:02 +0200

    Verify add will learn request and response bodies when provided with traffic

--- projects/openapi-cli/src/tests/commands/add.test.ts
+++ projects/openapi-cli/src/tests/commands/add.test.ts
@@ -1,5 +1,6 @@
 import { addOperations } from '../../commands/add';
 import { OpenAPIV3, SpecPatches, SpecPatch } from '../../specs';
+import { CapturedInteraction, CapturedBody } from '../../captures';
 import { HttpMethods } from '../../operations';
 import * as AT from '../../lib/async-tools';
 
@@ -74,6 +75,99 @@ describe('add command', () => {
 
       expect(specPatches.length).toBe(0);
     });
+
+    describe('with traffic', () => {
+      it('generates patches adding requests, responses and bodies for matching traffic', async () => {
+        let testSpec = specFixture({
+          '/orders': {
+            get: {
+              responses: {},
+            },
+          },
+        });
+
+        const interactions = AT.from([
+          // matching new operation
+          interactionFixture(
+            '/orders/3/products',
+            HttpMethods.POST,
+            CapturedBody.fromJSON(
+              {
+                name: 'some product',
+                optionalField: true,
+              },
+              'application/json'
+            ),
+            201,
+            CapturedBody.fromJSON(
+              {
+                id: 'product-1',
+                name: 'some product',
+                optionalField: true,
+              },
+              'application/json'
+            )
+          ),
+          // matching new operation
+          interactionFixture(
+            '/orders/3/products',
+            HttpMethods.POST,
+            CapturedBody.fromJSON(
+              {
+                name: 'another product',
+              },
+              'application/json'
+            ),
+            201,
+            CapturedBody.fromJSON(
+              {
+                id: 'product-2',
+                name: 'some product',
+              },
+              'application/json'
+            )
+          ),
+          // matching existing operation
+          interactionFixture(
+            '/orders',
+            HttpMethods.GET,
+            null,
+            200,
+            CapturedBody.fromJSON(
+              [
+                {
+                  id: 'order-1',
+                  status: 'pending',
+                },
+                {
+                  id: 'order-2',
+                  status: 'dispatched',
+                },
+              ],
+              'application/json'
+            )
+          ),
+        ]);
+
+        const results = addOperations(
+          testSpec,
+          [
+            {
+              pathPattern: '/orders/{orderId}/products',
+              methods: [HttpMethods.GET, HttpMethods.POST],
+            },
+          ],
+          interactions
+        );
+
+        const specPatches = await resultingPatches(results);
+
+        expect(specPatches.length).toBeGreaterThan(0);
+
+        let updatedSpec = patchSpec(testSpec, specPatches);
+        expect(updatedSpec).toMatchSnapshot();
+      });
+    });
   });
 });
 
@@ -91,6 +185,27 @@ function patchSpec(spec, patches: SpecPatch[]) {
     updatedSpec = SpecPatch.applyPatch(patch, updatedSpec);
   }
   return updatedSpec;
+}
+
+function interactionFixture(
+  path: string,
+  method: OpenAPIV3.HttpMethods,
+  requestBody: CapturedBody | null = null,
+  responseStatusCode: string | number = '200',
+  responseBody: CapturedBody | null = null
+): CapturedInteraction {
+  return {
+    request: {
+      host: 'optic.test',
+      method,
+      path,
+      body: requestBody,
+    },
+    response: {
+      statusCode: '' + responseStatusCode,
+      body: responseBody,
+    },
+  };
 }
 
 async function resultingPatches<O>(result: {
