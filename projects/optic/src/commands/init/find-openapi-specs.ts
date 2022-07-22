07e93b2e3f34a0150c4fc91e7bd09dbccd5d0c68
Alexandre Hervé <alexandre.herve@useoptic.com>
2022-07-22 11:29:41 +0200

    feat(optic): prevent init from failing when no path is found

--- projects/optic/src/commands/init/find-openapi-specs.ts
+++ projects/optic/src/commands/init/find-openapi-specs.ts
@@ -7,13 +7,18 @@ export const findOpenAPISpecs = async () => {
   // We search for yml or json files with a line containing either `openapi: ` (yml)
   // or `openapi": ` (json), followed by a `3`.
   // The file paths are resolved from the git root.
+  // `|| true` prevents the command to fail when no file is found.
   const command = `git grep --untracked --name-only -E 'openapi(:\\ |":\\ ).*3' -- \
 $(git rev-parse --show-toplevel)/'*.yml' \
 $(git rev-parse --show-toplevel)/'*.yaml' \
-$(git rev-parse --show-toplevel)/'*.json'`;
+$(git rev-parse --show-toplevel)/'*.json' \
+|| true`;
   const res = await exec_p(command);
   if (res.stderr) throw new Error(res.stderr);
-  const relativePaths = res.stdout.trim().split('\n');
+  const relativePaths = res.stdout
+    .trim()
+    .split('\n')
+    .filter((path) => !!path);
   const cwd = process.cwd();
   return relativePaths.map((p) => path.join(cwd, p));
 };
