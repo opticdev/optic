d9407e7215ab87b3054f97504a9993019395ddfd
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2022-08-03 08:19:23 -0700

    Revert "Sync history from private repo (#1277)"
    This reverts commit 219abb13f380922e2cb12b24ece2000f57124402.

--- projects/openapi-diff-experiment/src/services/diff/qualify-schema-differs.ts
+++
@@ -1,4 +0,0 @@
-const json = ['application/json', 'application/vnd.api+json'];
-export function qualifyJsonDiffer(contentType: string) {
-  return json.includes(contentType);
-}
--- projects/optic-ci/src/cli/commands/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './ci-context-parsers';
-export * from './compare-input-parser';
-export * from './files';
-export * from './load-spec';
-export * from './s3';
-export * from './shared-upload';
