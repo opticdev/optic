25aed0b55b3125a6a9de2c60870cd0979819e982
Nicholas <18374483+niclim@users.noreply.github.com>
2022-10-20 13:32:16 -0400

    format test

--- projects/optic/src/__tests__/integration/ruleset-publish.test.ts
+++ projects/optic/src/__tests__/integration/ruleset-publish.test.ts
@@ -1,23 +1,20 @@
 import { runOptic, setupWorkspace, normalizeWorkspace } from './integration';
-
 jest.setTimeout(30000);
-// TODO replace this with real mocks when connected to backend
-jest.mock('node-fetch');
 
 describe('optic ruleset publish', () => {
   let oldEnv: any;
   beforeEach(() => {
-    oldEnv = {...process.env}
-  })
+    oldEnv = { ...process.env };
+  });
 
   afterEach(() => {
     jest.resetAllMocks();
-    process.env = {...oldEnv}
+    process.env = { ...oldEnv };
   });
 
   test('can publish a ruleset', async () => {
     const workspace = await setupWorkspace('ruleset-publish/valid-js-file');
-    process.env.OPTIC_TOKEN = '123'
+    process.env.OPTIC_TOKEN = '123';
     const { combined, code } = await runOptic(
       workspace,
       'ruleset publish ./rules.js'
@@ -28,7 +25,7 @@ describe('optic ruleset publish', () => {
 
   test('exits if ruleset file shape is not valid', async () => {
     const workspace = await setupWorkspace('ruleset-publish/invalid-js-file');
-    process.env.OPTIC_TOKEN = '123'
+    process.env.OPTIC_TOKEN = '123';
     const { combined, code } = await runOptic(
       workspace,
       'ruleset publish ./rules.js'
