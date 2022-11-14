902d1b5282410af40762048378f9639d261f9807
Aidan Cunniffe <acunniffe@gmail.com>
2022-11-14 10:30:24 -0500

    example requirements only run on specific content types (#1405)
    * example requirements only run on specific content types
    
    * reverted accidental char
    
    * cleanup
    
    * bump versions
    
    * make added work

--- projects/standard-rulesets/src/examples/requireExample.ts
+++ projects/standard-rulesets/src/examples/requireExample.ts
@@ -2,16 +2,19 @@ import {
   OperationRule,
   RequestRule,
   ResponseBody,
+  ResponseBodyRule,
   ResponseRule,
   RuleError,
 } from '@useoptic/rulesets-base';
 import { appliesWhen } from './constants';
 import { OpenAPIV3 } from 'openapi-types';
+import { qualifiedContentType } from './qualifiedContentType';
 
 export const requireResponseExamples = (applies: typeof appliesWhen[number]) =>
-  new ResponseRule({
+  new ResponseBodyRule({
     name: 'require response body examples',
-    rule: (responseAssertions) => {
+    matches: (body) => qualifiedContentType(body.contentType),
+    rule: (responseBodyAssertions) => {
       function responseToNumberOfExamples(body: ResponseBody) {
         const numberOfExamples =
           (body.raw.example ? 1 : 0) +
@@ -20,26 +23,21 @@ export const requireResponseExamples = (applies: typeof appliesWhen[number]) =>
       }
 
       if (applies === 'always') {
-        responseAssertions.requirement((value) => {
-          value.bodies.forEach((body) => {
-            if (responseToNumberOfExamples(body) < 1) {
-              throw new RuleError({
-                message: `a valid example is required for every documented response body`,
-              });
-            }
-          });
+        responseBodyAssertions.body.requirement((body) => {
+          if (responseToNumberOfExamples(body) < 1) {
+            throw new RuleError({
+              message: `a valid example is required for every documented response body`,
+            });
+          }
         });
       }
-
-      if (applies === 'addedOrChanged') {
-        responseAssertions.addedOrChanged((value) => {
-          value.bodies.forEach((body) => {
-            if (responseToNumberOfExamples(body) < 1) {
-              throw new RuleError({
-                message: `a valid example is required for added response bodies`,
-              });
-            }
-          });
+      if (applies === 'addedOrChanged' || applies === 'added') {
+        responseBodyAssertions.body.addedOrChanged((body) => {
+          if (responseToNumberOfExamples(body) < 1) {
+            throw new RuleError({
+              message: `a valid example is required for added response bodies`,
+            });
+          }
         });
       }
     },
@@ -57,6 +55,8 @@ export const requireRequestExamples = (applies: typeof appliesWhen[number]) =>
 
       if (applies === 'always') {
         requestAssertions.body.requirement((value) => {
+          if (!qualifiedContentType(value.contentType)) return;
+
           const body = value.raw as unknown as OpenAPIV3.MediaTypeObject;
 
           if (requestToNumberOfExamples(body) < 1) {
@@ -67,9 +67,11 @@ export const requireRequestExamples = (applies: typeof appliesWhen[number]) =>
         });
       }
 
-      if (applies === 'addedOrChanged') {
+      if (applies === 'addedOrChanged' || applies === 'added') {
         requestAssertions.body.addedOrChanged((value) => {
           const body = value.raw as unknown as OpenAPIV3.MediaTypeObject;
+
+          if (!qualifiedContentType(value.contentType)) return;
 
           if (requestToNumberOfExamples(body) < 1) {
             throw new RuleError({
@@ -88,13 +90,15 @@ export const requireParameterExamples = (applies: typeof appliesWhen[number]) =>
       const lifecycle = applies === 'always' ? 'requirement' : 'addedOrChanged';
       const errorMessageType = applies === 'always' ? 'every' : 'added';
 
-      function schemaHasExample(schema: OpenAPIV3.SchemaObject | OpenAPIV3.ReferenceObject | undefined) {
+      function schemaHasExample(
+        schema: OpenAPIV3.SchemaObject | OpenAPIV3.ReferenceObject | undefined
+      ) {
         if (schema) {
           if ('example' in schema) {
-            return typeof schema.example !== 'undefined'
+            return typeof schema.example !== 'undefined';
           }
         }
-        return false
+        return false;
       }
 
       operation.headerParameter[lifecycle]((header) => {
