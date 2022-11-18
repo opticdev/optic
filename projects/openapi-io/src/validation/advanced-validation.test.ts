5ee03f49a1d8a5f84e8d2453745d8c51a5925051
Aidan Cunniffe <acunniffe@gmail.com>
2022-11-18 10:42:32 -0500

    Introduced more strict OpenAPI Validation (#1410)
    * strict schema validation
    
    * make errors transparent
    
    * remove logging
    
    * bump for pre-release
    
    * bump version

---
+++ projects/openapi-io/src/validation/advanced-validation.test.ts
@@ -0,0 +1,44 @@
+import { validateSchema } from './advanced-validation';
+
+it('a polymorphic schema cannot have type', () => {
+  expect(() =>
+    validateSchema({ oneOf: [], type: 'string' })
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"schema with oneOf cannot also include keywords: type"`
+  );
+  expect(() =>
+    validateSchema({ anyOf: [], type: 'string' })
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"schema with anyOf cannot also include keywords: type"`
+  );
+  expect(() =>
+    validateSchema({ allOf: [], type: 'string' })
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"schema with anyOf cannot also include keywords: type"`
+  );
+});
+
+it('a polymorphic schema have overlapping keywords type', () => {
+  expect(() =>
+    validateSchema({ oneOf: [], anyOf: [], allOf: [] })
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"schema with oneOf cannot also include keywords: allOf, anyOf"`
+  );
+});
+
+it('an object schema cannot have array items', () => {
+  expect(() =>
+    // @ts-ignore
+    validateSchema({ type: 'object', items: [] })
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"schema with type \\"object\\" cannot also include keywords: items"`
+  );
+});
+it('an array schema cannot have properties or required', () => {
+  expect(() =>
+    // @ts-ignore
+    validateSchema({ type: 'array', required: [], properties: {} })
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"schema with type \\"array\\" cannot also include keywords: properties, required"`
+  );
+});
--- projects/openapi-io/src/validation/validator.test.ts
+++ projects/openapi-io/src/validation/validator.test.ts
@@ -164,3 +164,34 @@ test('processValidatorErrors with sourcemap', async () => {
     );
   }).toThrowErrorMatchingSnapshot();
 });
+
+test('advanced validators run and append their results', () => {
+  const json: any = {
+    ...defaultEmptySpec,
+    paths: {
+      '/api/users/{userId}': {
+        get: {
+          responses: {
+            '200': {
+              description: 'hello',
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object',
+                    oneOf: [],
+                    anyOf: [],
+                    items: [],
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+
+  expect(() => {
+    validateOpenApiV3Document(json);
+  }).toThrowErrorMatchingSnapshot();
+});
