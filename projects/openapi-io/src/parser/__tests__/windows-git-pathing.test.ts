fc71fd18b08b1ea2f15fc4ba2ff9e2d0be454936
Aidan Cunniffe <acunniffe@gmail.com>
2022-12-08 11:07:06 -0500

    Fix windows shell (#1431)
    * fixes
    
    * prerelease
    
    * prerelease bump
    
    * logging adding
    
    * bump
    
    * fix diff command
    
    * version bunch
    
    * bump
    
    * remove loggers
    
    * remove import

---
+++ projects/openapi-io/src/parser/__tests__/windows-git-pathing.test.ts
@@ -0,0 +1,26 @@
+import { filePathToGitPath } from '../resolvers/git-branch-file-resolver';
+
+it('can relativize a windows path', () => {
+  expect(
+    filePathToGitPath(
+      'C:Users\\circleupx\\source\\openapi-demo\\',
+      'C:Users\\circleupx\\source\\openapi-demo\\todo-api.yaml'
+    )
+  ).toMatchInlineSnapshot('"todo-api.yaml"');
+});
+it('can relativize a deep windows path', () => {
+  expect(
+    filePathToGitPath(
+      'C:Users\\circleupx\\source\\openapi-demo\\',
+      'C:Users\\circleupx\\source\\openapi-demo\\todo\\specs\\a-service.yaml'
+    )
+  ).toMatchInlineSnapshot(`"todo/specs/a-service.yaml"`);
+});
+it(' windows path', () => {
+  expect(
+    filePathToGitPath(
+      'C:Users/aidancunniffe/Desktop/openapi-demo',
+      'C:Users/aidancunniffe/Desktop/openapi-demo/todo-api.yaml'
+    )
+  ).toMatchInlineSnapshot(`"todo-api.yaml"`);
+});
