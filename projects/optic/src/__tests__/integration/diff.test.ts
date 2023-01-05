67173af6306f08354774389b57dead797f3d650d
Nicholas <18374483+niclim@users.noreply.github.com>
2023-01-05 14:57:50 -0600

    add tests

--- projects/optic/src/__tests__/integration/diff.test.ts
+++ projects/optic/src/__tests__/integration/diff.test.ts
@@ -24,68 +24,6 @@ describe('diff', () => {
     expect(code).toBe(0);
   });
 
-  describe('with mock server', () => {
-    setupTestServer(async ({ url, method }) => {
-      if (method === 'GET' && /\/api\/rulesets/.test(url)) {
-        return JSON.stringify({
-          rulesets: [
-            {
-              name: '@org/custom-ruleset',
-              url: `${process.env.BWTS_HOST_OVERRIDE}/download-url`,
-              uploaded_at: '2022-11-02T17:55:48.078Z',
-            },
-          ],
-        });
-      } else if (method === 'GET' && /download-url/.test(url)) {
-        return fs.readFile(
-          path.resolve(
-            __dirname,
-            './workspaces/diff/custom-rules/rules/cloud-mock.js'
-          )
-        );
-      } else if (method === 'GET' && /\/api\/ruleset-configs\//) {
-        return JSON.stringify({
-          organization_id: 'abc',
-          config: {
-            ruleset: [{ name: 'breaking-changes', config: {} }],
-          },
-          ruleset_id: 'abc',
-          created_at: '2022-11-02T17:55:48.078Z',
-          updated_at: '2022-11-02T17:55:48.078Z',
-        });
-      }
-      return JSON.stringify({});
-    });
-
-    test('custom rules', async () => {
-      const workspace = await setupWorkspace('diff/custom-rules', {
-        repo: true,
-        commit: true,
-      });
-      const { combined, code } = await runOptic(
-        workspace,
-        'diff example-api-v0.json example-api-v1.json --check'
-      );
-
-      expect(code).toBe(1);
-      expect(normalizeWorkspace(workspace, combined)).toMatchSnapshot();
-    });
-
-    test('extends', async () => {
-      const workspace = await setupWorkspace('diff/extends', {
-        repo: true,
-        commit: true,
-      });
-      const { combined, code } = await runOptic(
-        workspace,
-        'diff example-api-v0.json example-api-v1.json --check'
-      );
-
-      expect(code).toBe(1);
-      expect(normalizeWorkspace(workspace, combined)).toMatchSnapshot();
-    });
-  });
-
   test('breaking changes exclusion', async () => {
     const workspace = await setupWorkspace('diff/breaking-changes-exclusion', {
       repo: true,
@@ -129,5 +67,95 @@ describe('diff', () => {
 
     expect(code).toBe(0);
     expect(normalizeWorkspace(workspace, combined)).toMatchSnapshot();
+  });
+
+  test('with --ruleset arg', async () => {
+    const workspace = await setupWorkspace('diff/with-ruleset-arg', {
+      repo: true,
+      commit: true,
+    });
+    const { combined, code } = await runOptic(
+      workspace,
+      'diff example-api-v0.json example-api-v1.json --check --ruleset ./ruleset.yml'
+    );
+
+    expect(code).toBe(1);
+    expect(normalizeWorkspace(workspace, combined)).toMatchSnapshot();
+  });
+
+  describe('with mock server', () => {
+    setupTestServer(async ({ url, method }) => {
+      if (method === 'GET' && /\/api\/rulesets/.test(url)) {
+        return JSON.stringify({
+          rulesets: [
+            {
+              name: '@org/custom-ruleset',
+              url: `${process.env.BWTS_HOST_OVERRIDE}/download-url`,
+              uploaded_at: '2022-11-02T17:55:48.078Z',
+            },
+          ],
+        });
+      } else if (method === 'GET' && /download-url/.test(url)) {
+        return fs.readFile(
+          path.resolve(
+            __dirname,
+            './workspaces/diff/custom-rules/rules/cloud-mock.js'
+          )
+        );
+      } else if (method === 'GET' && /\/api\/ruleset-configs\//) {
+        return JSON.stringify({
+          organization_id: 'abc',
+          config: {
+            ruleset: [{ name: 'breaking-changes', config: {} }],
+          },
+          ruleset_id: 'abc',
+          created_at: '2022-11-02T17:55:48.078Z',
+          updated_at: '2022-11-02T17:55:48.078Z',
+        });
+      }
+      return JSON.stringify({});
+    });
+
+    test('ruleset key on api spec', async () => {
+      const workspace = await setupWorkspace('diff/with-x-optic-ruleset', {
+        repo: true,
+        commit: true,
+      });
+      const { combined, code } = await runOptic(
+        workspace,
+        'diff example-api-v0.json example-api-v1.json --check'
+      );
+
+      expect(code).toBe(1);
+      expect(normalizeWorkspace(workspace, combined)).toMatchSnapshot();
+    });
+
+    test('custom rules', async () => {
+      const workspace = await setupWorkspace('diff/custom-rules', {
+        repo: true,
+        commit: true,
+      });
+      const { combined, code } = await runOptic(
+        workspace,
+        'diff example-api-v0.json example-api-v1.json --check'
+      );
+
+      expect(code).toBe(1);
+      expect(normalizeWorkspace(workspace, combined)).toMatchSnapshot();
+    });
+
+    test('extends', async () => {
+      const workspace = await setupWorkspace('diff/extends', {
+        repo: true,
+        commit: true,
+      });
+      const { combined, code } = await runOptic(
+        workspace,
+        'diff example-api-v0.json example-api-v1.json --check'
+      );
+
+      expect(code).toBe(1);
+      expect(normalizeWorkspace(workspace, combined)).toMatchSnapshot();
+    });
   });
 });
