854e9259fdcb66c2a2dd803175dd7ce487802cfc
Nicholas <18374483+niclim@users.noreply.github.com>
2023-01-06 13:58:44 -0600

    add prompts to api add

--- projects/optic/src/commands/api/add.ts
+++ projects/optic/src/commands/api/add.ts
@@ -1,13 +1,14 @@
-import path from 'path';
 import { promisify } from 'util';
 import { exec as callbackExec } from 'child_process';
 import { Command } from 'commander';
+import prompts from 'prompts';
 import { wrapActionHandlerWithSentry } from '@useoptic/openapi-utilities/build/utilities/sentry';
 
 import { OpticCliConfig, VCS } from '../../config';
 import { getFileFromFsOrGit, ParseResult } from '../../utils/spec-loaders';
 import { logger } from '../../logger';
 import { OPTIC_URL_KEY } from '../../constants';
+import chalk from 'chalk';
 
 const exec = promisify(callbackExec);
 
@@ -85,14 +86,116 @@ const getApiAddAction =
       return;
     }
 
-    if ('todo fetch orgs related to token') {
-      //  - if more than one org, ask for which org to add to
+    let org: { id: string; name: string };
+
+    // TODO replace this with an API call
+    const organizations: { id: string; name: string }[] = await [
+      { id: 'a', name: 'org1' },
+      { id: 'b', name: 'org2' },
+    ];
+    if (organizations.length > 1) {
+      const response = await prompts({
+        type: 'select',
+        name: 'orgId',
+        message: 'Select the organization you want to add APIs to',
+        choices: organizations.map((org) => ({
+          title: org.name,
+          value: org.id,
+        })),
+      });
+      org = organizations.find((o) => o.id === response.orgId)!;
+    } else if (organizations.length === 0) {
+      logger.error(
+        'Authenticated token was not associated with any organizations. Generate a new token at https://app.useoptic.com'
+      );
+      process.exitCode = 1;
+      return;
+    } else {
+      org = organizations[0];
+    }
+    logger.info('');
+
+    let ruleset = options.ruleset;
+    if (!ruleset) {
+      // TODO fetch existing rulesets
+      const existingStandards: { id: string; name: string; slug: string }[] =
+        [];
+
+      const rulesetResponse = await prompts([
+        {
+          type: 'confirm',
+          message:
+            'Would you like to attach a standard to added APIs? Note that you can always add a standard later',
+          name: 'add',
+        },
+        {
+          type: (prev) =>
+            prev && existingStandards.length > 0 ? 'select' : null,
+          message:
+            "Would you like to use an existing standard or create a new standard using Optic's built in rulesets",
+          name: 'useExisting',
+          choices: [
+            {
+              title: 'Use an existing standard',
+              value: 'existing',
+            },
+            {
+              title: "Create a new standard using Optic's built in rulesets",
+              value: 'new',
+            },
+          ],
+        },
+        {
+          type: (prev) => (prev === 'existing' ? 'select' : null),
+          name: 'rulesetSlug',
+          message: 'Select an existing standard to add to these APIs',
+          choices: existingStandards.map((r) => ({
+            title: r.slug,
+            value: r.slug,
+          })),
+        },
+      ]);
+
+      if (rulesetResponse.rulesetSlug) {
+        ruleset = rulesetResponse.rulesetSlug;
+      } else if (rulesetResponse.add) {
+        const createRulesetResponse = await prompts({
+          type: 'multiselect',
+          name: 'rulesets',
+          min: 1,
+          hint: '- Space to select. Return to submit',
+          message: 'Select the rulesets you want to add to your standard',
+          choices: [
+            { title: 'Breaking changes', value: 'breaking-changes' },
+            { title: 'Enforce naming conventions', value: 'naming' },
+            { title: 'Require examples in schemas', value: 'examples' },
+          ],
+        });
+
+        // TODO create ruleset API with sensible defaults
+        const rulesetUrl = 'todo';
+        logger.info(
+          `A new standard has been created. You can view and edit this standard at ${rulesetUrl}`
+        );
+      }
+    } else {
+      // TODO make API call to check that ruleset exists
+      const rulesetExists = 'asd';
+      if (!rulesetExists) {
+        logger.warn(
+          chalk.yellow(
+            `Warning: It looks like the standard ${options.ruleset} does not exist.`
+          )
+        );
+      }
     }
 
-    if (!options.ruleset) {
-      console.log('TODO set info for rulesets');
-      //  - prompt for a ruleset / give suggestions
-    }
+    logger.info('');
+    logger.info(
+      `Adding APIs to organization ${org.name}${
+        ruleset ? ` with ruleset ${ruleset}` : ''
+      }`
+    );
 
     const candidates = path_to_spec
       ? await getShasCandidatesForPath(path_to_spec, options.historyDepth)
