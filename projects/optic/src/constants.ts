4ff977f567edf61b8470025eb9851bb01ff02784
Nicholas <18374483+niclim@users.noreply.github.com>
2023-01-12 13:57:20 -0600

    update login to point at correct PAT link

--- projects/optic/src/constants.ts
+++ projects/optic/src/constants.ts
@@ -1,3 +1,4 @@
 export const OPTIC_STANDARD_KEY = 'x-optic-standard';
 export const OPTIC_URL_KEY = 'x-optic-url';
-export const OPTIC_LOGIN_PAT_LINK = 'https://app.useoptic.com/settings';
+export const OPTIC_LOGIN_PAT_LINK =
+  'https://app.useoptic.com/user-settings/personal-access-token';
