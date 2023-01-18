54c699d0f38479f635bce8a03749820a538748ca
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2023-01-18 11:49:33 -0600

    Fix upload flow with checksum (#1524)
    * try base64 sha
    
    * bump version
    
    * set checksum
    
    * try more things
    
    * throw on upload
    
    * match casing for headers
    
    * undo the case changes

--- projects/optic/src/utils/s3.ts
+++ projects/optic/src/utils/s3.ts
@@ -1,11 +1,21 @@
 import fetch from 'node-fetch';
 
-export const uploadFileToS3 = async (signedUrl: string, file: Buffer) => {
+export const uploadFileToS3 = async (
+  signedUrl: string,
+  file: string | Buffer,
+  additionalHeaders: Record<string, string> = {}
+) => {
   await fetch(signedUrl, {
     method: 'PUT',
     headers: {
+      ...additionalHeaders,
       'x-amz-server-side-encryption': 'AES256',
     },
     body: file,
+  }).then(async (response) => {
+    if (!response.ok) {
+      const text = await response.text();
+      throw new Error(`${response.status} ${response.statusText} \n${text}`);
+    }
   });
 };
