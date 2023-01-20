a7892967f513e86d43b89c5efa7d21ee2b90bb9f
Alan Johnson <alan.johnson@useoptic.com>
2023-01-20 10:03:40 -0500

    Update login to use env backed url

--- projects/optic/src/constants.ts
+++ projects/optic/src/constants.ts
@@ -1,4 +1,2 @@
 export const OPTIC_STANDARD_KEY = 'x-optic-standard';
 export const OPTIC_URL_KEY = 'x-optic-url';
-export const OPTIC_LOGIN_PAT_LINK =
-  'https://app.useoptic.com/user-settings/personal-access-token/new';
