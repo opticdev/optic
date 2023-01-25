e78bae988d9a74e27a25cb8a39e7ab783ea7c803
Aidan Cunniffe <aidan@useoptic.com>
2023-01-25 10:35:23 -0500

    put optic extensions under 'openapi' key

--- projects/optic/src/commands/api/write-to-file.ts
+++ projects/optic/src/commands/api/write-to-file.ts
@@ -1,8 +1,13 @@
 import fs from 'node:fs/promises';
 import { jsonPointerHelpers } from '@useoptic/json-pointer-helpers';
-import { applyOperationsToYamlString } from '@useoptic/openapi-io';
+import {
+  applyOperationsToYamlString,
+  loadYaml,
+  writeYaml,
+} from '@useoptic/openapi-io';
 
 import { OPTIC_STANDARD_KEY, OPTIC_URL_KEY } from '../../constants';
+import { FlatOpenAPIV3 } from '@useoptic/openapi-utilities';
 
 export async function writeJson(
   path: string,
@@ -34,23 +39,69 @@ export async function writeYml(
   await fs
     .readFile(path, 'utf-8')
     .then((file) => {
-      const ops: Parameters<typeof applyOperationsToYamlString>[1] = [
-        {
-          op: 'replace',
-          value: data[OPTIC_URL_KEY],
-          path: jsonPointerHelpers.compile([OPTIC_URL_KEY]),
-        },
-      ];
-
-      if (data[OPTIC_STANDARD_KEY]) {
-        ops.push({
-          op: 'replace',
-          value: data[OPTIC_STANDARD_KEY],
-          path: jsonPointerHelpers.compile([OPTIC_STANDARD_KEY]),
-        });
-      }
-
-      return applyOperationsToYamlString(file, ops);
+      return addExtensionsToYaml(
+        file,
+        data['OPTIC_STANDARD_KEY']
+          ? {
+              [OPTIC_URL_KEY]: data[OPTIC_URL_KEY],
+              [OPTIC_STANDARD_KEY]: data[OPTIC_STANDARD_KEY]!,
+            }
+          : { [OPTIC_URL_KEY]: data[OPTIC_URL_KEY] }
+      );
     })
     .then((file) => fs.writeFile(path, file));
+}
+
+export function addExtensionsToYaml(
+  yamlContents: string,
+  extensions: { [key: `x-${string}`]: string }
+): string {
+  const parsed: FlatOpenAPIV3.Document = loadYaml(yamlContents);
+
+  try {
+    // delete anything already in spec with a different value
+    const toDelete = Object.keys(extensions).filter(
+      (extension) =>
+        parsed[extension] && parsed[extension] !== extensions[extension]
+    );
+
+    const ops: Parameters<typeof applyOperationsToYamlString>[1] = toDelete.map(
+      (extension) => ({
+        op: 'remove',
+        path: jsonPointerHelpers.compile([extension]),
+      })
+    );
+
+    const cleanedYaml = applyOperationsToYamlString(yamlContents, ops);
+    const matches = /["']{0,1}openapi["']{0,1}.*$/gm.exec(cleanedYaml);
+
+    if (matches) {
+      const indexOfOpenApiLineNewLine = cleanedYaml.indexOf(
+        '\n',
+        matches.index
+      );
+
+      const extensionLines = Object.entries(extensions).map((extension) => {
+        return `${extension[0]}: "${extension[1]}"`;
+      });
+
+      return (
+        cleanedYaml.substring(0, indexOfOpenApiLineNewLine) +
+        (extensionLines.length ? '\n' + extensionLines.join(`\n`) : '') +
+        cleanedYaml.substring(indexOfOpenApiLineNewLine)
+      );
+    } else {
+      throw new Error('openapi: 3.x.x line not found');
+    }
+  } catch (e) {
+    const { openapi, ...other } = parsed;
+    const updated = {};
+
+    Object.entries(extensions).forEach(
+      ([key, value]) => (updated[key] = value)
+    );
+
+    // will respect ordering
+    return writeYaml({ openapi, ...updated, ...other });
+  }
 }
