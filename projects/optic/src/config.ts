aac798f84bfc2cb990951563e4279c86f0f46a13
Nicholas <18374483+niclim@users.noreply.github.com>
2023-01-27 08:57:49 -0500

    log config in debug mode

--- projects/optic/src/config.ts
+++ projects/optic/src/config.ts
@@ -12,6 +12,7 @@ import {
   isGitStatusClean,
   resolveGitRef,
 } from './utils/git-utils';
+import { logger } from './logger';
 
 export enum VCS {
   Git = 'git',
@@ -250,6 +251,8 @@ export async function initializeConfig(): Promise<OpticCliConfig> {
       // Git command can fail in a repo with no commits, we should treat this as having no commits
     }
   }
+
+  logger.debug(cliConfig);
 
   return cliConfig;
 }
