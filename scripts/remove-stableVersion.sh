7e1fca094169e6ed482cbdd8b462811d9826047c
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-02-21 11:20:57 -0800

    fix typo

--- scripts/remove-stableVersion.sh
+++ scripts/remove-stableVersion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Remove `stableVersion` before relreasing, as it's buggy.
+# Remove `stableVersion` before releasing, as it's buggy.
 # https://github.com/yarnpkg/berry/issues/3868
 echo "$(jq 'del(.stableVersion)' package.json)" >package.json
 
