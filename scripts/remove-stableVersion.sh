271540043038ba946a092d34c718d033fde3ca16
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-02-21 10:43:55 -0800

    script to remove stableVersion

---
+++ scripts/remove-stableVersion.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Remove `stableVersion` before relreasing, as it's buggy.
+# https://github.com/yarnpkg/berry/issues/3868
+echo "$(jq 'del(.stableVersion)' package.json)" >package.json
+
+for pkgjson in $(ls projects/*/package.json); do
+  echo $(jq 'del(.stableVersion)' $pkgjson) > $pkgjson
+done
