3c2d6d15cdbb59aaab34cfe0a462095bde177dbc
Aidan Cunniffe <aidan@useoptic.com>
2023-03-22 11:35:22 -0400

    fix line width oas

--- projects/openapi-io/src/write/index.ts
+++ projects/openapi-io/src/write/index.ts
@@ -1,7 +1,10 @@
 import yaml from 'yaml';
 
 export function writeYaml(document: any, indent: 2 | 4 = 2) {
-  return yaml.stringify(document, { aliasDuplicateObjects: false });
+  return yaml.stringify(document, {
+    aliasDuplicateObjects: false,
+    lineWidth: 0,
+  });
 }
 
 export function loadYaml(contents: string) {
