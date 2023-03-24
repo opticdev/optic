06329bae7d51a725177b8f72bc395165a6e6db3d
Aidan Cunniffe <aidan@useoptic.com>
2023-03-24 10:09:23 -0400

    update

--- projects/optic/src/commands/oas/capture.ts
+++ projects/optic/src/commands/oas/capture.ts
@@ -26,43 +26,14 @@ import { runVerify } from './verify';
 import { OpticCliConfig } from '../../config';
 import Path from 'path';
 import os from 'os';
+import { clearCommand } from './capture-clear';
 const tmpDirectory = os.tmpdir();
 
 export async function captureCommand(config: OpticCliConfig): Promise<Command> {
   const command = new Command('capture');
-
-  const clear = new Command('clear');
-  command.addCommand(
-    clear
-      .description('clear captures for the OpenAPI file')
-      .argument('[openapi-file]', 'an OpenAPI spec file to add an operation to')
-      .option('--all', 'clear all captured traffic')
-      .action(async (specPath) => {
-        const options = clear.opts();
-        if (options.all) {
-          const allCaptures = path.join(tmpDirectory, 'optic', 'captures');
-          await fs.remove(allCaptures);
-          feedback.success('Cleared all captures ' + allCaptures);
-        }
-        if (specPath) {
-          const absoluteSpecPath = Path.resolve(specPath);
-          if (!(await fs.pathExists(absoluteSpecPath))) {
-            return await feedback.inputError(
-              'OpenAPI specification file could not be found',
-              InputErrors.SPEC_FILE_NOT_FOUND
-            );
-          }
-          const { trafficDirectory } = await captureStorage(specPath);
-          await fs.remove(trafficDirectory);
-          feedback.success(
-            'Cleared capture folder for ' +
-              path.relative(process.cwd(), absoluteSpecPath)
-          );
-        }
-      })
-  );
-
   const feedback = createCommandFeedback(command);
+
+  command.addCommand(clearCommand());
 
   command
     .argument('<openapi-file>', 'an OpenAPI spec file to add an operation to')
