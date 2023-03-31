19d99aaaa0b8f7fc20c906ff43f2254535da099b
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2023-03-31 13:55:52 -0400

    write a test

---
+++ projects/optic/src/__tests__/integration/bundle.test.ts
@@ -0,0 +1,31 @@
+import {
+  test,
+  expect,
+  describe,
+  jest,
+  beforeEach,
+  afterEach,
+} from '@jest/globals';
+import { runOptic, setupWorkspace, normalizeWorkspace } from './integration';
+
+jest.setTimeout(30000);
+
+let oldEnv: any;
+beforeEach(() => {
+  oldEnv = { ...process.env };
+  process.env.LOG_LEVEL = 'info';
+  process.env.OPTIC_ENV = 'local';
+});
+
+afterEach(() => {
+  process.env = { ...oldEnv };
+});
+
+describe('bundle', () => {
+  test('bundles components together', async () => {
+    const workspace = await setupWorkspace('bundle/specs');
+    const { combined, code } = await runOptic(workspace, 'bundle openapi.yml');
+    expect(normalizeWorkspace(workspace, combined)).toMatchSnapshot();
+    expect(code).toBe(0);
+  });
+});
