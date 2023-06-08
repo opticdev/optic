5cf27948d752d09511248fd13e38d65eb9f1db85
Alexandre Hervé <alexandre.herve@useoptic.com>
2023-06-08 16:58:32 +0200

    adapt ci setup to open source flow (#1986)
    * adapt ci setup to open source flow
    
    * fixes
    
    * wording
    
    * minor version
    
    * wording fix

--- projects/optic/src/commands/ci/setup.ts
+++ projects/optic/src/commands/ci/setup.ts
@@ -4,18 +4,17 @@ import prompts from 'prompts';
 import fs from 'fs/promises';
 import path from 'path';
 import chalk from 'chalk';
-import open from 'open';
 import { guessRemoteOrigin } from '../../utils/git-utils';
 import { errorHandler } from '../../error-handler';
-import { getCiSetupUrl } from '../../utils/cloud-urls';
-
-type MaybeProvider = Awaited<ReturnType<typeof guessRemoteOrigin>>;
 
 const configsPath = path.join(__dirname, '..', '..', '..', 'ci', 'configs');
 
 const usage = () => `
   optic ci setup
 `;
+
+const matchPlaceholder = 'replace-me-openapi.yml';
+const generatePlaceholder = 'replace-me-generate.sh';
 
 export const registerCiSetup = (cli: Command, config: OpticCliConfig) => {
   cli
@@ -33,6 +32,8 @@ type PromptAnswers = {
   provider: 'GitHub' | 'GitLab';
   standardsFail: boolean;
   generatedSpecs: boolean;
+  generateScript: string;
+  match: string;
 };
 
 const getCiSetupAction = (config: OpticCliConfig) => async () => {
@@ -70,22 +71,30 @@ const getCiSetupAction = (config: OpticCliConfig) => async () => {
         ],
         initial: 1,
       },
+      {
+        type: (prev) => (prev === true ? 'text' : null),
+        name: 'generateScript',
+        message:
+          'The script or command you use to genrate your specs (ex: generate.sh)',
+      },
+      {
+        type: 'text',
+        name: 'match',
+        message:
+          'Path to your OpenAPI spec files (examples: "first-spec.yml,second-spec.yml" or "**/spec.yml")',
+      },
     ],
     { onCancel: () => process.exit(1) }
   );
 
   if (answers.provider === 'GitHub') {
-    await setupGitHub(config, maybeProvider, answers);
+    await setupGitHub(config, answers);
   } else if (answers.provider === 'GitLab') {
-    await setupGitLab(config, maybeProvider, answers);
+    await setupGitLab(config, answers);
   }
 };
 
-async function setupGitHub(
-  config: OpticCliConfig,
-  provider: MaybeProvider,
-  answers: PromptAnswers
-) {
+async function setupGitHub(config: OpticCliConfig, answers: PromptAnswers) {
   const target = '.github/workflows/optic.yml';
   const targetPath = path.join(config.root, target);
   const targetDir = path.dirname(targetPath);
@@ -106,46 +115,40 @@ async function setupGitHub(
   const standardsValue = answers.standardsFail ? 'true' : 'false';
   configContent = configContent.replace('%standards_fail', standardsValue);
 
+  const generateScript = answers.generateScript || generatePlaceholder;
+  configContent = configContent.replace('%generate_script', generateScript);
+
+  const match = answers.match || matchPlaceholder;
+  configContent = configContent.replace('%match', match);
+
   await fs.writeFile(path.join(config.root, target), configContent);
 
-  console.log();
   console.log(
     `${chalk.green('✔')} Wrote ${
       answers.provider
     } CI configuration to ${target}`
   );
 
-  const instructionsUrl = getCiSetupUrl(
-    config.client.getWebBase(),
-    provider?.provider,
-    provider?.web_url
+  console.log('');
+  console.log('Next:');
+  if (!answers.match)
+    console.log(
+      `- Replace "${matchPlaceholder}" with your OpenAPI paths in the generated file (examples: "first-spec.yml,second-spec.yml" or "**/spec.yml").`
+    );
+  if (answers.generatedSpecs && !answers.generateScript)
+    console.log(
+      `- Replace "${generatePlaceholder}" with your generate command or script in the generated file.`
+    );
+  console.log('- Commit the generated CI file.');
+  console.log(
+    '- Configure your standards and authorize Optic to comment on your PRs: https://useoptic.com/docs/setup-ci, then change your OpenAPI files and submit a PR to see Optic in action!'
   );
-
-  console.log();
-  console.log(chalk.red("Wait, you're not finished yet"));
-  if (answers.generatedSpecs) {
-    console.log(
-      `Before pushing your new GitHub Actions workflow, review the commented sections marked TODO in ${target}.`
-    );
-    console.log(
-      `Then, follow the instructions at ${instructionsUrl} to set up the required secrets in your repository.`
-    );
-  } else {
-    console.log(
-      `Before pushing your new GitHub Actions workflow, follow the instructions at ${instructionsUrl} to set up the required secrets in your repository.`
-    );
-  }
-
-  console.log();
-
-  await openUrlPrompt(instructionsUrl);
+  console.log(
+    '- Check Optic cloud to get hosted preview documentation, visual changelogs and API history: https://www.useoptic.com/docs/cloud-get-started'
+  );
 }
 
-async function setupGitLab(
-  config: OpticCliConfig,
-  provider: MaybeProvider,
-  answers: PromptAnswers
-) {
+async function setupGitLab(config: OpticCliConfig, answers: PromptAnswers) {
   const target = '.gitlab-ci.yml';
   const targetPath = path.join(config.root, target);
   const targetDir = path.dirname(targetPath);
@@ -164,14 +167,19 @@ async function setupGitLab(
   );
   let configContent = await fs.readFile(fromConfig, 'utf-8');
   const standardsValue = answers.standardsFail ? '' : '# ';
-  configContent = configContent.replace('%standards_fail', standardsValue);
+  configContent = configContent.replaceAll('%standards_fail', standardsValue);
+
+  const generateScript = answers.generateScript || generatePlaceholder;
+  configContent = configContent.replaceAll('%generate_script', generateScript);
+
+  const match = answers.match || matchPlaceholder;
+  configContent = configContent.replaceAll('%match', match);
 
   if (!exists) {
     await fs.mkdir(targetDir, { recursive: true });
 
     await fs.writeFile(targetPath, configContent);
 
-    console.log();
     console.log(
       `${chalk.green('✔')} Wrote ${
         answers.provider
@@ -190,33 +198,27 @@ async function setupGitLab(
     console.log('-----------------------');
   }
 
-  const instructionsUrl = getCiSetupUrl(
-    config.client.getWebBase(),
-    provider?.provider,
-    provider?.web_url
+  console.log('');
+  console.log('Next:');
+  if (!answers.match)
+    console.log(
+      `- Replace "${matchPlaceholder}" with your OpenAPI paths in the generated file (ex: openapi.yml,other.yml).`
+    );
+  if (answers.generatedSpecs && !answers.generateScript)
+    console.log(
+      `- Replace "${generatePlaceholder}" with your generate command or script in the generated file.`
+    );
+
+  console.log('- Commit the generated file.');
+  console.log(
+    '- Configure your standards and authorize Optic to comment on your MRs: https://useoptic.com/docs/setup-ci, then change your OpenAPI files and submit a MR to see Optic in action!'
   );
-
-  console.log();
-  console.log(chalk.red("Wait, you're not finished yet"));
-  if (answers.generatedSpecs) {
-    console.log(
-      `Before pushing your new GitLab CI/CD pipeline, review the commented sections marked TODO in ${target}.`
-    );
-    console.log(
-      `Then, follow the instructions at ${instructionsUrl} to set up the required secrets in your repository.`
-    );
-  } else {
-    console.log(
-      'Before pushing your new GitLab CI/CD pipeline, follow the instructions at\n' +
-        `${instructionsUrl} to set up the required secrets in your repository.`
-    );
-  }
-  console.log();
-
-  await openUrlPrompt(instructionsUrl);
+  console.log(
+    '- Check Optic cloud to get hosted preview documentation, visual changelogs and API history: https://www.useoptic.com/docs/cloud-get-started'
+  );
 }
 
-async function verifyPath(root: string, target: string): Promise<boolean> {
+async function verifyPath(_root: string, target: string): Promise<boolean> {
   let exists = false;
   try {
     await fs.access(target);
@@ -239,22 +241,4 @@ async function verifyPath(root: string, target: string): Promise<boolean> {
   }
 
   return true;
-}
-
-async function openUrlPrompt(url: string) {
-  const answer = await prompts(
-    {
-      type: 'confirm',
-      name: 'open',
-      message: `Open setup instructions in your browser?`,
-      initial: true,
-    },
-    { onCancel: () => process.exit(1) }
-  );
-
-  if (answer.open) {
-    await open(url, { wait: false });
-  }
-
-  return;
 }
