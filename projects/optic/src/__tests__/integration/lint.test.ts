971ad10a6c6d87ccb6e9e1afd255068d2ba424a5
Aidan Cunniffe <aidan@useoptic.com>
2023-06-28 14:36:51 -0400

    fix test suite

--- projects/optic/src/__tests__/integration/lint.test.ts
+++ projects/optic/src/__tests__/integration/lint.test.ts
@@ -15,7 +15,7 @@ beforeEach(() => {
   oldEnv = { ...process.env };
   process.env.LOG_LEVEL = 'info';
   process.env.OPTIC_ENV = 'local';
-  process.env.CI = 'false';
+  process.env.CI = '';
 });
 
 afterEach(() => {
