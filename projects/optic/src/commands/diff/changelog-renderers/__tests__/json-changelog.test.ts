c10fda6af85afc5e18acd04752271e19a1e25577
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2023-07-14 09:54:55 -0400

    Fix parameters added or changed rendering differently (#2099)

--- projects/optic/src/commands/diff/changelog-renderers/__tests__/json-changelog.test.ts
+++ projects/optic/src/commands/diff/changelog-renderers/__tests__/json-changelog.test.ts
@@ -3,6 +3,7 @@ import { openAPI as petStoreBase } from '../../../../../../openapi-utilities/src
 import { openAPI as petStoreUpdated } from '../../../../../../openapi-utilities/src/examples/petstore-updated';
 import { jsonChangelog } from '../json-changelog';
 import { diff, groupDiffsByEndpoint } from '@useoptic/openapi-utilities';
+import { loadSpec } from '../../../../utils/spec-loaders';
 
 test('json changelog collects changes properly', () => {
   const diffs = diff(petStoreBase, petStoreUpdated);
@@ -20,4 +21,33 @@ test('json changelog collects changes properly', () => {
   );
 
   expect(output).toMatchSnapshot('jsonChangelog');
+});
+
+test('json changelog with parameter changes', async () => {
+  const { jsonLike: before } = await loadSpec(
+    './src/commands/diff/changelog-renderers/__tests__/fixtures/params-old.yaml',
+    {} as any,
+    { strict: false, denormalize: true }
+  );
+  const { jsonLike: after } = await loadSpec(
+    './src/commands/diff/changelog-renderers/__tests__/fixtures/params-new.yaml',
+    {} as any,
+    { strict: false, denormalize: true }
+  );
+
+  const diffs = diff(before, after);
+  const specs = {
+    from: before,
+    to: after,
+  };
+  const groupedDiffs = groupDiffsByEndpoint(specs, diffs, []);
+  const output = jsonChangelog(
+    {
+      from: before,
+      to: after,
+    },
+    groupedDiffs
+  );
+
+  expect(output).toMatchSnapshot('parameters');
 });
