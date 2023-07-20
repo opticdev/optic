8d7a0a028feb497b08a02361959df13bd4e45d5a
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2023-07-20 11:45:45 -0400

    rm optic-ci

--- projects/optic-ci/src/constants.ts
+++
@@ -1,1 +0,0 @@
-export const OPTIC_CONFIG_PATH = 'optic.config.js';
--- projects/optic-ci/src/rule-runner.ts
+++
@@ -1,5 +0,0 @@
-import { Rule, Ruleset, RuleRunner } from '@useoptic/rulesets-base';
-
-export const initializeRuleRunner = (rules: (Ruleset | Rule)[]): RuleRunner => {
-  return new RuleRunner(rules);
-};
