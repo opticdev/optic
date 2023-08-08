708390db358013ccd5994415e67c8e71b82e2eff
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2023-08-08 11:28:52 -0400

    remove remove comment

--- projects/openapi-utilities/src/legacy-ci-types.ts
+++ projects/openapi-utilities/src/legacy-ci-types.ts
@@ -4,7 +4,6 @@ import { IChange } from './openapi3/sdk/types';
 export type CompareFileJson = {
   results: ResultWithSourcemap[];
   changes: IChange[];
-  // TODO remove this on 2022 Aug
   projectRootDir?: string | false;
   version?: string;
 };
