80a49130f7c639dc46008c72e828893b3980962c
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-10-24 06:58:17 -0700

    chore(cli): use echo service in `capture init` and simplify (#2429)

--- projects/optic/src/commands/capture/init.ts
+++ projects/optic/src/commands/capture/init.ts
@@ -26,42 +26,24 @@ export function captureConfigExample(
   skipConfigUpdate: boolean
 ) {
   return `
-${skipConfigUpdate ? `capture:\n  ${oasFile}:` : ''}
-    # 🔧 Runnable example with simple get requests. 
-    # Run with "optic capture ${oasFile} --update interactive" 
-    # You can change the server and the 'requests' section to experiment
+${skipConfigUpdate ? `capture:\n  ${oasFile}:` : ''}    
+    # Complete reference documentation for this configuration file is available:
+    #   https://www.useoptic.com/docs/capturing-traffic#configuration-reference
+
+    # Run with "optic capture ${oasFile} --update interactive"
     server:
-      url: https://api.github.com
+      url: https://echo.o3c.org
     requests:
       send: 
-      - path: /users/mojombo
-        method: GET
-      - path: /users/defunkt
-        method: GET
-      - path: /users/pjhyett/repos
-        method: GET
-      - path: /users/pjhyett/followers
-        method: GET
-      - path: /orgs/opticdev/repos
-        method: GET   
-      - path: /orgs/facebook/repos
-        method: GET   
-      - path: /orgs/github/repos
-        method: GET   
-    # When you are ready, set up an actual integration that run your test suite
-    # Read reference docs here: https://www.useoptic.com/docs/capturing-traffic#configuration-reference
-    # server:
-    #   # 🔧 Update this to the command to run your server.
-    #   # Optional: If omitted, Optic assumes the server is running or started elsewhere.
-    #   command: npm dev 
-    #   # 🔧 Update this url to where your server can be reached.
-    #   url: http://localhost:8080
-    # requests:
-    #   # ℹ️ Requests should be sent to the Optic proxy, the address of which is injected into 'run.command's env as OPTIC_PROXY (or the value of 'run.proxy_variable').
-    #   run:
-    #     # 🔧 Specify a command that will generate traffic
-    #     command: test
-    #     # 🔧 OPTIC_PROXY is added to your command's env and contains the URL of an Optic's local reverse proxy. Your command should send its requests to this URL.
-    #     proxy_variable: OPTIC_PROXY
+        - path: /users
+          headers:
+            x-response-json: '[{"id":0, "name":"aidan"}]'
+        - path: /users/create
+          method: POST
+          data:
+            name: nic
+          headers:
+            x-response-json: '{"id":1, "name":"nic"}'
+            x-response-code: "201"
   `;
 }
