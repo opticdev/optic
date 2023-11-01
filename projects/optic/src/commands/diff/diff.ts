7ccaf9b9e5a94dfc5a5d2d3ac84fb72970e7f409
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-11-01 07:08:38 -0700

    show these as optional

--- projects/optic/src/commands/diff/diff.ts
+++ projects/optic/src/commands/diff/diff.ts
@@ -52,8 +52,8 @@ const description = 'Run a diff between two API specs';
 
 const usage = () => `
   optic diff <file_path> --base <base>
-  optic diff <file_to_compare_against> <file_path>
-  optic diff <file_to_compare_against> <file_path> --check`;
+  optic diff [file_to_compare_against] <file_path>
+  optic diff [file_to_compare_against] <file_path> --check`;
 
 const helpText = `
 Example usage:
