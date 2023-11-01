3e0e0e821c87ea103237f5ce4bd2d1bcf98854e2
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-11-01 09:35:08 -0700

    in usage, refer to them as file1/2

--- projects/optic/src/commands/diff/diff.ts
+++ projects/optic/src/commands/diff/diff.ts
@@ -51,9 +51,9 @@ type DiffActionOptions = {
 const description = 'Run a diff between two API specs';
 
 const usage = () => `
-  optic diff [file_path] --base <base>
-  optic diff [file_to_compare_against] [file_path]
-  optic diff [file_to_compare_against] [file_path] --check`;
+  optic diff [file] --base <base>
+  optic diff [file1] [file2]
+  optic diff [file1] [file2] --check`;
 
 const helpText = `
 Examples:
