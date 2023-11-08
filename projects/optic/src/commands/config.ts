dca2255b625dd99acf5cda11004527af6d70bf93
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2023-11-08 15:01:23 -0800

    consolidate the help bits

--- projects/optic/src/commands/config.ts
+++ projects/optic/src/commands/config.ts
@@ -1,15 +1,6 @@
 import { Command } from 'commander';
 import { OpticCliConfig, RenderTemplate } from '../config';
-
 import * as yaml from 'yaml';
-
-const usage = () => `
-  optic config [command]`;
-
-const helpText = `
-Example usage:
-  Render and view the optic.yml file
-  $ optic config show`;
 
 export const registerConfigCommand = (
   cli: Command,
@@ -19,13 +10,13 @@ export const registerConfigCommand = (
   config
     .helpOption('-h, --help', 'Display help for the command')
     .addHelpCommand(false)
-    .configureHelp({ commandUsage: usage })
-    .addHelpText('after', helpText)
+    .configureHelp({ commandUsage: () => `\n  optic config <command>` })
     .description('Commands to manage the Optic configuration');
 
   config
     .command('show')
     .description('Display the rendered Optic configuration from optic.yml')
+    .configureHelp({ commandUsage: () => `\n  optic config show` })
     .action(async () => {
       console.log(
         // rendering the template a second time isn't awesome (it first happens when the CLI is initialized), but an OpticCliConfig contains more than just whats in optic.yml and doesn't have a simple way to convert to yaml anyway.
