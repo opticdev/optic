a6a151f4835b5b5b75503c5f9c99109c8246df0a
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2023-11-09 10:55:26 -0500

    handle case where no config path

--- projects/optic/src/commands/config.ts
+++ projects/optic/src/commands/config.ts
@@ -18,9 +18,13 @@ export const registerConfigCommand = (
     .description('Display the rendered Optic configuration from optic.yml')
     .configureHelp({ commandUsage: () => `\n  optic config show` })
     .action(async () => {
-      console.log(
-        // rendering the template a second time isn't awesome (it first happens when the CLI is initialized), but an OpticCliConfig contains more than just whats in optic.yml and doesn't have a simple way to convert to yaml anyway.
-        yaml.stringify(await RenderTemplate(cliConfig.configPath!), null, 2)
-      );
+      if (cliConfig.configPath) {
+        console.log(
+          // rendering the template a second time isn't awesome (it first happens when the CLI is initialized), but an OpticCliConfig contains more than just whats in optic.yml and doesn't have a simple way to convert to yaml anyway.
+          yaml.stringify(await RenderTemplate(cliConfig.configPath), null, 2)
+        );
+      } else {
+        console.log('no config was found');
+      }
     });
 };
