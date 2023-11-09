50ea274accf6da45d2204e564acfabe74574c432
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2023-11-09 11:29:51 -0500

    remove unnecessary dotenv check

--- projects/optic/src/index.ts
+++ projects/optic/src/index.ts
@@ -1,11 +1,5 @@
 #!/usr/bin/env node
-import dotenv from 'dotenv';
-import path from 'path';
 import { initCli } from './init';
-
-dotenv.config({
-  path: path.join(__dirname, '..', '.env'),
-});
 
 (async () => {
   const cli = await initCli();
