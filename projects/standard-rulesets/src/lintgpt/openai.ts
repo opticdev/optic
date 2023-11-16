cd84c6979e9be94bfcf77889df80bb6653f08467
Alexandre Hervé <alexandre.herve@useoptic.com>
2023-11-16 14:56:29 +0100

    remove unused openai file

--- projects/standard-rulesets/src/lintgpt/openai.ts
+++
@@ -1,9 +0,0 @@
-import { OpenAI } from 'openai';
-
-export const openai: OpenAI = (() => {
-  if (process.env.OPENAI_TOKEN) {
-    return new OpenAI({
-      apiKey: process.env.OPENAI_TOKEN,
-    });
-  }
-})() as OpenAI;
