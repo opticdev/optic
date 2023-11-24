b43531111a67201a1e75b2472110d575718f8108
Alexandre Hervé <alexandre.herve@useoptic.com>
2023-11-24 15:11:58 +0100

    stop lintgpt spinners on error

--- projects/standard-rulesets/src/lintgpt/rules-helper.ts
+++ projects/standard-rulesets/src/lintgpt/rules-helper.ts
@@ -82,11 +82,6 @@ export class LintgptRulesHelper {
 
     const ora = (await import('ora')).default;
 
-    for (const rule of rules) {
-      const rule_checksum = computeRuleChecksum(rule);
-      preparedRulesMap.set(rule_checksum, { rule, rule_checksum });
-    }
-
     let spinner = ora({
       text: this.getPrepSpinnerText({
         total: rules.length,
@@ -96,61 +91,71 @@ export class LintgptRulesHelper {
 
     spinner.start();
 
-    const getRulesWithoutPrep = () =>
-      [...preparedRulesMap.values()].filter(
-        ({ prep }) => !prep || prep.status === 'requested'
-      );
-
-    const maxTime = Date.now() + 3 * 60 * 1000;
-
-    let rulesWithoutPreps = getRulesWithoutPrep();
-    let firstRun = true;
-    const pollInterval = 2000;
-
-    while (rulesWithoutPreps.length && maxTime > Date.now()) {
-      if (!firstRun)
-        await new Promise((resolve) => setTimeout(resolve, pollInterval));
-
-      const results = await this.client.getLintgptPreps(
-        rulesWithoutPreps.map((r) => r.rule_checksum)
-      );
-
-      for (const result of results.lintgpt_preps) {
-        preparedRulesMap.set(result.rule_checksum, {
-          ...preparedRulesMap.get(result.rule_checksum)!,
-          prep: result,
-        });
+    try {
+      for (const rule of rules) {
+        const rule_checksum = computeRuleChecksum(rule);
+        preparedRulesMap.set(rule_checksum, { rule, rule_checksum });
       }
 
-      rulesWithoutPreps = getRulesWithoutPrep();
+      const getRulesWithoutPrep = () =>
+        [...preparedRulesMap.values()].filter(
+          ({ prep }) => !prep || prep.status === 'requested'
+        );
 
-      if (firstRun && rulesWithoutPreps.length) {
-        const rulesToPrep = rulesWithoutPreps.map((r) => r.rule);
-        await this.client.requestLintgptPreps(rulesToPrep);
-      }
+      const maxTime = Date.now() + 3 * 60 * 1000;
 
-      spinner.text = this.getPrepSpinnerText({
-        total: rules.length,
-        evaluated: rules.length - rulesWithoutPreps.length,
-      });
+      let rulesWithoutPreps = getRulesWithoutPrep();
+      let firstRun = true;
+      const pollInterval = 2000;
 
-      firstRun = false;
-    }
+      while (rulesWithoutPreps.length && maxTime > Date.now()) {
+        if (!firstRun)
+          await new Promise((resolve) => setTimeout(resolve, pollInterval));
 
-    if (rulesWithoutPreps.length) {
-      spinner.warn(
-        `LintGPT: ${rulesWithoutPreps.length}/${rules.length} rules timed out`
-      );
-    } else {
-      spinner.succeed(
-        this.getPrepSpinnerText({
+        const results = await this.client.getLintgptPreps(
+          rulesWithoutPreps.map((r) => r.rule_checksum)
+        );
+
+        for (const result of results.lintgpt_preps) {
+          preparedRulesMap.set(result.rule_checksum, {
+            ...preparedRulesMap.get(result.rule_checksum)!,
+            prep: result,
+          });
+        }
+
+        rulesWithoutPreps = getRulesWithoutPrep();
+
+        if (firstRun && rulesWithoutPreps.length) {
+          const rulesToPrep = rulesWithoutPreps.map((r) => r.rule);
+          await this.client.requestLintgptPreps(rulesToPrep);
+        }
+
+        spinner.text = this.getPrepSpinnerText({
           total: rules.length,
           evaluated: rules.length - rulesWithoutPreps.length,
-        })
-      );
-    }
+        });
 
-    return preparedRulesMap;
+        firstRun = false;
+      }
+
+      if (rulesWithoutPreps.length) {
+        spinner.warn(
+          `LintGPT: ${rulesWithoutPreps.length}/${rules.length} rules timed out`
+        );
+      } else {
+        spinner.succeed(
+          this.getPrepSpinnerText({
+            total: rules.length,
+            evaluated: rules.length - rulesWithoutPreps.length,
+          })
+        );
+      }
+
+      return preparedRulesMap;
+    } catch (e) {
+      spinner.fail(`LintGPT: an error occured while preparing rules`);
+      throw e;
+    }
   }
 
   private getEvalSpinnerText = ({
@@ -174,17 +179,6 @@ export class LintgptRulesHelper {
 
     const ora = (await import('ora')).default;
 
-    for (const eval_request of eval_requests) {
-      const rule_checksum = eval_request.rule_checksum;
-      const node_checksum = computeNodeChecksum(eval_request);
-      const key = getEvalKey(rule_checksum, node_checksum);
-      evalsMap.set(key, {
-        eval_request,
-        rule_checksum,
-        node_checksum,
-      });
-    }
-
     let spinner = ora({
       text: this.getEvalSpinnerText({
         total: eval_requests.length,
@@ -194,84 +188,102 @@ export class LintgptRulesHelper {
 
     spinner.start();
 
-    const getRequestsWithoutEvals = () =>
-      [...evalsMap.values()].filter(
-        ({ rule_eval }) => !rule_eval || rule_eval.status === 'requested'
-      );
-
-    const maxTime = Date.now() + 5 * 60 * 1000;
-
-    let requestsWithoutEvals = getRequestsWithoutEvals();
-    let firstRun = true;
-    let pollInterval0 = 0;
-    let pollInterval1 = 1000;
-
-    while (requestsWithoutEvals.length && maxTime > Date.now()) {
-      if (!firstRun) {
-        pollInterval1 = pollInterval0 + pollInterval1;
-        pollInterval0 = pollInterval1 - pollInterval0;
-        await new Promise((resolve) => setTimeout(resolve, pollInterval1));
-      }
-
-      const queryChunks = chunk(
-        requestsWithoutEvals.map((r) => ({
-          rule_checksum: r.rule_checksum,
-          node_checksum: r.node_checksum,
-        })),
-        20
-      );
-
-      const resultChunks = await Promise.all(
-        queryChunks.map((c) => this.client.getLintgptEvals(c))
-      );
-
-      const results = resultChunks.reduce(
-        (acc, val) => ({
-          lintgpt_evals: [...acc.lintgpt_evals, ...val.lintgpt_evals],
-        }),
-        { lintgpt_evals: [] }
-      );
-
-      for (const result of results.lintgpt_evals) {
-        const key = getEvalKey(result.rule_checksum, result.node_checksum);
-        if (!evalsMap.has(key)) {
-          continue;
-        }
+    try {
+      for (const eval_request of eval_requests) {
+        const rule_checksum = eval_request.rule_checksum;
+        const node_checksum = computeNodeChecksum(eval_request);
+        const key = getEvalKey(rule_checksum, node_checksum);
         evalsMap.set(key, {
-          ...evalsMap.get(key)!,
-          rule_eval: result,
+          eval_request,
+          rule_checksum,
+          node_checksum,
         });
       }
 
-      requestsWithoutEvals = getRequestsWithoutEvals();
+      const getRequestsWithoutEvals = () =>
+        [...evalsMap.values()].filter(
+          ({ rule_eval }) => !rule_eval || rule_eval.status === 'requested'
+        );
 
-      spinner.text = this.getEvalSpinnerText({
-        total: eval_requests.length,
-        evaluated: eval_requests.length - requestsWithoutEvals.length,
-      });
+      const maxTime = Date.now() + 5 * 60 * 1000;
 
-      if (firstRun && requestsWithoutEvals.length) {
-        const evalsToRequest = requestsWithoutEvals.map((r) => r.eval_request);
-        await this.client.requestLintgptEvals(evalsToRequest);
-      }
+      let requestsWithoutEvals = getRequestsWithoutEvals();
+      let firstRun = true;
+      let pollInterval0 = 0;
+      let pollInterval1 = 1000;
 
-      firstRun = false;
-    }
+      while (requestsWithoutEvals.length && maxTime > Date.now()) {
+        if (!firstRun) {
+          pollInterval1 = pollInterval0 + pollInterval1;
+          pollInterval0 = pollInterval1 - pollInterval0;
+          await new Promise((resolve) => setTimeout(resolve, pollInterval1));
+        }
 
-    if (requestsWithoutEvals.length) {
-      spinner.warn(
-        `LintGPT: ${requestsWithoutEvals.length}/${eval_requests.length} checks timed out`
-      );
-    } else {
-      spinner.succeed(
-        this.getEvalSpinnerText({
+        const queryChunks = chunk(
+          requestsWithoutEvals.map((r) => ({
+            rule_checksum: r.rule_checksum,
+            node_checksum: r.node_checksum,
+          })),
+          20
+        );
+
+        const resultChunks = await Promise.all(
+          queryChunks.map((c) => this.client.getLintgptEvals(c))
+        );
+
+        const results = resultChunks.reduce(
+          (acc, val) => ({
+            lintgpt_evals: [...acc.lintgpt_evals, ...val.lintgpt_evals],
+          }),
+          { lintgpt_evals: [] }
+        );
+
+        for (const result of results.lintgpt_evals) {
+          const key = getEvalKey(result.rule_checksum, result.node_checksum);
+          if (!evalsMap.has(key)) {
+            continue;
+          }
+          evalsMap.set(key, {
+            ...evalsMap.get(key)!,
+            rule_eval: result,
+          });
+        }
+
+        requestsWithoutEvals = getRequestsWithoutEvals();
+
+        spinner.text = this.getEvalSpinnerText({
           total: eval_requests.length,
           evaluated: eval_requests.length - requestsWithoutEvals.length,
-        })
-      );
-    }
+        });
 
-    return evalsMap;
+        if (firstRun && requestsWithoutEvals.length) {
+          const evalsToRequest = requestsWithoutEvals.map(
+            (r) => r.eval_request
+          );
+          await this.client.requestLintgptEvals(evalsToRequest);
+        }
+
+        firstRun = false;
+      }
+
+      if (requestsWithoutEvals.length) {
+        spinner.warn(
+          `LintGPT: ${requestsWithoutEvals.length}/${eval_requests.length} checks timed out`
+        );
+      } else {
+        spinner.succeed(
+          this.getEvalSpinnerText({
+            total: eval_requests.length,
+            evaluated: eval_requests.length - requestsWithoutEvals.length,
+          })
+        );
+      }
+
+      return evalsMap;
+    } catch (e) {
+      spinner.fail('LintGPT: an error occured while running evals');
+      throw e;
+    }
   }
 }
 
