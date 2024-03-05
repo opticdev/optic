93134c4cf18a4993de5ca66eac446505eb957c1d
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2024-03-05 11:03:21 -0500

    handle swagger 2.0 semver check (#2774)

--- projects/openapi-io/src/validation/openapi-versions.test.ts
+++ projects/openapi-io/src/validation/openapi-versions.test.ts
@@ -13,6 +13,7 @@ test('detects a 3.0.x openapi', async () => {
 });
 
 test('detects a 2.x.x openapi', async () => {
+  expect(checkOpenAPIVersion({ swagger: '2.0' })).toBe('2.x.x');
   expect(checkOpenAPIVersion({ swagger: '2.0.3' })).toBe('2.x.x');
   expect(checkOpenAPIVersion({ swagger: '2.1.2' })).toBe('2.x.x');
 });
